e13be4af9a5d18b2295be443f73f2249a0c70694
rern <rernrern@gmail.com>
2020-12-11 16:46:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 alias=r1
 
+sed -i 's/raudio$/&ap/' /etc/hostapd/hostapd.conf
+mv /srv/http/data/system/relaysset /etc/relays.conf &> /dev/null
+
 . /srv/http/bash/addons.sh
 
 installstart "$1"
 
 getinstallzip
-
-mv /srv/http/data/system/relaysset /etc/relays.conf &> /dev/null
 
 installfinish
