e48245a2ca816a2c861a48f8874f8c64e7a7c718
rern <rernrern@gmail.com>
2020-12-12 15:30:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 alias=r1
 
+[[ ! -e /etc/relays.conf ]] && wget -qO /etc/relays.conf https://github.com/rern/rOS/raw/main/etc/relays.conf
 sed -i 's/raudio$/&ap/' /etc/hostapd/hostapd.conf
 mv /srv/http/data/system/relaysset /etc/relays.conf &> /dev/null
 
