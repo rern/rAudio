f6e519f011fbc26aaf0e7871f71b8a830ddf0df6
rern <rernrern@gmail.com>
2020-12-15 06:25:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ alias=r1
 . /srv/http/bash/addons.sh
 
 sed -i '/^#IgnorePkg/ a\IgnorePkg   = raspberrypi-firmware raspberrypi-bootloader raspberrypi-bootloader-x' /etc/pacman.conf
-pacman -Sy cifs-utils
+pacman -Sy --noconfirm cifs-utils
 
 installstart "$1"
 
