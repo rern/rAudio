25af6fdc9757b7dfb25f7d49516e8cf2902a2305
rern <rernrern@gmail.com>
2020-12-17 10:28:32 +0700

    Update function.js

--- srv/http/assets/js/function.js
+++ srv/http/assets/js/function.js
@@ -473,13 +473,6 @@ function getOrientation( file, callback ) { // return: 1 - undefined
 function getPlaybackStatus() {
 	local();
 	bash( '/srv/http/bash/status.sh', function( status ) {
-/*		if ( status == -1 ) {
-			$( '#sampling' ).text( 'MPD not running.' );
-			$( '#time-knob, #volume-knob, #bar-bottom' ).addClass( 'hide' );
-			renderPlaybackBlank();
-			return
-		}*/
-		
 		if ( !status ) return
 		
 		$.each( status, function( key, value ) {
