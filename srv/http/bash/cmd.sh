3458b3460e6f0290217a5ff11fadc8b3a528d75b
rern <rernrern@gmail.com>
2020-12-27 17:55:54 +0700

    Add files via upload

--- srv/http/bash/cmd.sh
+++ srv/http/bash/cmd.sh
@@ -359,7 +359,7 @@ displayget )
 , "lock"       : '$( [[ -e $dirsystem/login ]] && echo true || echo false )'
 , "order"      : '$( cat $dirsystem/order 2> /dev/null )'
 , "relays"     : '$( [[ -e $dirsystem/relays ]] && echo true || echo false )'
-, "snapclient" : '$( [[ -e $dirsystem/snapclient ]] && echo true || echo false )'
+, "snapclient" : '$( systemctl -q is-active snapclient && echo true || echo false )'
 , "update"     : '$( cat $diraddons/update 2> /dev/null || echo false )'
 , "volumenone" : '$volume'
 }'
