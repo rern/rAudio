46b77bf6269bb6fbd3ff8b842675f669771074cf
rern <rernrern@gmail.com>
2020-12-31 21:29:14 +0700

    Update shairport.sh

--- srv/http/bash/shairport.sh
+++ srv/http/bash/shairport.sh
@@ -5,6 +5,7 @@ playerfile=/srv/http/data/shm/player
 if (( $# > 0 )); then # stop
 	mv $playerfile-{*,mpd}
 	curl -s -X POST http://127.0.0.1/pub?id=notify -d '{"title":"AirPlay","text":"Stop ...","icon":"airplay blink","delay":-1}'
+	/srv/http/bash/cmd.sh pushstatus$'\n'lcdchar
 	systemctl stop shairport-meta
 	systemctl restart shairport-sync
 else
@@ -14,6 +15,5 @@ else
 	systemctl try-restart snapclient spotifyd upmpdcli &> /dev/null
 	systemctl start shairport-meta
 	sleep 2
+	/srv/http/bash/cmd.sh pushstatus$'\n'lcdchar
 fi
-
-/srv/http/bash/cmd.sh pushstatus$'\n'lcdchar
