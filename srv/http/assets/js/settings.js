30e72405a2487e33433e0e49f10b451f4992fbc0
rern <rernrern@gmail.com>
2021-01-05 09:07:02 +0700

    Update settings.js

--- srv/http/assets/js/settings.js
+++ srv/http/assets/js/settings.js
@@ -22,14 +22,14 @@ var cmd = {
 	, crossfade    : [ 'mpc crossfade' ]
 	, fstab        : [ 'cat /etc/fstab' ]
 	, ifconfig     : [ 'ifconfig wlan0' ]
-	, iwconfig     : [ 'iw wlan0 info' ]
+	, iwconfig     : [ 'iwconfig wlan0' ]
 	, journalctl   : [ '/srv/http/bash/system.sh getjournalctl', 'journalctl -b' ]
 	, lan          : [ 'ifconfig eth0' ]
 	, mount        : [ 'mount | grep ^/dev' ]
 	, mpdconf      : [ 'cat /etc/mpd.conf' ]
 	, rfkill       : [ 'rfkill' ]
 	, soundprofile : [ '/srv/http/bash/system.sh soundprofileget', "sysctl kernel.sched_latency_ns<br># sysctl vm.swappiness<br># ifconfig eth0 | grep 'mtu\\|txq'" ]
-	, wlan         : [ 'iwconfig wlan0' ]
+	, wlan         : [ 'ifconfig wlan0' ]
 }
 var services = [ 'hostapd', 'localbrowser', 'mpd', 'mpdscribble', 'shairport-sync', 'smb', 'snapclient', 'snapserver', 'spotifyd', 'upmpdcli' ];
 function codeToggle( id, target ) {
