93c79cae998ce597a9abf5e8c15c06f54c4e74a4
rern <rernrern@gmail.com>
2021-01-14 20:56:43 +0700

    Update system.js

--- srv/http/assets/js/system.js
+++ srv/http/assets/js/system.js
@@ -55,7 +55,8 @@ function renderStatus() {
 		+'<br>'+ ( G.cputemp < 80 ? G.cputemp +' °C' : '<red><i class="fa fa-warning blink red"></i>&ensp;'+ G.cputemp +' °C</red>' )
 		+'<br>'+ G.time.replace( ' ', ' <gr>&bull;</gr> ' ) +'&emsp;<grw>'+ G.timezone.replace( '/', ' · ' ) +'</grw>'
 		+'<br>'+ G.uptime +'<span class="wide">&emsp;<gr>since '+ G.uptimesince.replace( ' ', ' &bull; ' ) +'</gr></span>'
-		+'<br><gr>kernel:</gr> '+ Math.round( startup[ 0 ] ) +'s + <gr>userspace:</gr> '+ Math.round( startup[ 1 ] ) +'s';
+		+'<br><gr>kernel:</gr> '+ Math.round( startup[ 0 ] ) +'s + <gr>userspace:</gr> '
+		+ ( startup[ 1 ] ? Math.round( startup[ 1 ] ) +'s' : 'Running ...' );
 	if ( G.throttled ) { // https://www.raspberrypi.org/documentation/raspbian/applications/vcgencmd.md
 		var bits = parseInt( G.throttled ).toString( 2 ); // 20 bits: 19..0 ( hex > decimal > binary )
 		if ( bits.slice( -1 ) == 1 ) {                    // bit# 0  - undervoltage now
