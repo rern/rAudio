84c2089861a99905b5f6dd81c3777d8d82899186
rern <rernrern@gmail.com>
2021-01-15 12:44:31 +0700

    Update cmd-list.sh

--- srv/http/bash/cmd-list.sh
+++ srv/http/bash/cmd-list.sh
@@ -38,12 +38,13 @@ album_artist_file=$( mpc -f '%album%^^[%albumartist%|%artist%]^^%file%' listall 
 #	!a[$0]++    - not duplicate lines
 
 if (( $? != 0 )); then # very large database
+	buffer=8192
+	
 	albums=$( mpc list album )
 	if (( $? == 0 )); then
 		listAlbums "$albums"
 	else
-		buffer=8192
-		for (( i=1; i < 9; i++ )); do
+		for (( i=1; i < 10; i++ )); do
 			(( i++ ))
 			sed -i '/^max_output_buffer/ d'/etc/mpd.conf
 			sed -i '1 i\max_output_buffer_size "'$(( i * $buffer ))'"' /etc/mpd.conf
@@ -54,8 +55,18 @@ if (( $? != 0 )); then # very large database
 	fi
 	if [[ -n $album ]]; then
 		listAlbums "$albums"
-		echo $buffer > $dirsystem/bufferoutputset
-		touch $dirsystem/bufferoutput
+		if (( $? != 0 )); then
+			for (( j=$i; j < 10; j++ )); do
+				(( j++ ))
+				sed -i '/^max_output_buffer/ d'/etc/mpd.conf
+				sed -i '1 i\max_output_buffer_size "'$(( j * $buffer ))'"' /etc/mpd.conf
+				systemctl restart mpd
+				listAlbums "$albums"
+				(( $? == 0 )) && break
+			done
+		else
+			echo $buffer > $dirsystem/bufferoutputset
+		fi
 	else
 		toolarge=1
 		sed -i '/^max_output_buffer/ d'/etc/mpd.conf
