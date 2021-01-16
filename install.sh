83531d51780e257801e5c01d50d23bd91cfecb9a
rern <rernrern@gmail.com>
2021-01-16 15:45:48 +0700

    u

--- install.sh
+++ install.sh
@@ -4,6 +4,15 @@ alias=r1
 
 . /srv/http/bash/addons.sh
 
+if grep -q 'default_options_exfat.*umask=0077' /etc/udevil/udevil.conf; then
+	sed -i -e '/default_options_exfat.*umask=0077/ d
+' -e '/^default_options_vfat/ a\
+default_options_exfat     = nosuid, noexec, nodev, noatime, fmask=0000, dmask=0000, utf8
+' -e '/allowed_options_vfat/ a\
+allowed_options_exfat     = nosuid, noexec, nodev, noatime, fmask=0000, dmask=0000, utf8
+' /etc/udevil/udevil.conf
+fi
+
 file=/etc/lcdchar.conf
 if [[ -e $file ]]; then
 	! grep -q backlight $file && echo backlight=False >> $file
