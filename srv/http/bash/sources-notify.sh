805f09a96bfc9d8c177b6a4043ca00b923a26a36
rern <rernrern@gmail.com>
2021-01-17 03:01:41 +0700

    u

--- srv/http/bash/sources-notify.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -s -X POST http://127.0.0.1/pub?id=refresh -d '{ "page": "sources" }'
--- srv/http/bash/sources-update.sh
+++ srv/http/bash/sources-update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # for startup.sh, source.sh and /etc/conf.d/devmon (devmon@http.service)
+curl -s -X POST http://127.0.0.1/pub?id=refresh -d '{ "page": "sources" }'
 
 path=${1:9}
 if (( $( mpc stats | awk '/Songs/ {print $NF}' ) == 0 )); then
