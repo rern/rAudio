7c7281f0d95d372a58706e25f24bfe735239a8be
rern <rernrern@gmail.com>
2021-01-23 20:17:06 +0700

    u

--- srv/http/assets/js/main.js
+++ srv/http/assets/js/main.js
@@ -63,13 +63,10 @@ for( i = 0; i < 360; i += 0.25 ) {
 	ctx.lineTo( cw + cw * Math.cos( rad ), ch + ch * Math.sin( rad ) );
 	ctx.stroke();
 }
-var stopwatch = '<svg id="stopwatch" viewBox="0 0 42 49">'
-		+'<circle cx="21" cy="28" r="20"/>'
-		+'<rect x="17" width="8" height="8"/>'
-		+'<rect x="3" y="11.17" width="4" height="4" transform="translate(-7.85 7.39) rotate(-45)"/>'
-		+'<rect x="35" y="11.17" width="4" height="4" transform="translate(1.52 30.02) rotate(-45)"/>'
-		+'<polygon points="22 31 20 31 21 10 21 10 22 31"/>'
-		+'</svg>'
+var stopwatch = '<svg id="stopwatch" viewBox="0 0 87.36 117.36">'
+				+'<image width="87" height="117" xlink:href="/assets/img/stopwatch.png"/>'
+				+'<polygon points="44.62 82.57 42.62 82.57 43.42 41.66 43.82 41.66 44.62 82.57"/>'
+				+'</svg>';
 var pagenext = {
 	  playback : [ 'library',  'playlist' ]
 	, playlist : [ 'playback', 'library' ]
