5f60cdedc14a1eab66637e915593a91ebb51db1a
rern <rernrern@gmail.com>
2021-01-30 09:37:11 +0700

    Update index-body.php

--- srv/http/index-body.php
+++ srv/http/index-body.php
@@ -72,7 +72,7 @@ foreach( $modes as $mode ) {
 			<div id="mode-'.$modeLC.'" class="mode" data-mode="'.$modeLC.'">
 				<a class="lipath">'.$mode.'</a>
 				<i class="fa fa-'.$modeLC.'"></i>
-				'.( $counts ? '<grl>'.number_format( $counts->$modeLC ).'</grl>' : '<grl></grl>' ).'
+				'.( $counts && $counts->$modeLC ? '<grl>'.number_format( $counts->$modeLC ).'</grl>' : '<grl></grl>' ).'
 				<a class="label">'.$mode.'</a>
 			</div>
 		</div>
