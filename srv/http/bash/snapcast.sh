9aacf1a84a52c03500b4ffefbdd3edeb25fb38a9
rern <rernrern@gmail.com>
2021-02-18 17:04:49 +0700

    Update snapcast.sh

--- srv/http/bash/snapcast.sh
+++ srv/http/bash/snapcast.sh
@@ -4,6 +4,10 @@ dirtmp=/srv/http/data/shm
 snapserverfile=$dirtmp/snapserverip
 snapclientfile=$dirtmp/snapclientip
 
+pushstream() {
+	curl -s -X POST http://$1/pub?id=snapcast -d "$2"
+}
+
 if [[ $1 == start ]]; then # client start - save server ip
 	mpc -q stop
 	systemctl start snapclient
@@ -13,21 +17,20 @@ if [[ $1 == start ]]; then # client start - save server ip
 		mv $dirtmp/player-{*,snapclient}
 		echo $serverip > $snapserverfile
 		clientip=$( ifconfig | awk '/inet .*broadcast/ {print $2}' )
-		curl -s -X POST http://$serverip/pub?id=snapcast -d '{ "add": "'$clientip'" }'
+		pushstream $serverip '{ "add": "'$clientip'" }'
 		systemctl try-restart shairport-sync spotifyd upmpdcli &> /dev/null
 	else
 		systemctl stop snapclient
 		echo -1
 	fi
 elif [[ $1 == stop ]]; then # client stop - delete server ip, curl remove client ip
-	echo stop
 	systemctl stop snapclient
 	mv $dirtmp/player-{*,mpd}
 	curl -s -X POST http://127.0.0.1/pub?id=mpdplayer -d "$( /srv/http/bash/status.sh )"
 	serverip=$( cat $snapserverfile )
 	clientip=$( ifconfig | awk '/inet .*broadcast/ {print $2}' )
 	rm $snapserverfile
-	curl -s -X POST http://$serverip/pub?id=snapcast -d '{ "remove": "'$clientip'" }'
+	pushstream $serverip '{ "remove": "'$clientip'" }'
 elif [[ $1 == add ]]; then # connected from client - save client ip
 	clientip=$2
 	! grep -q $clientip $snapclientfile 2> /dev/null && echo $clientip >> $snapclientfile
@@ -41,7 +44,7 @@ elif [[ $1 == serverstop ]]; then # force clients stop
 		if [[ -s $snapclientfile ]]; then
 			mapfile -t clientip < $snapclientfile
 			for ip in "${clientip[@]}"; do
-				curl -s -X POST http://$ip/pub?id=snapcast -d -1
+				pushstream $ip -1
 			done
 		fi
 		rm -f $snapclientfile
