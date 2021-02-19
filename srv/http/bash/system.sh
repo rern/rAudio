accc73c58355bc32f9e9540abf8873517df40a86
rern <rernrern@gmail.com>
2021-02-19 20:37:26 +0700

    Update system.sh

--- srv/http/bash/system.sh
+++ srv/http/bash/system.sh
@@ -178,7 +178,7 @@ datarestore )
 	
 	uuid1=$( head -1 /etc/fstab | cut -d' ' -f1 )
 	uuid2=${uuid1:0:-1}2
-	sed -i "s/root=.* rw/root=$uuid rw/; s/elevator=noop //" $dirconfig/boot/cmdline.txt
+	sed -i "s/root=.* rw/root=$uuid1 rw/; s/elevator=noop //" $dirconfig/boot/cmdline.txt
 	sed -i "s/^PARTUUID=.*-01  /$uuid1  /; s/^PARTUUID=.*-02  /$uuid2  /" $dirconfig/etc/fstab
 	
 	cp -rf $dirconfig/* /
