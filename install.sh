e8e2f741b33aba535512673e71ddd7293b960e0f
rern <rernrern@gmail.com>
2021-02-23 02:05:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ alias=r1
 
 . /srv/http/bash/addons.sh
 
-sed -i -e \"/id === 'infoRadio'/ {N;d}\" -e \"s/chk === ''/\\!chk/\" /srv/http/assets/js/info.js
+sed -i -e "/id === 'infoRadio'/ {N;d}" -e "s/chk === ''/\!chk/" /srv/http/assets/js/info.js
 sed -i '/^-.*pam_systemd_home/ s/^/#/' /etc/pam.d/system-auth
 sed -i '/IgnorePkg   = linux-firmware/ d' /etc/pacman.conf
 
