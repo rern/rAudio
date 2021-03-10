017931227c4d16f5d9ff2689b3459be33dda0e75
rern <rernrern@gmail.com>
2021-03-11 02:10:06 +0700

    u

--- srv/http/bash/status-radiofrance.sh
+++ srv/http/bash/status-radiofrance.sh
@@ -5,25 +5,25 @@
 
 name=$( echo $1 | sed 's|.*/\(.*\)\-.*|\1|' )
 case $name in
-	fipelectro ) id=74;;
-	fipgroove ) id=66;;
-	fip ) id=7;;
-	fipjazz ) id=65;;
+	fip )           id=7;;
+	fipelectro )    id=74;;
+	fipgroove )     id=66;;
+	fipjazz )       id=65;;
 	fipnouveautes ) id=70;;
-	fippop ) id=78;;
-	fipreggae ) id=71;;
-	fiprock ) id=64;;
-	fipworld ) id=69;;
-	francemusiquebaroque ) id=402;;
-	francemusiqueclassiqueplus ) id=408;;
+	fippop )        id=78;;
+	fipreggae )     id=71;;
+	fiprock )       id=64;;
+	fipworld )      id=69;;
+	francemusique )                    id=4;;
+	francemusiquebaroque )             id=408;;
+	francemusiqueclassiqueplus )       id=402;;
 	francemusiqueconcertsradiofrance ) id=403;;
-	francemusiqueeasyclassique ) id=401;;
-	francemusique ) id=4;;
-	francemusiquelabo ) id=407;;
-	francemusiquelacontemporaine ) id=406;;
-	francemusiquelajazz ) id=405;;
-	francemusiqueocoramonde ) id=404;;
-	francemusiqueopera ) id=409;;
+	francemusiqueeasyclassique )       id=401;;
+	francemusiquelabo )                id=407;;
+	francemusiquelacontemporaine )     id=406;;
+	francemusiquelajazz )              id=405;;
+	francemusiqueocoramonde )          id=404;;
+	francemusiqueopera )               id=409;;
 esac
 
 metadataGet() {
