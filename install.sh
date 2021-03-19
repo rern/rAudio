58a7c90044ce6387fffc7c8c02040c787f50a0e7
rern <rernrern@gmail.com>
2021-03-19 07:31:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 alias=r1
 
+if [[ -e /etc/systemd/system/smb.service.d ]]; then
+	systemctl -q is-active smb && smb=1
+	rm -rf /etc/systemd/system/smb.service.d
+	sed -i '/Requires=smb\|nobody/ d' /etc/systemd/system/wsdd.service
+	systemctl daemon-reload
+	[[ -n $smb ]] && systemctl restart smb wsdd
+fi
 . /srv/http/bash/addons.sh
 
 [[ -e /usr/lib/systemd/system/spotifyd.service ]] || ln -s /usr/lib/systemd/{user,system}/spotifyd.service
