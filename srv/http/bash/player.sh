76a7baebc02fc63ad0e9d86867b58307177b8dcd
rern <rernrern@gmail.com>
2021-03-25 12:06:01 +0700

    u

--- srv/http/bash/player.sh
+++ srv/http/bash/player.sh
@@ -9,7 +9,7 @@ pushstream() {
 	curl -s -X POST http://127.0.0.1/pub?id=$1 -d "$2"
 }
 pushRefresh() {
-	pushstream refresh '{ "page": "mpd" }'
+	pushstream refresh '{ "page": "player" }'
 }
 restartMPD() {
 	/srv/http/bash/mpd-conf.sh
