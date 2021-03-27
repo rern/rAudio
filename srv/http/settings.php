ba65f370526faf83756d9d4d0a6420dd44141c56
rern <rernrern@gmail.com>
2021-03-27 20:25:53 +0700

    Update settings.php

--- srv/http/settings.php
+++ srv/http/settings.php
@@ -83,7 +83,7 @@ foreach ( [ 'Features', 'MPD', 'Networks', 'System' ] as $name ) {
 <script src="/assets/js/settings.<?=$time?>.js"></script>
 	<?php if ( $page !== 'guide' ) { ?>
 <script src="/assets/js/<?=$page?>.<?=$time?>.js"></script>
-	<?php	if ( in_array( $page, [ 'features', 'mpd', 'system' ] ) ) { ?>
+	<?php	if ( in_array( $page, [ 'features', 'player', 'system' ] ) ) { ?>
 <script src="/assets/js/plugin/jquery.selectric.min.<?=$time?>.js"></script>
 	<?php	} else if ( $page === 'networks' ) { ?>
 <script src="/assets/js/plugin/qrcode.min.<?=$time?>.js"></script>
