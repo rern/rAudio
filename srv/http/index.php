1c8a24af844191581efb7328314de1a92b90e821
rern <rernrern@gmail.com>
2021-03-29 12:11:26 +0700

    Update index.php

--- srv/http/index.php
+++ srv/http/index.php
@@ -1,4 +1,5 @@
 <?php
+header("Set-Cookie: key=value; path=/; domain=fanart.tv; HttpOnly; SameSite=Lax");
 $login = file_exists( '/srv/http/data/system/login' );
 if ( $login ) session_start();
 $time = time();  // for cache busting
