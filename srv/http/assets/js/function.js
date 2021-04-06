254c9be0eea7057d4254af7383cf2541509c3a63
rern <rernrern@gmail.com>
2021-04-06 19:07:04 +0700

    Update function.js

--- srv/http/assets/js/function.js
+++ srv/http/assets/js/function.js
@@ -1644,13 +1644,9 @@ function volumePushstream() {
 	bash( [ 'volumepushstream' ] );
 }
 function volumeSpeed( vol ) {
-	if ( G.volumeanimate ) {
-		var diff = Math.abs( vol - G.status.volume );
-		if ( diff === 0 ) diff = Math.abs( G.status.volume - G.status.volumemute ); // mute/unmute
-		var sec = Math.ceil( diff / 5 ) * 0.2;
-	} else {
-		var sec = 0;
-	}
+	var diff = Math.abs( vol - G.status.volume );
+	if ( diff === 0 ) diff = Math.abs( G.status.volume - G.status.volumemute ); // mute/unmute
+	var sec = Math.ceil( diff / 5 ) * 0.2;
 	$( '#volume .rs-transition' ).css( 'transition-duration', sec +'s' );
 }
 function vu() {
