0b35804c6084ba29848649bf522be58da9de7b9e
rern <rernrern@gmail.com>
2021-04-16 19:49:52 +0700

    Update status.sh

--- srv/http/bash/status.sh
+++ srv/http/bash/status.sh
@@ -6,7 +6,7 @@ dirtmp=/srv/http/data/shm
 btclient=$( [[ -e $dirtmp/btclient ]] && echo true || echo false )
 consume=$( mpc | grep -q 'consume: on' && echo true || echo false )
 counts=$( cat /srv/http/data/mpd/counts 2> /dev/null || echo false )
-[[ -z $counts ]] && counts=false
+[[ -z $counts ]] && counts=false # fix - sometime blank on startup
 lcd=$( grep -q dtoverlay=tft35a /boot/config.txt 2> /dev/null && echo true || echo false )
 librandom=$( [[ -e $dirsystem/librandom ]] && echo true || echo false )
 player=$( ls $dirtmp/player-* 2> /dev/null | cut -d- -f2  )
