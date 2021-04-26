3232b5c145f922a9ec78416b0271c5a29e7abe00
rern <rernrern@gmail.com>
2021-04-26 17:04:30 +0700

    u

--- srv/http/assets/js/info.js
+++ srv/http/assets/js/info.js
@@ -588,7 +588,7 @@ function checkRequired() {
 	var $empty = $( '.infoinput.required' ).filter( function() {
 		return !$( this ).val();
 	} );
-	$( '#infoOk, .hg-button-enter' ).toggleClass( 'disabled', $empty.length > 0 );
+	$( '#infoOk' ).toggleClass( 'disabled', $empty.length > 0 );
 }
 function renderOption( $el, htm, chk ) {
 	$el.html( htm ).promise().done( function() {
