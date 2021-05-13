080d04856bdf6af11fb28c060ea43dd07e6f3d29
rern <rernrern@gmail.com>
2021-05-13 10:10:48 +0700

    Update status-coverart.sh

--- srv/http/bash/status-coverart.sh
+++ srv/http/bash/status-coverart.sh
@@ -2,56 +2,42 @@
 
 readarray -t args <<< "$1"
 
-[[ ${#args[@]} > 1 ]] && mpdpath=${args[2]} || mpdpath=${args[0]} # for mpdlibrary.php
-[[ ${mpdpath: -14:10} == .cue/track ]] && mpdpath=$( dirname "$mpdpath" )
-
-path="/mnt/MPD/$mpdpath"
-### 1 - coverfile in directory ##################################
-[[ -d $path ]] && dir=$path || dir=$( dirname "$path" )
-for name in cover folder front album; do
-	for ext in jpg png gif; do
-		coverfile="$dir/$name"
-		[[ -e "$coverfile.$ext" ]] && break 2
-		coverfile="$dir/${name^}" # capitalize
-		[[ -e "$coverfile.$ext" ]] && break 2
-	done
-	coverfile=
-done
-
 date=$( date +%s )
+covername=$( echo "$1" | head -2 | tr -d '\n "`?/#&'"'" ) # Artist Album file > ArtistAlbum
 
-if [[ -n $coverfile ]]; then
-	coverfile=$( php -r "echo rawurlencode( '${coverfile//\'/\\\'}' );" )
-	echo $coverfile.$date.$ext
-	exit
-fi
+### 1 - already fetched online-file #########################
+urlname=/data/shm/online-$covername
+coverfile=$( ls /srv/http$urlname.* 2> /dev/null )
+[[ -e $coverfile ]] && echo $urlname.$date.${coverfile/*.} && exit
 
-[[ ${#args[@]} == 1 ]] && exit
+urlname=/data/shm/licover-$covername
+coverfile=$( ls /srv/http$urlname.* 2> /dev/null )
+[[ -e $coverfile ]] && echo $urlname.$date.${coverfile/*.} && exit
 
 ### 2 - already extracted embedded-file #########################
-covername=$( echo "$1" | head -2 | tr -d '\n "`?/#&'"'" ) # Artist Album file > ArtistAlbum
-embeddedfile=/srv/http/data/embedded/$covername.jpg
-coverfile=/data/embedded/$covername.$date.jpg
-if [[ -e $embeddedfile ]]; then
-	echo $coverfile
-	exit
-fi
+urlname=/data/embedded/$covername
+[[ -e /srv/http$urlname.jpg ]] && echo $urlname.$date.jpg && exit
 
-### 3 - already fetched online-file #########################
-fetchedfile=$( ls /srv/http/data/shm/online-$covername.* 2> /dev/null )
-if [[ -e $fetchedfile ]]; then
-	echo /data/shm/online-$covername.$date.${fetchedfile/*.}
-	exit
-fi
+### 3 - coverfile in directory ##################################
+[[ ${#args[@]} > 1 ]] && mpdpath=${args[2]} || mpdpath=${args[0]} # for mpdlibrary.php
+[[ ${mpdpath: -14:10} == .cue/track ]] && mpdpath=$( dirname "$mpdpath" )
+path="/mnt/MPD/$mpdpath"
+[[ -d $path ]] && dir=$path || dir=$( dirname "$path" )
+coverfile=$( ls -1 "$dir" \
+				| grep -i '^cover\.\|^folder\.\|^front\.\|^album\.' \
+				| grep -i '.gif$\|.jpg$\|.png$' \
+				| head -1 )
+[[ -e $coverfile ]] && echo $dir/${coverfile%.*}.$date.${coverfile/*.} && exit
 
 ### 4 - embedded ################################################
+urlname=/data/embedded/$covername
+coverfile=/srv/http$urlname.jpg
 files=$( mpc ls "$mpdpath" 2> /dev/null )
 readarray -t files <<<"$files"
 for file in "${files[@]}"; do
 	file="/mnt/MPD/$file"
 	if [[ -f "$file" ]]; then
-		#ffmpeg -i "$file" $file &> /dev/null
-		kid3-cli -c "select \"$file\"" -c "get picture:$embeddedfile" &> /dev/null # suppress '1 space' stdout
-		[[ -e $embeddedfile ]] && echo $coverfile && exit
+		kid3-cli -c "select \"$file\"" -c "get picture:$coverfile" &> /dev/null # suppress '1 space' stdout
+		[[ -e $coverfile ]] && echo $urlname.$date.jpg && exit
 	fi
 done
