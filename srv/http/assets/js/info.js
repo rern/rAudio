e0f19e4acfc33be7a66d90cbfa58adb352d64fe5
rern <rernrern@gmail.com>
2021-05-25 12:56:25 +0700

    Update info.js

--- srv/http/assets/js/info.js
+++ srv/http/assets/js/info.js
@@ -173,6 +173,8 @@ $( '#infoContent' ).on( 'click', '.fa-eye', function() {
 } );
 
 function infoReset() {
+	console.log('infoReset');
+	console.log('sequence' in O)
 	var keep = 'arrowleft' in O || 'arrowright' in O || 'sequence' in O;
 	if ( !keep ) $( '#infoOverlay' ).addClass( 'hide' ).removeClass( 'noscroll' );
 	O.infoscroll = 0;
@@ -249,10 +251,7 @@ function info( json ) {
 			.html( 'oklabel' in O ? O.oklabel : 'OK' )
 			.css( 'background-color', O.okcolor || '' )
 			.removeClass( 'hide' );
-			if ( typeof O.ok === 'function' ) $( '#infoOk' ).click( function() {
-				$( '#infoOverlay' ).addClass( 'hide' ).removeClass( 'noscroll' );
-				O.ok
-			} );
+			if ( typeof O.ok === 'function' ) $( '#infoOk' ).click( O.ok );
 		if ( 'cancel' in O && O.cancel ) {
 			$( '#infoCancel' )
 				.html( 'cancellabel' in O ? O.cancellabel : 'Cancel' )
