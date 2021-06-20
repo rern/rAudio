96c04492eb1ad373743bf42dbd334d32f86467d7
rern <rernrern@gmail.com>
2021-06-20 14:46:02 +0700

    Update cmd-pushstatus.sh

--- srv/http/bash/cmd-pushstatus.sh
+++ srv/http/bash/cmd-pushstatus.sh
@@ -2,7 +2,9 @@
 
 status=$( /srv/http/bash/status.sh )
 
-if [[ $1 == lcdchar ]]; then
+curl -s -X POST http://127.0.0.1/pub?id=mpdplayer -d "$status"
+
+if [[ -e $dirsystem/lcdchar ]]; then
 	killall lcdchar.py &> /dev/null
 	readarray -t data <<< $( echo $status \
 								| jq -r '.Artist, .Title, .Album, .state, .Time, .elapsed, .timestamp' \
@@ -10,14 +12,6 @@ if [[ $1 == lcdchar ]]; then
 	/srv/http/bash/lcdchar.py "${data[@]}" &
 	exit
 fi
-
-curl -s -X POST http://127.0.0.1/pub?id=mpdplayer -d "$status"
-
-if [[ -e /srv/http/data/system/librandom ]]; then
-	/srv/http/bash/cmd-librandom.sh
-	sleep 1
-fi
-
 if [[ -e /srv/http/data/shm/snapclientip ]]; then
 	status=$( echo $status | sed 's/"player" :.*"single" : false , //' )
 	readarray -t clientip < /srv/http/data/shm/snapclientip
@@ -25,3 +19,4 @@ if [[ -e /srv/http/data/shm/snapclientip ]]; then
 		[[ -n $ip ]] && curl -s -X POST http://$ip/pub?id=mpdplayer -d "$status"
 	done
 fi
+[[ -e /srv/http/data/system/librandom ]] && /srv/http/bash/cmd-librandom.sh
