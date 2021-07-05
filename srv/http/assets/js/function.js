8717a155dda5108d7973fbded360fad272a4bdb0
rern <rernrern@gmail.com>
2021-07-05 12:04:05 +0700

    Update function.js

--- srv/http/assets/js/function.js
+++ srv/http/assets/js/function.js
@@ -357,7 +357,7 @@ function displaySave( keys ) {
 	} );
 	if ( vumeter !== G.display.vumeter ) {
 		banner( 'VU Meter', G.display.vumeter ? 'Enable ...' : 'Disable ...', 'playback' )
-		G.status.Album = ''; // force renderPlaybackCoverart()
+		G.status.Album += ' '; // force renderPlaybackCoverart()
 		bash( [ 'vumeter', G.display.vumeter ] );
 	}
 	$.post( cmdphp, { cmd: 'displayset', displayset : JSON.stringify( G.display ) } );
