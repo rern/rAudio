a95ddb07c1ad01a25418ca15828427e13d746e03
rern <rernrern@gmail.com>
2021-07-12 10:51:02 +0700

    Update vu.sh

--- srv/http/bash/vu.sh
+++ srv/http/bash/vu.sh
@@ -7,24 +7,10 @@ if [[ -e /srv/http/data/system/vuled ]]; then
 	for i in ${p[@]}; do
 		gpio export $i out
 	done
-	readarray -t off <<< "\
-${p[@]}
-${p[@]:1}
-${p[@]:2}
-${p[@]:3}
-${p[@]:4}
-${p[@]:5}
-${p[@]:6}
-"
-		readarray -t on <<< "\
-
-${p[@]:0:1}
-${p[@]:0:2}
-${p[@]:0:3}
-${p[@]:0:4}
-${p[@]:0:5}
-${p[@]:0:6}
-${p[@]}"
+	for i in {0..7}; do
+		off+=( "$( echo ${p[@]:$i} )" )
+		on+=( "$( echo ${p[@]:0:$i} )" )
+	done
 fi
 
 j=0
