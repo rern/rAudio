65a3d7bae774c720513cadf82a800498b12a0163
rern <rernrern@gmail.com>
2021-07-19 19:28:57 +0700

    Update lcdchar.py

--- srv/http/bash/lcdchar.py
+++ srv/http/bash/lcdchar.py
@@ -15,26 +15,21 @@ section = 'var'
 cols = int( config.get( section, 'cols' ) )
 charmap = config.get( section, 'charmap' )
 backlight = config.get( section, 'backlight' )
+rows = cols == 16 and 2 or 4
 
-if config.has_option( section, 'address' ):
+if config.has_option( section, 'address' ): # i2c
     address = int( config.get( section, 'address' ), 16 ) # base 16 string > integer ( can be hex or int )
     chip = config.get( section, 'chip' )
+    from RPLCD.i2c import CharLCD
+    lcd = CharLCD( chip, address )
+    lcd = CharLCD( cols=cols, rows=rows, charmap=charmap, address=address, i2c_expander=chip, auto_linebreaks=False )
 else:
-    address = ''
     pin_rs = int( config.get( section, 'pin_rs' ) )
     pin_rw = int( config.get( section, 'pin_rw' ) )
     pin_e = int( config.get( section, 'pin_e' ) )
     data = config.get( section, 'pins_data' ).split( ',' )
     data = map( int, data )
     pins_data = list( data )
-    
-rows = cols == 16 and 2 or 4
-
-if address: # i2c
-    from RPLCD.i2c import CharLCD
-    lcd = CharLCD( chip, address )
-    lcd = CharLCD( cols=cols, rows=rows, charmap=charmap, address=address, i2c_expander=chip, auto_linebreaks=False )
-else:
     from RPLCD.gpio import CharLCD
     from RPi import GPIO
     lcd = CharLCD( cols=cols, rows=rows, charmap=charmap, numbering_mode=GPIO.BOARD, pin_rs=pin_rs, pin_rw=pin_rw, pin_e=pin_e, pins_data=pins_data, auto_linebreaks=False )
