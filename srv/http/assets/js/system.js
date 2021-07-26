36325e3bc9d4929dcac0847893bed61764d8a6f8
rern <rernrern@gmail.com>
2021-07-26 21:13:11 +0700

    Update system.js

--- srv/http/assets/js/system.js
+++ srv/http/assets/js/system.js
@@ -466,7 +466,7 @@ var infolcdchar = heredoc( function() { /*
 		<td colspan="2"><input type="text" id="pins_data"></td>
 	</tr>
 	<tr><td></td>
-		<td colspan="2"><label><input id="backlight" type="checkbox">Backlight off <gr>(stop >60s)</gr></label></td>
+		<td colspan="2"><label><input id="backlight" type="checkbox">Sleep <gr>(60s)</gr></label></td>
 	</tr>
 	</table>
 */ } );
