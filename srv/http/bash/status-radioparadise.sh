52e70b4c2822fa38b1401ca791e3fd03b0287542
rern <rernrern@gmail.com>
2021-07-27 17:30:27 +0700

    Update status-radioparadise.sh

--- srv/http/bash/status-radioparadise.sh
+++ srv/http/bash/status-radioparadise.sh
@@ -67,7 +67,8 @@ $coverart" > $dirtmp/status
 	fi
 	/srv/http/bash/cmd.sh onlinefileslimit
 	# next fetch
-	[[ -n $time ]] && sleep $time || sleep 5
+	[[ -n $time ]] && sec=$(( time + 5 )) || sec=5 # add 5s delay
+	sleep $sec
 	metadataGet
 }
 
