33063602c0c3f58b7a402681f7aca97e87b4ac6e
rern <rernrern@gmail.com>
2021-08-04 07:40:14 +0700

    Add files via upload

--- srv/http/assets/js/function.js
+++ srv/http/assets/js/function.js
@@ -344,18 +344,17 @@ function displayPlayback() {
 //	$( '#coverTL' ).toggleClass( 'disabled', window.innerHeight < 461 || document.body.clientWidth < 614 );
 }
 function displaySave( keys ) {
-	var values = infoVal();
 	G.vumeter = G.display.vumeter;
-	var volumenone = G.display.volumenone;
+	var values = infoVal();
+	var display = JSON.parse( JSON.stringify( G.display ) );
 	keys.forEach( function( k, i ) {
-		G.display[ k ] = values[ i ];
+		display[ k ] = values[ i ];
 	} );
-	G.coverdefault = G.display.novu ? G.coverart : G.covervu;
+	G.coverdefault = display.novu ? G.coverart : G.covervu;
 	[ 'color', 'order', 'update', 'updating_db', 'volumenone' ].forEach( function( item ) {
-		delete G.display[ item ];
+		delete display[ item ];
 	} );
-	bash( [ 'displaysave', JSON.stringify( G.display ) ] );
-	G.display.volumenone = volumenone;
+	bash( [ 'displaysave', JSON.stringify( display ) ] );
 }
 /*function flag( iso ) { // from: https://stackoverflow.com/a/11119265
 	var iso0 = ( iso.toLowerCase().charCodeAt( 0 ) - 97 ) * -15;
