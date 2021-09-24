da7a64ddb4abc97d3c3314c9e046ddcc54029078
rern <rernrern@gmail.com>
2021-09-24 13:44:57 +0700

    Update status.sh

--- srv/http/bash/status.sh
+++ srv/http/bash/status.sh
@@ -138,10 +138,10 @@ fi
 vuMeter() {
 	[[ -e $dirsystem/vuled ]] && vuled=1
 	if [[ -n $vumeter || -n $vuled ]]; then
+		killall cava &> /dev/null
 		if [[ $state == play ]]; then
-			pgrep cava &> /dev/null || cava -p /etc/cava.conf | $dirbash/vu.sh &> /dev/null &
+			cava -p /etc/cava.conf | $dirbash/vu.sh &> /dev/null &
 		else
-			killall cava &> /dev/null
 			curl -s -X POST http://127.0.0.1/pub?id=vumeter -d '{"val":0}'
 			if [[ -n $vuled ]]; then
 				p=$( cat /srv/http/data/system/vuled.conf )
