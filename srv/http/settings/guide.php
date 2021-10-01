bc9075f4dca7cdd8313403984907967c746bfde3
rern <rernrern@gmail.com>
2021-10-01 09:53:38 +0700

    Update guide.php

--- srv/http/settings/guide.php
+++ srv/http/settings/guide.php
@@ -17,7 +17,7 @@ include 'logosvg.php';
 	<link rel="stylesheet" href="/assets/css/common.<?=$time?>.css">
 	<link rel="stylesheet" href="/assets/css/settings.<?=$time?>.css">
 </head>
-<body style="height: 100%">
+<body style="height: 100%; user-select: none;">
 <div class="head" style="top: 0">
 	<i class="page-icon fa fa-question-circle"></i><span class='title'>USER GUIDE</span>
 	<a href="/"><i id="close" class="fa fa-times"></i></a>
@@ -29,9 +29,8 @@ include 'logosvg.php';
 	<div id="playlist" class="btn btn-default"><i class="fa fa-playlist"></i><span>Playlist</span></div>
 	<div id="settings" class="btn btn-default"><i id="settings" class="fa fa-gear"></i></div>
 	<div class="prev-next"><i id="previous" class="fa fa-arrow-left"></i>&emsp;<i id="next" class="fa fa-arrow-right"></i></div>
-	<img src="/assets/img/guide/1.<?=$time?>.jpg">
+	<img id="image" src="/assets/img/guide/1.<?=$time?>.jpg">
 </div>
-<script src="/assets/js/plugin/jquery-3.6.0.min.js"></script>
 <script>
 var xstart, xend;
 var xswipe = 100;
@@ -41,36 +40,53 @@ var nsettings = 47;
 var ntotal = 58;
 var n = 1;
 
-$( '#count' ).text( n +' / '+ ntotal );
-$( '.btn' ).click( function() {
-	var page = {
-		  playback : 1
-		, library  : nlibrary
-		, playlist : nplaylist
-		, settings : nsettings
-	}
-	n = page[ this.id ]
-	renderPage( n );
+var $ = function( id ) { return document.getElementById( id ) }
+var btn = document.getElementsByClassName( 'btn' );
+var count = $( 'count' );
+var image = $( 'image' );
+var next = $( 'next' );
+var previous = $( 'previous' );
+
+count.textContent = n +' / '+ ntotal;
+Array.from( btn ).forEach( function( el ) {
+	el.addEventListener( 'click', function() {
+		var page = {
+			  playback : 1
+			, library  : nlibrary
+			, playlist : nplaylist
+			, settings : nsettings
+		}
+		n = page[ this.id ]
+		renderPage( n );
+	} );
 } );
-$( '#next' ).click( function() {
+next.addEventListener( 'click', function() {
 	n = n < ntotal ? n + 1 : 1;
 	renderPage( n );
 } );
-$( '#previous' ).click( function() {
+previous.addEventListener( 'click', function() {
 	n = n > 1 ? n - 1 : ntotal;
 	renderPage( n );
 } );
-$( 'body' ).on( 'touchstart mousedown', function( e ) {
-	xstart = e.pageX || e.originalEvent.touches[ 0 ].pageX;
-} ).on( 'touchend mouseup', function( e ) {
-	xend = e.pageX || e.originalEvent.touches[ 0 ].pageX;
-	var xdiff = xstart - xend;
-	if ( Math.abs( xdiff ) > xswipe ) xdiff > 0 ? $( '#next' ).click() : $( '#previous' ).click();
+[ 'touchstart', 'mousedown' ].forEach( function( ev ) {
+	document.addEventListener( ev, function( e ) {
+		xstart = e.pageX || e.originalEvent.touches[ 0 ].pageX;
+	} );
 } );
+[ 'touchend', 'mouseup' ].forEach( function( ev ) {
+	document.addEventListener( ev, function( e ) {
+		xend = e.pageX || e.originalEvent.touches[ 0 ].pageX;
+		var xdiff = xstart - xend;
+		if ( Math.abs( xdiff ) > xswipe ) xdiff > 0 ? next.click() : previous.click();
+	} );
+} );
+
 function renderPage( n ) {
-	$( 'img' ).attr( 'src', '/assets/img/guide/'+ n +'.<?=$time?>.jpg' );
-	$( '#count' ).text( n +' / '+ ntotal );
-	$( '.btn' ).removeClass( 'active' );
+	image.src = '/assets/img/guide/'+ n +'.<?=$time?>.jpg';
+	count.textContent = n +' / '+ ntotal;
+	Array.from( btn ).forEach( function( el ) {
+		el.classList.remove( 'active' );
+	} );
 	if ( n >= 1 && n < nlibrary ) {
 		var id = 'playback';
 	} else if ( n >= nlibrary && n < nplaylist ) {
@@ -80,7 +96,7 @@ function renderPage( n ) {
 	} else {
 		var id = 'settings';
 	}
-	$( '#'+ id ).addClass( 'active' );
+	$( id ).classList.add( 'active' );
 }
 </script>
 </body>
