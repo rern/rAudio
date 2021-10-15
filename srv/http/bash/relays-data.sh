91113b37ce54f4071b49f69b24e8c59a97da8935
rern <rernrern@gmail.com>
2021-10-15 13:54:35 +0700

    Add files via upload

--- srv/http/bash/relays-data.sh
+++ srv/http/bash/relays-data.sh
@@ -2,9 +2,8 @@
 
 dirsystem=/srv/http/data/system
 file=$dirsystem/relays.conf
-if [[ -e $file ]]; then
-	. $file
-	data='
+. $file
+data='
   "page"    : "relays"
 , "pin"     : '$pin'
 , "name"    : '$name'
@@ -14,17 +13,5 @@ if [[ -e $file ]]; then
 , "offd"    : [ '$( echo ${offd[@]} | tr ' ' , )' ]
 , "timer"   : '$timer'
 , "enabled" : '$( [[ -e $dirsystem/relays ]] && echo true || echo false )
-else
-	data='
-  "page"    : "relays"
-, "pin"     : [ 11, 13, 15, 16 ]
-, "name"    : [ "DAC", "PreAmp", "Amp", "Subwoofer" ]
-, "on"      : [ 11, 13, 15, 16 ]
-, "off"     : [ 16, 15, 13, 11 ]
-, "ond"     : [ 2, 2, 2 ]
-, "offd"    : [ 2, 2, 2 ]
-, "timer"   : 5
-, "enabled" : false'
-fi
 
 echo "{$data}"
