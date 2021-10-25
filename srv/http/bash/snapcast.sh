88478f1c2f090c4ba8d0a01c3df111b32b3cb982
rern <rernrern@gmail.com>
2021-10-25 09:08:41 +0700

    Update snapcast.sh

--- srv/http/bash/snapcast.sh
+++ srv/http/bash/snapcast.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-dirtmp=/srv/http/data/shm
-snapserverfile=$dirtmp/snapserverip
-snapclientfile=$dirtmp/snapclientip
+dirshm=/srv/http/data/shm
+snapserverfile=$dirshm/snapserverip
+snapclientfile=$dirshm/snapclientip
 snapclientpwfile=/srv/http/data/system/snapclientpw
 
 pushstream() {
@@ -15,7 +15,7 @@ if [[ $1 == start ]]; then # client start - save server ip
 	sleep 2
 	serverip=$( journalctl -u snapclient | grep 'Connected to' | tail -1 | awk '{print $NF}' )
 	if [[ -n $serverip ]]; then
-		mv $dirtmp/player-{*,snapclient}
+		mv $dirshm/player-{*,snapclient}
 		echo $serverip > $snapserverfile
 		clientip=$( ifconfig | awk '/inet .*broadcast/ {print $2}' )
 		snapserverpw=$( cat $snapclientpwfile 2> /dev/null || echo ros )
@@ -27,7 +27,7 @@ if [[ $1 == start ]]; then # client start - save server ip
 	fi
 elif [[ $1 == stop ]]; then # client stop - delete server ip, curl remove client ip
 	systemctl stop snapclient
-	mv $dirtmp/player-{*,mpd}
+	mv $dirshm/player-{*,mpd}
 	curl -s -X POST http://127.0.0.1/pub?id=mpdplayer -d "$( /srv/http/bash/status.sh )"
 	serverip=$( cat $snapserverfile )
 	clientip=$( ifconfig | awk '/inet .*broadcast/ {print $2}' )
@@ -35,7 +35,7 @@ elif [[ $1 == stop ]]; then # client stop - delete server ip, curl remove client
 	sshpass -p "$snapserverpw" ssh -q root@$serverip "/srv/http/bash/snapcast.sh $clientip"
 	rm $snapserverfile
 elif [[ $1 == serverstop ]]; then # force clients stop
-	snapclientfile=$dirtmp/snapclientip
+	snapclientfile=$dirshm/snapclientip
 	if [[ -e $snapclientfile ]]; then
 		readarray -t clientip < $snapclientfile
 		for ip in "${clientip[@]}"; do
@@ -46,7 +46,7 @@ elif [[ $1 == serverstop ]]; then # force clients stop
 else # sshpass from snapclient
 	snapclientip=$1
 	snapserverip=$2
-	snapclientfile=$dirtmp/snapclientip
+	snapclientfile=$dirshm/snapclientip
 	sed -i "/$snapclientip/ d" $snapclientfile &> /dev/null
 	[[ -s $snapclientfile ]] || rm $snapclientfile
 	if [[ -n $snapserverip ]]; then
