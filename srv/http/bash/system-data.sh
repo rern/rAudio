a0cf1cc2d7404078e20633aa1b7561685b66c983
rern <rernrern@gmail.com>
2021-11-22 16:19:03 +0700

    Update system-data.sh

--- srv/http/bash/system-data.sh
+++ srv/http/bash/system-data.sh
@@ -117,7 +117,7 @@ if [[ -e $dirsystem/lcdchar.conf ]]; then
 	vals=$( cat $dirsystem/lcdchar.conf \
 				| grep -v '\[var]' \
 				| sed -e '/charmap\|inf\|chip/ s/.*=\(.*\)/"\1"/; s/.*=//' \
-					  -e 's/[][]//g; s/,/ /g; s/\(True\|False\)/\L\1/' )
+					  -e 's/[][]//g; s/,/ /g; s/\(True\|False\)/\l\1/' )
 	if grep -q i2c <<< "$vals"; then
 		vals=$( echo $vals | sed 's/\(true\|false\)$/15 18 16 21 22 23 24 \1/' )
 	else
