6af93f1f3cae79feabf805007be6ed944273e97f
rern <rernrern@gmail.com>
2021-11-26 11:34:13 +0700

    Update features.js

--- srv/http/assets/js/features.js
+++ srv/http/assets/js/features.js
@@ -186,7 +186,7 @@ $( '#setting-localbrowser' ).click( function() {
 				var up = $( this ).hasClass( 'up' );
 				var zoom = +$( '#zoom' ).val();
 				if ( ( up && zoom < 300 ) || ( !up && zoom > 50 ) ) $( '#zoom' ).val( up ? zoom += 10 : zoom -= 10 );
-				checkChanged();
+				$( '#infoOk' ).toggleClass( 'disabled', O.values.join( '' ) === infoVal().join( '' ) );
 			} );
 			$( '#infoContent' ).on( 'change', '#screenoff', function() {
 				if ( $( this ).val() != 0 ) {
