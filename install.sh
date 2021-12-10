e3fb3d92167fce07ec5f5adafd22e0a10dcae9b8
rern <rernrern@gmail.com>
2021-12-10 18:48:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,9 @@ dirsystem=/srv/http/data/system
 
 #20211210
 revision=$( awk '/Revision/ {print $NF}' /proc/cpuinfo )
-[[ ${revision: -3:2} == 12 ]] && echo dtparam=krnbt=on >> /boot/config.txt
+if [[ ${revision: -3:2} == 12 ]]; then
+	grep -q dtparam=krnbt=on /boot/config.txt || echo dtparam=krnbt=on >> /boot/config.txt
+fi
 
 file=/etc/samba/smb.conf
 if [[ -e $file ]] && ! grep -q 'force user' $file; then
