1163bbf8cadf37e2b0c25deac4dc6e13e14acb68
rern <rernrern@gmail.com>
2022-01-07 09:34:38 +0700

    Update system.js

--- srv/http/assets/js/system.js
+++ srv/http/assets/js/system.js
@@ -1,6 +1,6 @@
 $( function() { // document ready start >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-var gpiosvg = $( '#gpiosvg' ).html().replace( 'width="380px', 'width="330px' );;
+var gpiosvg = $( '#gpiosvg' ).html().replace( 'width="380px', 'width="330px' );
 var pin2gpio = {
 	   3:2,   5:3,   7:4,   8:14, 10:15, 11:17, 12:18, 13:27, 15:22, 16:23, 18:24, 19:10, 21:9
 	, 22:25, 23:11, 24:8,  26:7,  29:5,  31:6,  32:12, 33:13, 35:19, 36:16, 37:26, 38:20, 40:21
