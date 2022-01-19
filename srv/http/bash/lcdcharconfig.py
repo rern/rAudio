6ab378a3dd52b46c89c6c24eeaff9bbbbad6e3a0
rern <rernrern@gmail.com>
2022-01-19 07:52:03 +0700

    Add files via upload

--- srv/http/bash/lcdcharconfig.py
+++ srv/http/bash/lcdcharconfig.py
@@ -5,21 +5,21 @@ import configparser
 config = configparser.ConfigParser()
 config.read( '/srv/http/data/system/lcdchar.conf' )
 section = 'var'
-cols = int( config.get( section, 'cols' ) )
+cols = config.getint( section, 'cols' )
 charmap = config.get( section, 'charmap' )
-backlight = config.get( section, 'backlight' )
+backlight = config.getboolean( section, 'backlight' )
 rows = cols == 16 and 2 or 4
 
 if config.has_option( section, 'address' ): # i2c
-    address = int( config.get( section, 'address' ) )
+    address = config.getint( section, 'address' )
     chip = config.get( section, 'chip' )
     from RPLCD.i2c import CharLCD
     lcd = CharLCD( cols=cols, rows=rows, charmap=charmap, address=address, i2c_expander=chip )
 else:
     import json
-    pin_rs = int( config.get( section, 'pin_rs' ) )
-    pin_rw = int( config.get( section, 'pin_rw' ) )
-    pin_e = int( config.get( section, 'pin_e' ) )
+    pin_rs = config.getint( section, 'pin_rs' )
+    pin_rw = config.getint( section, 'pin_rw' )
+    pin_e = config.getint( section, 'pin_e' )
     pins_data = json.loads( config.get( section, 'pins_data' ) )
     from RPLCD.gpio import CharLCD
     from RPi import GPIO
