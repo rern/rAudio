6082f4dfbb7d6d52e624e7c2d8b65b71b8e4cd9a
rern <rernrern@gmail.com>
2022-02-06 11:55:26 +0700

    Update cmd.sh

--- srv/http/bash/cmd.sh
+++ srv/http/bash/cmd.sh
@@ -269,6 +269,32 @@ webradioCount() {
 	pushstream webradio $count
 	sed -i 's/\("webradio": \).*/\1'$count'/' $dirmpd/counts
 }
+webRadioSampling() {
+	url=$1
+	file=$2
+	timeout 3 wget -q $url -O /tmp/webradio
+	if [[ ! -s /tmp/webradio ]]; then
+		pushstreamNotify WebRadio "URL cannot be streamed:<br>$url" warning 8000
+		exit
+	fi
+	
+	data=( $( ffprobe -v quiet -select_streams a:0 \
+				-show_entries stream=sample_rate \
+				-show_entries format=bit_rate \
+				-of default=noprint_wrappers=1:nokey=1 \
+				/tmp/webradio ) )
+	if [[ ! $data ]]; then
+		pushstreamNotify WebRadio "URL contains no stream data:<br>$url" webradio 8000
+		exit
+	fi
+	
+	samplerate=${data[0]}
+	bitrate=${data[1]}
+	sample="$( awk "BEGIN { printf \"%.1f\n\", $samplerate / 1000 }" ) kHz"
+	rate="$(( bitrate / 1000 )) kbit/s"
+	sed -i "2 s|.*|$sample $rate|" "$file"
+	rm /tmp/webradio
+}
 
 case ${args[0]} in
 
@@ -1125,20 +1151,7 @@ $name
 $charset" > "$file"
 	chown http:http "$file" # for edit in php
 	webradioCount
-	(
-		timeout 3 wget -q $url -O /tmp/webradio
-		data=( $( ffprobe -v quiet -select_streams a:0 \
-					-show_entries stream=sample_rate \
-					-show_entries format=bit_rate \
-					-of default=noprint_wrappers=1:nokey=1 \
-					/tmp/webradio ) )
-		samplerate=${data[0]}
-		bitrate=${data[1]}
-		sample="$( awk "BEGIN { printf \"%.1f\n\", $samplerate / 1000 }" ) kHz"
-		rate="$(( bitrate / 1000 )) kbit/s"
-		sed -i "2 s|.*|$sample $rate|" "$file"
-		rm /tmp/webradio
-	) &
+	webRadioSampling $url "$file" &
 	;;
 webradiocoverreset )
 	coverart=${args[1]}
@@ -1161,20 +1174,20 @@ webradioedit )
 	dir=${args[4]}
 	url=${args[5]}
 	urlname=${url//\//|}
-	urlnamenew=${urlnew//\//|}
 	[[ $dir ]] && dir="$dir/"
 	fileprev="$dirwebradios/$dir$urlname"
-	filenew="$dirwebradios/$dir$urlnamenew"
-	if [[ $url != $urlnew ]]; then
-		mv "$fileprev" "$filenew"
-		mv ${dirwebradios}img/{$urlname,$urlnamenew}.jpg 
-		mv ${dirwebradios}img/{$urlname,$urlnamenew}-thumb.jpg 
-	fi
-	sed -
 	echo "\
 $name
-$( sed -n 2p "$filenew" )
-$charset" > "$filenew"
+$( sed -n 2p "$fileprev" )
+$charset" > "$fileprev"
+	if [[ $url != $urlnew ]]; then
+		urlnamenew=${urlnew//\//|}
+		filenew="$dirwebradios/$dir$urlnamenew"
+		mv "$fileprev" "$filenew"
+		mv ${dirwebradios}img/{$urlname,$urlnamenew}.jpg
+		mv ${dirwebradios}img/{$urlname,$urlnamenew}-thumb.jpg
+		webRadioSampling $urlnew "$filenew" &
+	fi
 	pushstream webradio -1
 	;;
 wrdirdelete )
