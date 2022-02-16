cbdc7b94763c84e05de3bcc540911d5df9891330
rern <rernrern@gmail.com>
2022-02-16 21:12:26 +0700

    u

--- install.sh
+++ install.sh
@@ -2,23 +2,19 @@
 
 alias=r1
 
-dirbash=/srv/http/bash
-dirshm=/srv/http/data/shm
-dirsystem=/srv/http/data/system
-
 # 20220211
 [[ -e /boot/kernel.img ]] && echo 'Server = http://alaa.ad24.cz/repos/2022/02/06/$arch/$repo' > /etc/pacman.d/mirrorlist
-sed -i '/latency/ d' $dirsystem/soundprofile.conf &> /dev/null
+sed -i '/latency/ d' /srv/http/data/system/soundprofile.conf &> /dev/null
 
 # 20220204
-if ! grep -q 'assets,bash' $dirbash/addons.sh; then
+if ! grep -q 'assets,bash' /srv/http/bash/addons.sh; then
 	sed -i -e '/chown/ d
 ' -e '/chmod 755 .srv/ i\
 	chown -R http:http /srv/http/{assets,bash,settings}
-' $dirbash/addons.sh
+' /srv/http/bash/addons.sh
 fi
 
-. $dirbash/addons.sh
+. /srv/http/bash/addons.sh
 
 installstart "$1"
 
