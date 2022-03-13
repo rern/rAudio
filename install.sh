821ac3d8eea8b39d0ad97d56cdaae2a39a2b8e38
rern <rernrern@gmail.com>
2022-03-13 07:23:54 +0700

    u

--- install.sh
+++ install.sh
@@ -10,19 +10,21 @@ if ! ls $dirplaylists/*.m3u &> /dev/null; then
 	for plfile in "${plfile[@]}"; do
 		basename "$plfile"
 		list=$( grep '"file":' "$plfile" | sed 's/^\s*"file": "//; s/",$//; s/\\//g' )
-		readarray -t file_track <<< $( grep -B1 -A5 '"Range":' "$plfile" \
-										| grep '"file":\|"Track":' \
-										| sed 's/^\s*"file": "\|^\s*"Track": //; s/",$\|,$//; s/\\//g' )
-		iL=${#file_track[@]}
-		for (( i=0; i < iL; i++ )); do
-			track=000${file_track[$(( i + 1 ))]}
-			file=${file_track[i]}
-			filecue="${file%.*}.cue/track${track: -4}"
-			list=$( sed "s|$file|$filecue|" <<< "$list" )
-			(( i++ ))
-		done
+		if grep -q '^\s*"Range": ' "$plfile"; then
+			readarray -t file_track <<< $( grep -B1 -A5 '"Range":' "$plfile" \
+											| grep '"file":\|"Track":' \
+											| sed 's/^\s*"file": "\|^\s*"Track": //; s/",$\|,$//; s/\\//g' )
+			iL=${#file_track[@]}
+			for (( i=0; i < iL; i++ )); do
+				track=000${file_track[$(( i + 1 ))]}
+				file=${file_track[i]}
+				filecue="${file%.*}.cue/track${track: -4}"
+				list=$( sed "s|$file|$filecue|" <<< "$list" )
+				(( i++ ))
+			done
+		fi
 		echo "$list" > "$plfile.m3u"
-	    rm "$plfile"
+		rm "$plfile"
 	done
 	chown mpd:audio $dirplaylists
 	sed -i "s|/var/lib/mpd/playlists|$dirplaylists|" /etc/mpd.conf
--- srv/http/bash/system.sh
+++ srv/http/bash/system.sh
@@ -229,7 +229,8 @@ datarestore )
 			done
 		fi
 	fi
-	# temp ###
+	
+	# temp 20220312 ###
 	dirplaylists=$dirdata/playlists
 	if ! ls $dirplaylists/*.m3u &> /dev/null; then
 		echo -e "\n\e[38;5;6m\e[48;5;6m . \e[0m Convert saved playlists ..."
@@ -237,27 +238,28 @@ datarestore )
 		for plfile in "${plfile[@]}"; do
 			basename "$plfile"
 			list=$( grep '"file":' "$plfile" | sed 's/^\s*"file": "//; s/",$//; s/\\//g' )
-			readarray -t file_track <<< $( grep -B1 -A5 '"Range":' "$plfile" \
-											| grep '"file":\|"Track":' \
-											| sed 's/^\s*"file": "\|^\s*"Track": //; s/",$\|,$//; s/\\//g' )
-			iL=${#file_track[@]}
-			for (( i=0; i < iL; i++ )); do
-				track=000${file_track[$(( i + 1 ))]}
-				file=${file_track[i]}
-				filecue="${file%.*}.cue/track${track: -4}"
-				list=$( sed "s|$file|$filecue|" <<< "$list" )
-				(( i++ ))
-			done
+			if grep -q '^\s*"Range": ' "$plfile"; then
+				readarray -t file_track <<< $( grep -B1 -A5 '"Range":' "$plfile" \
+												| grep '"file":\|"Track":' \
+												| sed 's/^\s*"file": "\|^\s*"Track": //; s/",$\|,$//; s/\\//g' )
+				iL=${#file_track[@]}
+				for (( i=0; i < iL; i++ )); do
+					track=000${file_track[$(( i + 1 ))]}
+					file=${file_track[i]}
+					filecue="${file%.*}.cue/track${track: -4}"
+					list=$( sed "s|$file|$filecue|" <<< "$list" )
+					(( i++ ))
+				done
+			fi
 			echo "$list" > "$plfile.m3u"
 		    rm "$plfile"
 		done
 		chown mpd:audio $dirplaylists
 	fi
-	# temp ###
+	# temp 20220312 ###
+	
 	chown -R http:http /srv/http/{assets,bash,settings}
 	chmod 755 /srv/http/* $dirbash/* /srv/http/settings/*
-	chmod -R 777 $dirdata/playlists
-	chown mod:audio $dirdata/playlists
 	[[ -e $dirsystem/color ]] && $dirbash/cmd.sh color
 	$dirbash/cmd.sh power$'\n'reboot
 	;;
