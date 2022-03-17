3fc44be48642007ac6ceb876c3eb8f72e5b89450
rern <rernrern@gmail.com>
2022-03-17 13:41:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ alias=r1
 
 # 20220312
 file=/srv/http/data/system/display
-grep -q latest $file || sed -i '/playlists/ a\  "latest": true,' $file
+if ! grep -q latest $file; then
+	sed -i '/playlists/ a\
+  "latest": true,' $file
+fi
 
 dirplaylists=/srv/http/data/playlists
 readarray -t plfiles <<< $( ls -I '*.*' $dirplaylists )
