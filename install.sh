7ee47e04b6ce9bae035982dc3fd6461bda4c80ae
rern <rernrern@gmail.com>
2022-03-17 13:51:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,6 @@
 
 alias=r1
 
-# 20220312
-file=/srv/http/data/system/display
-if ! grep -q latest $file; then
-	sed -i '/playlists/ a\
-  "latest": true,
-' $file
-fi
-
 . /srv/http/bash/addons.sh
 
 installstart "$1"
