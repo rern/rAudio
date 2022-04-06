ee78c09307a452cbb1ecc433045aa06bf60fdff0
rern <rernrern@gmail.com>
2022-04-06 08:21:09 +0700

    u

--- srv/http/bash/camilladsp.sh
+++ srv/http/bash/camilladsp.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 
-ccv=$( /srv/http/bash/cmd.sh volumecontrolget )
-card=${ccv/^*}
-control=$( echo $ccv | cut -d^ -f2 ) # keep trailing space if any
-volume=${ccv/*^}
-[[ ! $control ]] && exit
-
-amixer -c $card -Mq sset "$control" 0% # mute
-
-cardl=$( aplay -l \
-			| grep 'Loopback.*device 0' \
-			| sed 's/^card \(.\):.*$/\1/' )
-aplay -D hw:$cardl,0 /srv/http/assets/sound/3seconds.wav  &> /dev/null &
-sleep 2
-format=$( grep -r ^format: /proc/asound/card$cardl/pcm*p \
-			| awk '{print $NF}' \
-			| tr -d _ )
-			
-amixer -c $card -Mq sset "$control" $volume% # unmute
-
-[[ ! $format ]] && exit
+camillaRestart() {
+	systemctl start camilladsp
+	sleep 2
+	systemctl -q is-active camilladsp && exit
+}
 
 fileyml=/srv/http/data/camilladsp/configs/camilladsp.yml
 linedevice=$( sed -n '/playback:/,/device:/=' $fileyml | tail -1 )
-lineformat=$( sed -n '/playback:/,/format:/=' $fileyml | tail -1 )
-sed -i -e "$linedevice s/\(device: \).*/\1hw:$card,0/
-" -e "$lineformat s/\(format: \).*/\1$format/
-" $fileyml
+card=$( cat /srv/http/data/shm/asoundcard )
+sed -i "$linedevice s/\(device: hw:\).*/\1$card,0/" $fileyml
 
-systemctl restart camilladsp
+camillaRestart
+
+lineformat=$( sed -n '/playback:/,/format:/=' $fileyml | tail -1 )
+for format in S16LE S24LE S24LE3 S32LE FLOAT32LE FLOAT64LE; do
+	sed -i "$lineformat s/\(format: \).*/\1$format/" $fileyml
+	camillaRestart
+done
