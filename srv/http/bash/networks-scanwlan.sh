93c699133a8ce71f591c8d1f52cc28b6c8e1144b
rern <rernrern@gmail.com>
2022-04-11 14:40:33 +0700

    Update networks-scanwlan.sh

--- srv/http/bash/networks-scanwlan.sh
+++ srv/http/bash/networks-scanwlan.sh
@@ -5,54 +5,36 @@
 wlandev=$( cat $dirshm/wlan )
 ifconfig $wlandev up
 
-listProfile() {
-	netctllist=$( netctl list | grep -v eth | sed 's/^\s*\**\s*//' )
-	i=0
-	if grep -q '^+' <<<"$netctllist"; then # leading '+' = connecting
-		(( i++ ))
-		(( i == 15 )) && echo -1 && exit
-		
-		sleep 2
-		listProfile
-	fi
-}
-listProfile
-
-if [[ $netctllist ]]; then
-	readarray -t netctllist_ar <<<"$netctllist"
-	# pre-scan saved profile to force display hidden ssid
-	for name in "${netctllist_ar[@]}"; do
-		grep -q '^Hidden=yes' "/etc/netctl/$name" && iwlist $wlandev scan essid "$name" &> /dev/null
+# pre-scan hidden ssid to force responding to scan
+readarray -t hiddenprofiles <<< $( grep -rl --exclude-dir=examples ^Hidden=yes /etc/netctl )
+if [[ $hiddenprofiles ]]; then
+	for file in "${hiddenprofiles[@]}"; do
+		iwlist $wlandev scan essid "$( basename "$file" )" &> /dev/null
 	done
 fi
 
-connectedssid=$( iwgetid $wlandev -r )
-
-readarray -t lines <<< $( iwlist $wlandev scan \
-							| grep -E '^\s*Quality|^\s*Encryption|^\s*ESSID|WPA |WPA2' \
-							| sed 's/^\s*Quality.*level\| dBm *$\|^\s*Encryption.*:\|^\s*ESSID.*:\|\\x00//g; s/IE: .*\(WPA.*\) .* .*/\1/' \
-							| sed 's/^"\|"$//g' \
-							| tr '\n' '^' \
-							| sed 's/=/\n/g' \
-							| awk NF \
-							| sort -V )
-for line in "${lines[@]}"; do
-	line=$( echo $line | tr '^' '\n' )
-	readarray -t val <<< "$line"
-	ssid=${val[2]}
-	[[ ! $ssid ]] && continue
-	
-	dbm=${val[0]}
-	encrypt=${val[1]}
-	[[ ! ${val[3]} ]] && echo true
-	data+=',{
-  "dbm"       : "'$dbm'"
-, "ssid"      : "'$ssid'"
-, "encrypt"   : "'$encrypt'"
-, "profile"   : '$( [[ -e "/etc/netctl/$ssid" ]] && echo true )'
-, "connected" : '$( [[ $ssid == $connectedssid ]] && echo true )'
-, "wep"       : '$wep'
-}'
+# ESSID:"NAME"
+# Encryption key:on
+# Quality=37/70  Signal level=-73 dBm --- Quality=0/100  Signal level=25/100
+# IE: IEEE 802.11i/WPA2 Version 1
+# IE: WPA Version 1
+scan=$( iwlist $wlandev scan \
+							| sed 's/^\s*\|\s*$//g' \
+							| grep -E '^Cell|^ESSID|^Encryption|^IE.*WPA|^Quality' \
+							| sed -e 's/^Cell.*/,{/
+									  s/^ESSID:/ "ssid":/
+									  s/^Encryption key:\(.*\)/,"encrypt":"\1"/
+									  s/^IE.*WPA.*/,"wpa":true/
+									  s/^Quality.*level.\(.*\)/,"signal":"\1"\n}/' )
+# save profile
+readarray -t ssids <<< $( grep '"ssid":' <<< "$scan" \
+			| sed 's/^.*:"\(.*\)"/\1/' \
+			| grep . )
+for ssid in "${ssids[@]}"; do
+	[[ -e "/etc/netctl/$ssid" ]] && scan=$( sed '/"ssid":"'$ssid'"/ a\,"profile":true' <<< "$scan" )
 done
+# connected ssid
+connectedssid=$( iwgetid $wlandev -r )
+scan=$( sed '/"ssid":"'$connectedssid'"/ a\,"connected":true' <<< "$scan" )
 
-data2json "$data"
+echo "[ ${scan:1} ]" | jq
