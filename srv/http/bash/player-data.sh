bafdf80796eebb3315a6bdfd79a075662222d8cf
rern <rernrern@gmail.com>
2022-04-17 15:42:49 +0700

    Update player-data.sh

--- srv/http/bash/player-data.sh
+++ srv/http/bash/player-data.sh
@@ -19,7 +19,7 @@ data='
 , "audioaplayname"   : "'$aplayname'"
 , "audiooutput"      : "'$output'"
 , "autoupdate"       : '$( grep -q '^auto_update.*yes' /etc/mpd.conf && echo true )'
-, "btaplayname"      : "'$( amixer -D bluealsa scontrols 2> /dev/null | cut -d"'" -f2 )'"
+, "btaplayname"      : "'$( cat $dirshm/btclient )'"
 , "buffer"           : '$( grep -q '^audio_buffer_size' /etc/mpd.conf && echo true )'
 , "bufferconf"       : '$( cat $dirsystem/buffer.conf 2> /dev/null || echo 4096 )'
 , "bufferoutput"     : '$( grep -q '^max_output_buffer_size' /etc/mpd.conf && echo true )'
