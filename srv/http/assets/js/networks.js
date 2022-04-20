457c817c69a08ff687e32cde72b089ce146f7aab
rern <rernrern@gmail.com>
2022-04-20 18:19:17 +0700

    Update networks.js

--- srv/http/assets/js/networks.js
+++ srv/http/assets/js/networks.js
@@ -414,7 +414,8 @@ function renderBluetooth() {
 				G.btconnected = list.name;
 				$( '#divbt heading' ).addClass( 'status' );
 			}
-			htmlbt += '<li class="bt" data-name="'+ list.name +'" data-sink="'+ list.sink +'" data-connected="'+ list.connected +'"><i class="fa fa-'+ ( list.sink ? 'btclient' : 'bluetooth' ) +'"></i>';
+			htmlbt += '<li class="bt" data-name="'+ list.name +'" data-sink="'+ list.sink +'" data-connected="'+ list.connected +'">'
+					 +'<i class="fa fa-'+ ( list.sink ? 'btclient' : 'bluetooth' ) +'"></i>';
 			htmlbt += list.connected ? '<grn>•</grn>&ensp;' : '<gr>•</gr>&ensp;'
 			htmlbt += list.name +'</li>';
 		} );
