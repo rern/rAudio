9172f55cfc764e561c40568c5d825ec24dd5d7e4
rern <rernrern@gmail.com>
2022-05-02 11:01:51 +0700

    Update networks.php

--- srv/http/settings/networks.php
+++ srv/http/settings/networks.php
@@ -8,21 +8,22 @@ htmlHead( [ //////////////////////////////////
 ?>
 	<ul id="listbt" class="entries"></ul>
 	<pre id="codebluetooth" class="status hide"></pre>
-	<div class="help-block hide"><wh>rAudio as sender:</wh>
+	<div class="help-block hide"><wh>rAudio as sender:</wh>&ensp;<i>(or pairing non-audio devices)</i>
  • Pairing:
- &emsp; - On receiver device - Turn on <code>pairing</code> / <code>discovery</code> mode
- &emsp; - On rAudio - Search and select the receiver device to pair
-• Connecting:
- &emsp; - On receiver devices - <code>power on</code> / <code>power off</code> : <code>connect</code> / <code>disconnect</code>
- • Device buttons:
- &emsp; - On receiver devices - Support <code>play</code> <code>pause</code> <code>previous</code> <code>next</code>
+ &emsp; - On receiver - Turn on <code>discovery</code> / <code>pairing</code> mode
+ &emsp; - On rAudio -&ensp;<i class="fa fa-search"></i>Search > Select receiver to pair
+ • Connecting:
+ &emsp; - On receiver - <code>power on</code> / <code>power off</code> > <code>connect</code> / <code>disconnect</code>
+ • Receiver buttons:
+ &emsp; - Playback controls <code>play</code> <code>pause</code> <code>previous</code> <code>next</code>
+
 <wh>rAudio as receiver:</wh>
  • Pairing:
  &emsp; - On rAudio - System > Bluetooth setting - enable <code>Discoverable by senders</code>
- &emsp; - On sender device - Search and select rAudio to pair
- &emsp; - To remove from the list - <code>Forget</code> both on rAudio and sender device
+ &emsp; - On sender - Search > Select rAudio to pair
+ &emsp; - <code>Forget</code> / <code>Remove</code> should be done on both rAudio and sender
  • Connecting:
- &emsp; - On sender devices - Select rAudio > <code>connect</code> / <code>disconnect</code>
+ &emsp; - On sender - Select rAudio > <code>connect</code> / <code>disconnect</code>
 </div>
 </div>
 <div id="divwl" class="section">
