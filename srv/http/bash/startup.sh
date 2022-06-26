5f3c63205f6b97ab143a9f4c50ea24717a6d523d
rern <rernrern@gmail.com>
2022-06-26 17:57:40 +0700

    Add files via upload

--- srv/http/bash/startup.sh
+++ srv/http/bash/startup.sh
@@ -140,12 +140,10 @@ fi
 
 if [[ ! $shareddata && ! -e $dirmpd/mpd.db ]]; then
 	if [[ ! -z $( ls /mnt/MPD/NAS ) || ! -z $( ls /mnt/MPD/SD ) || ! -z $( ls /mnt/MPD/USB ) ]]; then
-		echo rescan > $dirmpd/updating
-		mpc -q rescan
+		$dirbash/cmd.sh$'\n'rescan
 	fi
 elif [[ -e $dirmpd/updating ]]; then
-	path=$( cat $dirmpd/updating )
-	[[ $path == rescan ]] && mpc -q rescan || mpc -q update "$path"
+	$dirbash/cmd.sh$'\n'"$( cat $dirmpd/updating )"
 elif [[ -e $dirmpd/listing || ! -e $dirmpd/counts ]]; then
 	$dirbash/cmd-list.sh &> dev/null &
 fi
