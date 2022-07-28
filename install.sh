8d3d5098ce4190dfc96bb7588a95a007d4fc8b4a
rern <rernrern@gmail.com>
2022-07-28 11:49:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 alias=r1
 
 # 20220729
+if [[ -e /boot/kernel.img && $( pacman -Q linux-rpi-legacy ) != 'linux-rpi-legacy 5.10.90-1' ]]; then
+	pkgfile=linux-rpi-legacy-5.10.90-1-armv6h.pkg.tar.xz
+	wget https://github.com/rern/_assets/raw/master/$pkgfile
+	pacman -U --noconfirm $pkgfile
+	rm $pkgfile
+fi
+
 grep -q gpio-poweroff /boot/config.txt && sed -i '/gpio-poweroff\|gpio-shutdown/ d' /boot/config.txt
 
 # 20220708
