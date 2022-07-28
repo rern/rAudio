7edb37b54eb454868b5ae4cbf7a0332e8a7206cd
rern <rernrern@gmail.com>
2022-07-28 12:22:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 alias=r1
 
 # 20220729
-if [[ $(  uname -m ) == armv6l && $( uname -r | cut -c 1-7 ) != 5.10.90 ]]; then
-	echo Downgrade kernel to 5.10.90 ...
-	pkgfile=linux-rpi-legacy-5.10.90-1-armv6h.pkg.tar.xz
+if [[ $(  uname -m ) == armv6l && $( uname -r ) != '5.10.92-2-rpi-legacy-ARCH' ]]; then
+	echo Downgrade kernel to 5.10.92 ...
+	pkgfile=linux-rpi-legacy-5.10.92-2-armv6h.pkg.tar.xz
 	curl -skLO https://github.com/rern/_assets/raw/master/$pkgfile
 	pacman -U --noconfirm $pkgfile
 	rm $pkgfile
