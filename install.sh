cce1b409ff2cc958089e08b0f6dab9ff5e93d258
rern <rernrern@gmail.com>
2022-07-30 18:59:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 alias=r1
 
 # 20220729
-if [[ $( pacman -Q dab-scanner 2> /dev/null ) == 'dab-scanner 0.8-1' ]]; then
-	pacman -Sy --noconfirm dab-scanner
+[[ $( pacman -Q dab-scanner 2> /dev/null ) == 'dab-scanner 0.8-1' ]] && pacman -Sy --noconfirm dab-scanner
+if [[ -e /srv/http/bash/dab ]]; then
+	rm -rf /srv/http/bash/dab
 	stations=$( sed '1,/^paths:/ d' /etc/rtsp-simple-server/rtsp-simple-server.yml )
 	[[ $stations ]] && echo "$stations" | sed 's|dab/dabstart|dab-start|' >> /etc/rtsp-simple-server.yml
 	rm -f /srv/http/data/webradios/DAB/*.jpg
@@ -15,7 +16,7 @@ fi
 
 grep -A1 'plugin.*ffmpeg' /etc/mpd.conf | grep -q no && sed -i '/decoder/,+4 d' /etc/mpd.conf
 
-if [[ $(  uname -m ) == armv6l && $( uname -r ) != '5.10.92-2-rpi-legacy-ARCH' ]]; then
+if [[ $(  uname -m ) == armv6l && $( uname -r ) != 5.10.92-2-rpi-legacy-ARCH ]]; then
 	echo Downgrade kernel to 5.10.92 ...
 	pkgfile=linux-rpi-legacy-5.10.92-2-armv6h.pkg.tar.xz
 	curl -skLO https://github.com/rern/_assets/raw/master/$pkgfile
