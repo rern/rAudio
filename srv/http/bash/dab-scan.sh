b18f8c95f1b270172be7d1dd3416d21f460262f0
rern <rernrern@gmail.com>
2022-08-03 11:14:21 +0700

    Update dab-scan.sh

--- srv/http/bash/dab-scan.sh
+++ srv/http/bash/dab-scan.sh
@@ -2,8 +2,6 @@
 
 . /srv/http/bash/common.sh
 
-LC_CTYPE=C # this is needed to consider the lowercase accented chars outside of "alnum"
-
 dabscan=$( dab-scanner-rtlsdr -C 5A )
 services=$( echo "$dabscan" | grep ^audioservice )
 if [[ ! $services ]]; then
@@ -20,18 +18,17 @@ pathurl="$dirdabradio/rtsp:||$( hostname -f )"
 readarray -t services <<< "$services"
 for service in "${services[@]}"; do
 	readarray -d ';' -t field <<< $service
-	service_name=$( echo ${field[1]} | xargs )
-	service_chan=$( echo ${field[2]} )
-	service_id=$( echo ${field[3]} )
-	legal_nameU=${service_name//[^[:alnum:]]/_}
-	legal_name=${legal_nameU,,} # rtsp-simple-server does not like all uppercase/number service names
+	name=$( echo ${field[1]} | xargs )
+	channel=$( echo ${field[2]} )
+	id=$( echo ${field[3]} )
+	station=${id,,}_${channel,,}
 	echo "\
-$service_name
+$name
 48 kHz 160 kbit/s
-" > "$pathurl|$legal_name"
+" > "$pathurl|$station"
 	list+="\
-  $legal_name:
-    runOnDemand: /srv/http/bash/dab-start.sh $service_id $service_chan \$RTSP_PORT \$RTSP_PATH
+  $station:
+    runOnDemand: /srv/http/bash/dab-start.sh $id $channel \$RTSP_PORT \$RTSP_PATH
     runOnDemandRestart: yes
     runOnDemandStartTimeout: 15s
     runOnDemandCloseAfter: 3s
