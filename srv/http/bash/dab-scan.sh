a0fcb9c2e0b70efc7b261d4a0f84c8c12284fea0
rern <rernrern@gmail.com>
2022-08-03 09:17:58 +0700

    Update dab-scan.sh

--- srv/http/bash/dab-scan.sh
+++ srv/http/bash/dab-scan.sh
@@ -15,28 +15,27 @@ fi
 MYNAME=$( hostname -f )
 dirdabradio=$dirdata/dabradio
 mkdir -p $dirdabradio/img
-rm -f $dirdabradio/*
+rm -f $dirdabradio/* 2> /dev/null
 
 readarray -t services <<< "$services"
 for service in "${services[@]}"; do
 	readarray -d ';' -t field <<< $service
 	service_name=$( echo ${field[1]} | xargs )
-	service_chan=$( echo ${field[2]} | xargs )
-	service_id=$( echo ${field[3]} | xargs )
+	service_chan=$( echo ${field[2]} )
+	service_id=$( echo ${field[3]} )
 	legal_nameU=${service_name//[^[:alnum:]]/_}
 	legal_name=${legal_nameU,,} # rtsp-simple-server does not like all uppercase/number service names
 	echo "\
 $service_name
 48 kHz 160 kbit/s
 " > "$dirdabradio/rtsp:||$MYNAME|$legal_name"
-	list+=$( cat << EOF
+	list+="\
   $legal_name:
     runOnDemand: /srv/http/bash/dab-start.sh $service_id $service_chan \$RTSP_PORT \$RTSP_PATH
     runOnDemandRestart: yes
     runOnDemandStartTimeout: 15s
     runOnDemandCloseAfter: 3s
-EOF
-)
+"
 done
 
 fileyml=/etc/rtsp-simple-server.yml
