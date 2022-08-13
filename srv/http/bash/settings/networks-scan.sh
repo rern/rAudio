284fc3e90c87c1feea9224da35de977d80a7df32
rern <rernrern@gmail.com>
2022-08-13 20:47:18 +0700

    u

--- srv/http/bash/settings/networks-scan.sh
+++ srv/http/bash/settings/networks-scan.sh
@@ -2,6 +2,34 @@
 
 . /srv/http/bash/common.sh
 
+if [[ $1 != wlan ]]; then
+	listBt() {
+		bluetoothctl $1 \
+			| grep -v ' ..-..-..-..-..-..$' \
+			| sed -E 's/Device (..:..:..:..:..:..) (.*)/\2^\1/' \
+			| sort -f
+	}
+
+	bluetoothctl --timeout=10 scan on &> /dev/null
+
+	devices=$( listBt devices )
+	[[ ! $devices ]] && exit
+
+	paired=$( listBt paired-devices )
+	[[ $paired ]] && devices=$( diff <( echo "$paired" ) <( echo "$devices" ) | grep '^>' | cut -c 3- )
+	readarray -t devices <<< "$devices"
+	for dev in "${devices[@]}"; do
+		name=${dev/^*}
+		mac=${dev/*^}
+		data+=',{
+  "name" : "'$name'"
+, "mac"  : "'$mac'"
+}'
+	done
+	data2json "$data"
+	exit
+fi
+
 wlandev=$( cat $dirshm/wlan )
 ip link set $wlandev up
 
