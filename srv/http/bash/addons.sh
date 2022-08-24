545b396caebbce56d8942222436e7407f1741702
rern <rernrern@gmail.com>
2022-08-24 09:46:55 +0700

    Update addons.sh

--- srv/http/bash/addons.sh
+++ srv/http/bash/addons.sh
@@ -97,10 +97,12 @@ getinstallzip() {
 		| grep -v '/$' \
 		| sed 's|^|/|' \
 		| sort -V
+	rm /srv/http/*.*
+	rm -rf /srv/http/{assets,bash,settings}
 	bsdtar xf $installfile --strip 1 -C $tmpdir
-	rm $installfile $tmpdir/* &> /dev/null
-	cp -rp $tmpdir/* /
-	rm -r $tmpdir
+	rm $installfile $tmpdir/{.*,*} &> /dev/null
+	cp -r $tmpdir/* /
+	rm -rf $tmpdir
 	chmod +x $dirbash/cmd.sh
 	$dirbash/cmd.sh dirpermissions
 	[[ -e $dirsystem/color ]] && $dirbash/cmd.sh color
