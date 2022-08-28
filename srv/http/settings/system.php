81c5087b7c08b0ad02536ec67472b94c07d57bfe
rern <rernrern@gmail.com>
2022-08-28 18:14:57 +0700

    Add files via upload

--- srv/http/settings/system.php
+++ srv/http/settings/system.php
@@ -169,7 +169,7 @@ HTML
 	, 'help'     => <<< HTML
 I²S DAC/audio HAT(Hardware Attached on Top) for high quality audio output.
  • HAT with EEPROM might be automatically detected and setup. See <i class="fa fa-player gr"></i>Player if it's already listed in Output.
- • HAT with obsolete EEPROM - Disable the EEPROM to use selected driver by kernel. ( <i class="fa fa-gear gr"></i>next to selected HAT)
+ • HAT with obsolete EEPROM - Disable EEPROM read to use selected driver by kernel. ( <i class="fa fa-gear gr"></i>next to selected HAT)
 HTML
 	]
 	, [
