5e537013b8666d1a2c0742c67e2e65e41b62f9ea
rern <rernrern@gmail.com>
2022-09-04 09:16:49 +0700

    Update networks-scan.sh

--- srv/http/bash/settings/networks-scan.sh
+++ srv/http/bash/settings/networks-scan.sh
@@ -17,7 +17,7 @@ if [[ $1 == wlan ]]; then
 	scan=$( echo "$scan" \
 				| sed -E 's/^\s*|\s*$//g' \
 				| egrep '^Cell|^ESSID|^Encryption|^IE.*WPA|^Quality' \
-				| sed -E 's/^Cell.*/},{/
+				| sed -E 's/^Cell.*/,{/
 						  s/^Quality.*level.(.*)/,"signal":"\1"/
 						  s/^Encryption key:(.*)/,"encrypt":"\1"/
 						  s/^ESSID:/,"ssid":/
@@ -25,10 +25,8 @@ if [[ $1 == wlan ]]; then
 						  s/\\x00//g' \
 				| tr -d '\n' \
 				| sed 's/{,/{/g; s/,{/\n&/g' \
-				| sed -e '1 d' \
-					  -e '/"ssid":""/ d' \
-					  -e 's/wpa.*wpa/wpa/' \
-					  -e '$ s/$/}/' \
+				| sed -E -e '/^$|"ssid":""/ d' \
+						 -e 's/wpa.*wpa/wpa/; s/$/}/' \
 				| sort )
 	
 	# omit saved profile
