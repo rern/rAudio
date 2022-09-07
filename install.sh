1653f68e1ffc6a8427ba55b706e7417f49514c3f
rern <rernrern@gmail.com>
2022-09-07 12:58:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 alias=r1
 
+# 20220907
+[[ $( pacman -Q bluez ) < 'bluez 5.65-3' ]] && pacman -Sy --noconfirm bluez
+
 # 20220826
 rm /srv/http/bash/{camilladsp*,features*,networks*,player*,relays*,system*} &> /dev/null
 
