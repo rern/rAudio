add9d2e9b79d285e6b4b93c83735cd19f39006a2
rern <rernrern@gmail.com>
2022-10-25 11:41:51 +0700

    Update status-push.sh

--- srv/http/bash/status-push.sh
+++ srv/http/bash/status-push.sh
@@ -43,7 +43,7 @@ fi
 	&& -e $dirsystem/scrobble && ! -e $dirshm/scrobble ]] && scrobble=1
 
 if [[ -e $dirsystem/onwhileplay ]]; then
-	[[ ! $state ]] && state=$( awk -F'"' '/^state/ {print $2}' $dirshm/status ) # $1 == statusradio
+	[[ ! $state ]] && state=$( grep ^state $dirshm/status | cut -d'"' -f2 ) # $1 == statusradio
 	export DISPLAY=:0
 	[[ $state == play ]] && sudo xset -dpms || sudo xset +dpms
 fi
