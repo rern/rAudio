0580e4e1930aef7bc1ab584175798e11918aca86
rern <rernrern@gmail.com>
2023-01-02 09:39:25 +0700

    Update motd.sh

--- srv/http/bash/motd.sh
+++ srv/http/bash/motd.sh
@@ -2,7 +2,6 @@
 clear
 def='\e[0m'
 bg='\e[30m\e[46m'
-col=$( tput cols )
-printf "$bg%*s$def\n" $col
-printf "$bg%-${col}s$def\n" "   r A u d i o"
-printf "$bg%*s$def\n\n" $col
+printf "$bg%*s$def\n" $COLUMNS
+printf "$bg%-${COLUMNS}s$def\n" "   r A u d i o"
+printf "$bg%*s$def\n\n" $COLUMNS
