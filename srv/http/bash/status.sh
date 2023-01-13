eb5be0e5ac45dd0b0e362e2f006059c437a6405f
rern <rernrern@gmail.com>
2023-01-13 08:30:29 +0700

    Update status.sh

--- srv/http/bash/status.sh
+++ srv/http/bash/status.sh
@@ -315,7 +315,7 @@ $radiosampling" > $dirshm/radio
 						systemctl start $service
 					fi
 				else
-					[[ -e $dirshm/status ]] && . <( grep -E '^Artist|^Album|^Title|^coverart|^station' $dirshm/status )
+					. <( grep -E '^Artist|^Album|^Title|^coverart|^station' $dirshm/status )
 					[[ ! $displaycover ]] && coverart=
 				fi
 			elif [[ $Title && $displaycover ]]; then
