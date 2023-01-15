bda6e4f3d07a75ecbe2c4290b07f96590ba288fb
rern <rernrern@gmail.com>
2023-01-15 09:48:16 +0700

    Update player-conf.sh

--- srv/http/bash/settings/player-conf.sh
+++ srv/http/bash/settings/player-conf.sh
@@ -172,9 +172,7 @@ if [[ -e /usr/bin/spotifyd ]]; then
 	if [[ $btmixer ]]; then
 		device=$( bluealsa-aplay -L | head -1 )
 	else
-		cardname=$( aplay -l 2> /dev/null | awk '/^card '$i'/ {print $3;exit}' )
-		[[ $cardname ]] && device=$( aplay -L | grep -m1 "^default.*$cardname" )
-#		device="default:CARD=$( aplay -l 2> /dev/null | awk '/^card '$i'/ {print $3;exit}' )"
+		device="default:CARD=$( aplay -l 2> /dev/null | awk '/^card '$i'/ {print $3;exit}' )"
 	fi
 ########
 	conf='[global]
