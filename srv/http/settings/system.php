bd5e0c46b81f83586453814d4b35a43ba3492e85
rern <rernrern@gmail.com>
2023-02-28 20:04:32 +0700

    u

--- srv/http/settings/system.php
+++ srv/http/settings/system.php
@@ -421,7 +421,7 @@ for( $i = 'A'; $i !== 'AA'; $i++ ) {
 <div id="divabout" class="section">
 	<a href="https://github.com/rern/rAudio-1/discussions"><img src="/assets/img/icon.svg<?=$hash?>" style="width: 40px"></a>
 	<div id="logotext">rAudio
-	<br><gr>by&emsp;r e r n</gr></div>
+	<br><gr>b y&emsp;r e r n</gr></div>
 	
 	<heading class="subhead">Back End</heading>
 	<div class="list">
