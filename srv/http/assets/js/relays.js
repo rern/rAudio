8d14028029fc5944bdcc465a0961f7bfea8d45a0
rern <rernrern@gmail.com>
2023-03-01 15:38:31 +0700

    u

--- srv/http/assets/js/relays.js
+++ srv/http/assets/js/relays.js
@@ -1,50 +1,35 @@
 $( function() { //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-R = {}
-var values0;
-
 $( '.gpio-no' ).addClass( 'hide' );
 
-$( '.container' ).on( 'change', 'select', function() {
-	if ( this.id.slice( 0, 3 ) === 'pin' ) {
-		var i    = +this.id.slice( -1 );
-		var val  = +this.value;
-		var val0 = R.pin[ i ];
-		R.on.forEach( ( el, i ) => {
-			if ( el == val0 ) R.on[ i ] = val;
-		} );
-		R.off.forEach( ( el, i ) => {
-			if ( el == val0 ) R.off[ i ] = val;
-		} );
-	}
-	refreshValues();
-} );
+$( 'select' ).change( refreshValues );
 $( 'input' ).keyup( refreshValues );
 $( '.back' ).click( function() {
 	location.href = 'settings.php?p=system';
 } );
 $( '#undo' ).click( function() {
-	R = {}
-	renderPage( false );
+	R = JSON.parse( JSON.stringify( S ) );
+	renderPage();
+	$( '#undo' ).addClass( 'disabled' )
 } );
 $( '#save' ).off( 'click' ).click( function() {
 	var onorder  = [];
 	var offorder = [];
 	for ( i = 0; i < 4; i++ ) {
-		onorder.push(  $( '#on'+ i +' option:selected' ).text() );
-		offorder.push( $( '#off'+ i+' option:selected' ).text() );
+		onorder.push(  $( '.on' ).eq( i ).find( 'option:selected' ).text() );
+		offorder.push( $( '.off' ).eq( i ).find( 'option:selected' ).text() );
 	}
 	var cmd = [
-				  'save'
-				, 'pin=\'[ '+ R.pin.join( ',' ) +' ]\''
-				, 'name=\'[ "'+ R.name.join( '","' ) +'" ]\''
-				, 'onorder=\'[ "'+ onorder.join( '","' ) +'" ]\''
-				, 'offorder=\'[ "'+ offorder.join( '","' ) +'" ]\''
-				, 'on=( '+ R.on.join( ' ' ) +' )'
-				, 'ond=( '+ R.ond.join( ' ' ) +' )'
-				, 'off=( '+ R.off.join( ' ' ) +' )'
-				, 'offd=( '+ R.offd.join( ' ' ) +' )'
-				, 'timer='+ R.timer
+		  'save'
+		, 'pin=\'[ '+ R.pin.join( ',' ) +' ]\''
+		, 'name=\'[ "'+ R.name.join( '","' ) +'" ]\''
+		, 'onorder=\'[ "'+ onorder.join( '","' ) +'" ]\''
+		, 'offorder=\'[ "'+ offorder.join( '","' ) +'" ]\''
+		, 'on=( '+ R.on.join( ' ' ) +' )'
+		, 'ond=( '+ R.ond.join( ' ' ) +' )'
+		, 'off=( '+ R.off.join( ' ' ) +' )'
+		, 'offd=( '+ R.offd.join( ' ' ) +' )'
+		, 'timer='+ R.timer
 	];
 	bash( cmd );
 	banner( 'relays', 'Relays', 'Change ...' );
@@ -53,59 +38,38 @@ $( '#save' ).off( 'click' ).click( function() {
 } ); //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 function refreshValues() {
-	$( '#gpio-num .pin' ).each( ( i, el ) => R.pin[ i ]  = +$( el ).val() );
-	$( '.name' ).each(          ( i, el ) => R.name[ i ] = $( el ).val() );
-	$( '.on.delay' ).each(      ( i, el ) => R.ond[ i ]  = R.on[ i + 1 ]  ? +$( el ).val() : 0 );
-	$( '.off.delay' ).each(     ( i, el ) => R.offd[ i ] = R.off[ i + 1 ] ? +$( el ).val() : 0 );
-	R.timer = +$( '#timer' ).val();
-	var values  = [].concat.apply( [], Object.values( R ) ).toString();
-	renderPage( values !== values0 );
+	setTimeout( () => { // fix select2 not ready
+		$( '.pin' ).each(  ( i, el ) => R.pin[ i ]  = +$( el ).val() );
+		$( '.name' ).each( ( i, el ) => R.name[ i ] = $( el ).val() );
+		$( '.on' ).each(   ( i, el ) => R.on[ i ]   = +$( el ).val() );
+		$( '.off' ).each(  ( i, el ) => R.off[ i ]  = +$( el ).val() );
+		$( '.ond' ).each(  ( i, el ) => R.ond[ i ]  = R.on[ i + 1 ]  ? +$( el ).val() : 0 );
+		$( '.offd' ).each( ( i, el ) => R.offd[ i ] = R.off[ i + 1 ] ? +$( el ).val() : 0 );
+		R.timer     = +$( '#timer' ).val();
+		$( '.infobtn' ).toggleClass( 'disabled', V === JSON.stringify( R ) )
+	}, 0 );
 }
-function renderPage( changed ) {
-	if ( ! ( 'pin' in R ) ) {
-		R       = JSON.parse( JSON.stringify( S ) );
-		values0 = [].concat.apply( [], Object.values( S ) ).toString();
-	}
-	$( '#save' ).toggleClass( 'disabled', S.enabled && ! changed )
-	$( '#undo' ).toggleClass( 'disabled', ! changed )
-	var optnamepin = '<option value="0">--- none ---</option>';
-	for ( i = 0; i < 4; i++ ) optnamepin += '<option value="'+ R.pin[ i ] +'">'+ R.name[ i ] || '(no name)' +'</option>';
-	var htmlon  = '';
-	var htmloff = '';
-	var optsec  = '<option value="0">0</option>';
-	for ( i = 1; i < 11; i++ ) optsec += '<option value="'+ i +'">'+ i +'</option>';
-	htmlsec     = '</select><span class="sec">sec.</span>';
+function renderPage() {
+	V = JSON.stringify( S );
+	R = JSON.parse( V );
+	$( '#save' ).toggleClass( 'disabled', S.enabled )
 	for ( i = 0; i < 4; i++ ) {
-		htmlon  +=  '<select id="on'+ i +'" class="on pin">'+ optnamepin +'</select>';
-		htmloff += '<select id="off'+ i +'" class="off pin">'+ optnamepin +'</select>';
-		if ( i < 3 ) {
-			htmlon  += '<select id="ond'+ i +'" class="on delay">'+ optsec + htmlsec;
-			htmloff += '<select id="offd'+ i +'" class="off delay">'+ optsec + htmlsec;
-		}
+		$( '.pin' ).eq( i ).val(  R.pin[ i ] );
+		$( '.name' ).eq( i ).val( R.name[ i ] );
+		$( '.on' ).eq( i ).val(   R.on[ i ] );
+		$( '.off' ).eq( i ).val(  R.off[ i ] );
+		$( '.ond' ).eq( i ).val(  R.ond[ i ] );
+		$( '.offd' ).eq( i ).val( R.offd[ i ] );
 	}
-	$( '#timer' )
-		.html( optsec )
-		.find( 'option[value='+ R.timer +']' ).prop( 'selected', 1 );
-	$( '#on' ).html( htmlon );
-	$( '#off' ).html( htmloff );
-	for ( i = 0; i < 4; i++ ) {
-		$( '#pin'+ i ).val(  R.pin[ i ] );
-		$( '#name'+ i ).val( R.name[ i ] );
-		$( '#on'+ i ).val(   R.on[ i ] );
-		$( '#off'+ i ).val(  R.off[ i ] );
-		$( '#ond'+ i ).val(  R.ond[ i ] );
-		$( '#offd'+ i ).val( R.offd[ i ] );
-	}
-	$( '.ond, .offd' ).prop( 'disabled', 0 );
+	$( '#timer' ).val( R.timer );
 	var $el0 = $( '.on, .off' ).filter( ( i, el ) => {
 		var $this = $( el );
 		return ! $this.hasClass( 'delay' ) && $this.val() == 0;
 	} );
-	if ( $el0.length ) {
-		$el0.find( '.label' ).addClass( 'gr' );
-		$el0.prop( 'disabled', 1 );
-	}
+	if ( $el0.length ) $el0.find( '.label' ).addClass( 'gr' );
 	showContent();
-	$( '#on > .select2' ).odd().css( 'cssText', 'width:70px !important' );
-	$( '#off > .select2' ).odd().css( 'cssText', 'width:70px !important' );
+	$( '#on > .select2' )
+		.odd()
+		.add( $( '#off > .select2' ).odd() )
+		.css( 'cssText', 'width:70px !important' );
 }
--- srv/http/settings/relays.php
+++ srv/http/settings/relays.php
@@ -1,13 +1,35 @@
 <?php
+$data       = shell_exec( '/srv/http/bash/settings/relays-data.sh' );
+$data       = json_decode( $data );
+
 $pins      = [ 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 32, 33, 35, 36, 37, 38, 40 ];
 $optionpin = '';
 foreach ( $pins as $p ) $optionpin.= '<option value='.$p.'>'.$p.'</option>';
 $htmlpin   = '';
 $htmlname  = '';
 for ( $i = 0; $i < 4; $i++ ) {
-	$htmlpin .= '<select id="pin'.$i.'" class="pin">'.$optionpin.'</select>';
-	$htmlname.= '<input id="name'.$i.'" type="text" class="name" placeholder="(no name)">';
+	$htmlpin .= '<select class="pin">'.$optionpin.'</select>';
+	$htmlname.= '<input type="text" class="name" placeholder="(no name)">';
 }
+
+$optnamepin = '<option value="0">--- none ---</option>';
+for ( $i = 0; $i < 4; $i++ ) $optnamepin.= '<option value="'.$data->pin[ $i ].'">'.$data->name[ $i ] ?? '(no name)</option>';
+
+$optsec     = '<option value="0">0</option>';
+for( $i = 1; $i < 11; $i++ ) $optsec.= '<option value="'.$i.'">'.$i.'</option>';
+
+$htmlsec    = '</select><span class="sec">sec.</span>';
+$selecton     = '';
+$selectoff    = '';
+for ( $i = 0; $i < 4; $i++ ) {
+	$selecton .= '<select class="on">'.$optnamepin.'</select>';
+	$selectoff.= '<select class="off">'.$optnamepin.'</select>';
+	if ( $i < 3 ) {
+		$selecton .= '<select class="ond delay">'.$optsec.$htmlsec;
+		$selectoff.= '<select class="offd delay">'.$optsec.$htmlsec;
+	}
+}
+
 echo '<div class="section">';
 htmlHead( [ //////////////////////////////////
 	  'title'  => 'Relay Module'
@@ -28,7 +50,7 @@ On/Off:  <a class="helpmenu"><?=( i( 'raudio' ) )?> System<?=( i( 'relays sub'
 		<span class="gpio-text"><?=( i( 'gpiopins bl' ) )?>Pin</span>
 		<?=$htmlpin?>
 		<span class="gpio-text"><?=( i( 'stoptimer yl' ) )?> Idle</span>
-		<select id="timer" class="timer"></select>
+		<select id="timer" class="timer"><?=$optsec?></select>
 	</div>
 	<div class="column c2" id="gpio-name">
 		<span class="gpio-text"><?=( i( 'tag bl' ) )?> Name</span>
@@ -39,13 +61,13 @@ On/Off:  <a class="helpmenu"><?=( i( 'raudio' ) )?> System<?=( i( 'relays sub'
 <div class="gpio-float-r">
 	<div class="column">
 		<span class="gpio-text"><?=( i( 'power grn' ) )?> On Sequence</span>
-		<div id="on"></div>
+		<div id="on"><?=$selecton?></div>
 	</div>
 	<div class="column c2">
 		<span class="gpio-text"><?=( i( 'power red' ) )?> Off Sequence</span>
-		<div id="off"></div>
+		<div id="off"><?=$selectoff?></div>
 		<br>
-		<a id="undo" class="infobtn infobtn"><?=( i( 'undo' ) )?>Undo</a><!--
+		<a id="undo" class="infobtn infobtn disabled"><?=( i( 'undo' ) )?>Undo</a><!--
 	 --><a id="save" class="infobtn infobtn-primary"><?=( i( 'save' ) )?>Save</a>
 	</div>
 </div>
