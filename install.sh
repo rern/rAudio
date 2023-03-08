6b8620046be31a78c04f4f477f7d0b1fa4e34712
rern <rernrern@gmail.com>
2023-03-08 13:51:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ alias=r1
 . /srv/http/bash/addons.sh
 
 # 20230310
-[[ -e $dirmpdconf/bluetooth.conf ]] && touch $dirsystem/btoutputonly
+[[ ! -e $dirmpdconf/bluetooth.conf ]] && touch $dirsystem/btoutputonly
 
 # 20230218
 sed -E -i 's/(cursor=)true/\1yes/; s/(cursor=)false/\1no/' $dirsystem/localbrowser.conf &> /dev/null
