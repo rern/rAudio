b0abba834928a4a16ea049daa96d3a9bf568e7bc
rern <rernrern@gmail.com>
2023-03-15 18:48:01 +0700

    Update addons-data.sh

--- srv/http/bash/settings/addons-data.sh
+++ srv/http/bash/settings/addons-data.sh
@@ -9,14 +9,14 @@ data=$( curl -sSfL https://github.com/rern/rAudio-addons/raw/main/addons-list.js
 
 addons=$( sed -n '/^\s, .*{$/ {s/.*, "\(.*\)".*/\1/; p}' <<< $data )
 for addon in $addons; do
-	this=$( sed -n '/"'$addon'"/,/^\s}/ p' <<< $data )
-	hide=$( sed -n '/"hide"/ {s/.* : "//; s/"$//; p}' <<< $this )
-	verify=$( sed -n '/"command"/ {s/.* : "//; s/"$//; p}' <<< $this )
+	addondata=$( sed -n '/"'$addon'"/,/^\s}/ p' <<< $data )
+	hide=$( sed -n '/"hide"/ {s/.* : "//; s/"$//; p}' <<< $addondata )
+	verify=$( sed -n '/"command"/ {s/.* : "//; s/"$//; p}' <<< $addondata )
 	[[ $hide && $( eval $hide 2> /dev/null ) == 1 ]] && hide+=',"'$addon'"'
 	[[ $verify && $( eval $verify 2> /dev/null ) == 1 ]] && notverified+=',"'$addon'"'
 	if [[ -e $diraddons/$addon ]]; then
 		installed+=',"'$addon'"'
-		version=$( sed -n '/"version"/ {s/.* : "//; s/"$//; p}' <<< $this )
+		version=$( sed -n '/"version"/ {s/.* : "//; s/"$//; p}' <<< $addondata )
 		[[ $( < $diraddons/$addon ) < $version ]] && update+=',"'$addon'"'
 	fi
 done
