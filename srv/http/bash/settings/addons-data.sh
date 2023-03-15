e1fac0b0c4730926d6ee16ecc61d4e827ba16da2
rern <rernrern@gmail.com>
2023-03-15 18:39:52 +0700

    Update addons-data.sh

--- srv/http/bash/settings/addons-data.sh
+++ srv/http/bash/settings/addons-data.sh
@@ -4,7 +4,7 @@
 
 ! internetConnected && echo 'No internet connection.' && exit
 
-addonslist=$( curl -sSfL https://github.com/rern/rAudio-addons/raw/main/addons-list.json )
+addonslist=$( curl -sSfL https://github.com/rern/rAudio-addons/raw/main/addons-list.json | head -n -1 ) # remove last } for append
 [[ $? != 0 ]] && echo 'Database download failed.' && exit
 
 addons=$( sed -n '/^\s, .*{$/ {s/.*, "\(.*\)".*/\1/; p}' <<< $addonslist )
@@ -16,31 +16,27 @@ for addon in $addons; do
 	[[ $verify && $( eval $verify 2> /dev/null ) == 1 ]] && notverified+=',"'$addon'"'
 	if [[ -e $diraddons/$addon ]]; then
 		installed+=',"'$addon'"'
-		verinstalled=$( < $diraddons/$addon )
-		versions+=',"'$addon'" : "'$verinstalled'"'
 		version=$( sed -n '/"version"/ {s/.* : "//; s/"$//; p}' <<< $data )
-		[[ $verinstalled > 1 && $verinstalled < $version ]] && update+=',"'$addon'"'
+		[[ $( < $diraddons/$addon ) < $version ]] && update+=',"'$addon'"'
 	fi
 done
-if [[ $update ]]; then
-	pushstream option '{"addons":1}'
-	touch $diraddons/update
-else
-	rm -f $diraddons/update
-fi
 [[ $hide ]] && hide='['${hide:1}']' || hide='[""]'
 [[ $installed ]] && installed='['${installed:1}']' || installed='[""]'
 [[ $notverified ]] && notverified='['${notverified:1}']' || notverified='[""]'
-[[ $update ]] && update='['${update:1}']' || update='[""]'
-data=$( sed '$d' <<< $addonslist )
-data+='
+if [[ $update ]]; then
+	update='['${update:1}']'
+	pushstream option '{"addons":1}'
+	touch $diraddons/update
+else
+	update='[""]'
+	rm -f $diraddons/update
+fi
+data=$addonslist'
 	, "status" : {
-		  "versioninstalled" : {'${versions:1}'}
-		, "hide"             : '$hide'
+		  "hide"             : '$hide'
 		, "installed"        : '$installed'
 		, "notverified"      : '$notverified'
 		, "update"           : '$update'
 	}
 }'
-
 echo "$data"
