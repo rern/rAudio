d660c7e7f1c01014c3bd760e01d51c93b3d22ba7
rern <rernrern@gmail.com>
2023-04-18 13:48:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if crontab -l | grep -q addonsupdates; then
 $( crontab -l | grep -v addonsupdates )" | crontab -
 fi
 
-if ls $dirsystem/autoplay* 2> /dev/null; then
+if ls $dirsystem/autoplay* 2> /dev/null && [[ ! -s $dirsystem/autoplay ]]; then
 	k=( startup bluetooth cd )
 	f=( autoplay autoplaybt autoplaycd )
 	for (( i=0; i < 3; i++ )); do
@@ -28,7 +28,9 @@ rm -f $dirsystem/btoutputonly
 [[ -e $dirmpdconf/bluetooth.conf && -e $dirmpdconf/output.conf ]] && touch $dirsystem/btoutputall
 
 file=$dirsystem/equalizer.conf
-if [[ -e $file ]]; then
+if [[ ! -e ${file/.*} ]]; then
+	rm -f $file
+elif [[ ! -e$dirsystem/equalizer.json ]]; then
 	readarray -t lines <<< $( grep -v '^Flat$' $file )
 	for l in "${lines[@]}"; do
 		preset+=', "'${l/^*}'" : [ '$( tr ' ' , <<< ${l/*^} )' ]'
@@ -45,47 +47,45 @@ for file in display order; do
 	[[ -e $dirsystem/$file ]] && mv $dirsystem/$file{,.json}
 done
 
-file=$dirsystem/lcdchar
-if [[ -e $file ]]; then
-	if grep -q var $file.conf; then
-		lines=$( < $file.conf )
-		data=$( grep -Ev 'var|pins_data' <<< $lines )
-		if grep -q pins_data <<< $lines; then
-			p=( $( sed -E -n '/pins_data/ {s/.*\[(.*)]/\1/; s/,/ /g; p}' <<< $lines ) )
-			for (( i=0; i < 4; i++ )); do
-				data+="
+file=$dirsystem/lcdchar.conf
+if [[ ! -e ${file/.*} ]]; then
+	rm -f $file
+elif [[ ! -e $dirsystem/lcdcharconf.py ]]; then
+	lines=$( < $file )
+	data=$( grep -Ev 'var|pins_data' <<< $lines )
+	if grep -q pins_data <<< $lines; then
+		p=( $( sed -E -n '/pins_data/ {s/.*\[(.*)]/\1/; s/,/ /g; p}' <<< $lines ) )
+		for (( i=0; i < 4; i++ )); do
+			data+="
 p$i=$p"
-			done
-		fi
-		echo $data > $dirsystem/lcdcharconf.py
+		done
 	fi
-else
-	rm -f $file.conf
+	echo $data > $dirsystem/lcdcharconf.py
+	rm -f $file
 fi
 
-if systemctl -q is-active localbrowser; then
-	file=$dirsystem/localbrowser.conf
-	if [[ $( sed -n 6p $file ) != cursor* ]]; then
-		[[ -e $dirsystem/onwhileplay ]] && onwhileplay=true && rm $dirsystem/onwhileplay
-		grep -q hdmi_force_hotplug=1 /boot/config.txt && hdmi=true
-		. $file
-		conf="\
-	rotate=$rotate
-	zoom=$zoom
-	screenoff=$screenoff
-	onwhileplay=$onwhileplay
-	hdmi=$hdmi
-	cursor=$( [[ $cursor == yes ]] && echo true )"
-		echo "$conf" > $file
-	fi
-else
-	rm -f $dirsystem/localbrowser.conf
+file=$dirsystem/localbrowser.conf
+if ! systemctl -q is-active localbrowser; then
+	rm -f $file
+elif [[ $( sed -n 6p $file ) != cursor* ]]; then
+	[[ -e $dirsystem/onwhileplay ]] && onwhileplay=true && rm $dirsystem/onwhileplay
+	grep -q hdmi_force_hotplug=1 /boot/config.txt && hdmi=true
+	. $file
+	conf="\
+rotate=$rotate
+zoom=$zoom
+screenoff=$screenoff
+onwhileplay=$onwhileplay
+hdmi=$hdmi
+cursor=$( [[ $cursor == yes ]] && echo true )"
+	echo "$conf" > $file
 fi
 
-if [[ ! -e $dirsystem/relays ]]; then
-	rm -f $dirsystem/relays.conf
-elif grep -q ^pin $dirsystem/relays.conf; then
-	. $dirsystem/relays.conf
+file=$dirsystem/relays.conf
+if [[ ! -e ${file/.*} ]]; then
+	rm -f $file
+elif [[ ! -e $dirsystem/relays.json ]]; then
+	. $file
 	data="\
 on='${on[@]}'
 off='${off[@]}'
@@ -110,31 +110,19 @@ timer=$timer"
 	data+='
 orderon="'$( sed -E 's/(["`])/\\\1/g; s/\\n$//' <<< $orderon )'"
 orderoff="'$( sed -E 's/(["`])/\\\1/g; s/\\n$//' <<< $orderoff )'"'
-	echo "$data" > $dirsystem/relays.conf
+	echo "$data" > $file
 fi
 
 file=$dirsystem/vuled.conf
-if [[ -e $file ]]; then
+if [[ ! -e ${file/.*} ]]; then
+	rm $file
+elif ! grep -q ^p0 $file; then
 	pins=( $( < $file ) )
 	data=
 	for (( i=0; i < 7; i++ )); do
 		data+="p$i=${pins[i]}"$'\n'
 	done
 	echo "$data" > $file
-fi
-
-# 20230218
-sed -E -i 's/(cursor=)true/\1yes/; s/(cursor=)false/\1no/' $dirsystem/localbrowser.conf &> /dev/null
-
-[[ -d $dirsystem/scrobble.conf ]] && rm -rf $dirsystem/scrobble.conf
-if [[ -e /boot/kernel7.img ]]; then
-	if [[ ! -e /usr/bin/firefox ]]; then
-		echo -e "$bar Switch Browser on RPi to Firefox ..."
-		pacman -R --noconfirm chromium
-		pacman -Sy --noconfirm firefox
-		timeout 1 firefox --headless &> /dev/null
-	fi
-	! grep -q hdmi_force_hotplug=1 /boot/config.txt && echo hdmi_force_hotplug=1 >> /boot/config.txt
 fi
 
 #-------------------------------------------------------------------------------
