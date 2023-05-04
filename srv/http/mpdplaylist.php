79705b82c2aea393a36725ed7877ddc0b9fe03bf
rern <rernrern@gmail.com>
2023-05-04 13:00:47 +0700

    Update mpdplaylist.php

--- srv/http/mpdplaylist.php
+++ srv/http/mpdplaylist.php
@@ -247,6 +247,7 @@ function htmlTrack( $lists, $plname = '' ) {
 	if ( $countradio ) $counthtml.= i( 'webradio' ).'<wh id="pl-radiocount">'.$countradio.'</wh>';
 	if ( $countupnp )  $counthtml.= '&emsp;'.i( 'upnp' );
 	$song = exec( 'mpc status %songpos%' ) - 1;
+	if ( $song === -1 ) $song = 0;
 	$mmss = exec( 'mpc status %currenttime%' );
 	$mmss = explode( ':', $mmss );
 	$elapsed = $mmss[ 0 ] * 60 + $mmss[ 1 ];
