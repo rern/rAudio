84c0480306872a2cde8b1e1202e8a60ac52a841b
rern <rernrern@gmail.com>
2023-06-03 07:41:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ alias=r1
 # 20230522
 [[ -e /srv/http/bash/settings/addons.sh ]] && . /srv/http/bash/settings/addons.sh || . /srv/http/bash/addons.sh
 
-# 20230601
+# 20230603
 [[ $( pacman -Q bluealsa ) != 'bluealsa 4.1.0-1' ]] && pacman -Sy --noconfirm bluealsa
 
 # 20230528
