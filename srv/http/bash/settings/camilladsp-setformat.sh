136d051a45a2aec1dd0317aeaafc0b0b6d1af2a7
rern <rernrern@gmail.com>
2023-06-30 10:18:57 +0700

    u

--- srv/http/bash/settings/camilladsp-setformat.sh
+++ srv/http/bash/settings/camilladsp-setformat.sh
@@ -10,29 +10,16 @@ camilladspyml=$dirconfigs/camilladsp.yml
 card=$( < $dirsystem/asoundcard )
 sed -i -E "/playback:/,/device:/ s/(device: hw:).*/\1$card,0/" $camilladspyml
 
-camilladsp $camilladspyml &> /dev/null &
-sleep 1
-if pgrep -x camilladsp &> /dev/null; then
-	formatok=1
-else
-	notify -blink camilladsp CamillaDSP "Set Playback format ..."
-	lineformat=$( sed -n '/playback:/,/format:/ {/format:/ =}' $camilladspyml )
-	for format in FLOAT64LE FLOAT32LE S32LE S24LE3 S24LE S16LE; do
-		sed -i -E "$lineformat s/(format: ).*/\1$format/" $camilladspyml
-		camilladsp $camilladspyml &> /dev/null &
-		sleep 1
-		if pgrep -x camilladsp &> /dev/null; then
-			formatok=1
-			break
-		fi
-	done
-fi
-if [[ $formatok ]]; then
+notify -blink camilladsp CamillaDSP "Set Playback format ..."
+for format in FLOAT64LE FLOAT32LE S32LE S24LE3 S24LE S16LE; do
+	sed -i -E '/playback:/,/format:/ {/format:/ {s/(.*: ).*/\1'$format'/}}' $camilladspyml
+	camilladsp $camilladspyml &> /dev/null &
+	sleep 1
+	pgrep -x camilladsp &> /dev/null && break || format=
+done
+if [[ $format ]]; then
 	killall camilladsp
-	if [[ $format ]]; then
-		notify camilladsp CamillaDSP "Playback format: <wh>$format</wh>"
-		sed -i -n '/playback:/,/format:/ {/format:/ {s/:.*/: '$format'/; p}}' $dirconfigs/default_config.yml
-	fi
+	notify camilladsp CamillaDSP "Playback format: <wh>$format</wh>"
 else
 	dsp=
 	notify camilladsp CamillaDSP "Setting failed: <wh>Playback format</wh>" 10000
