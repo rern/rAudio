3f3da921ae633ca76a08cbb288d072801929e7d3
rern <rernrern@gmail.com>
2023-09-22 14:04:25 +0700

    Update status-coverartupnp.py

--- srv/http/bash/status-coverartupnp.py
+++ srv/http/bash/status-coverartupnp.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 
 import socket
-import sys
 import upnpp
 
 device = socket.gethostname() +'-UPnP/AV'
-srv = upnpp.findTypedService( device, 'avtransport', True )
-if not srv: sys.exit() # AVTransport service not found
-    
-retdata = upnpp.runaction( srv, 'GetMediaInfo', ['0'] )
-metadata = retdata[ 'CurrentURIMetaData' ]
-if not metadata: sys.exit()
-
-dirc = upnpp.UPnPDirContent()
-dirc.parse( metadata )
-if dirc.m_items.size() == 0: sys.exit()
-
-mprops = dirc.m_items[0].m_props
-if 'upnp:albumArtURI' in mprops: print( mprops['upnp:albumArtURI'] )
+srv = upnpp.findTypedService( device, 'avtransport', True ) # AVTransport service
+if srv:
+    retdata = upnpp.runaction( srv, 'GetMediaInfo', ['0'] )
+    metadata = retdata[ 'CurrentURIMetaData' ]
+    if metadata:
+        dirc = upnpp.UPnPDirContent()
+        dirc.parse( metadata )
+        if dirc.m_items.size():
+            mprops = dirc.m_items[0].m_props
+            if 'upnp:albumArtURI' in mprops: print( mprops['upnp:albumArtURI'] )
