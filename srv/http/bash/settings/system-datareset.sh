db658c1fd2745f7cd73e151f35e475b92116dd81
rern <rernrern@gmail.com>
2023-11-28 21:16:38 +0700

    Update system-datareset.sh

--- srv/http/bash/settings/system-datareset.sh
+++ srv/http/bash/settings/system-datareset.sh
@@ -92,7 +92,7 @@ fi
 
 # data directories
 mkdir -p $dirdata/{addons,audiocd,bookmarks,camilladsp,lyrics,mpd,mpdconf,playlists,system,webradio,webradio/img} /mnt/MPD/{NAS,SD,USB}
-mkdir -p $dircamilladsp/{coeffs,configs,configs-bt}
+[[ -e /usr/bin/camilladsp ]] && mkdir -p $dircamilladsp/{coeffs,configs,configs-bt}
 ln -sf /dev/shm $dirdata
 ln -sf /mnt /srv/http/
 chown -h http:http $dirshm /srv/http/mnt
