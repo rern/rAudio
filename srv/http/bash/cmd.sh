67da94cf60281d9e79f58cb43374ce590a3f3053
rern <rernrern@gmail.com>
2024-02-16 13:02:56 +0700

    Update cmd.sh

--- srv/http/bash/cmd.sh
+++ srv/http/bash/cmd.sh
@@ -539,7 +539,7 @@ mpcplayback )
 	radioStop
 	if [[ $ACTION == play ]]; then
 		mpc -q $ACTION
-		if [[ $( mpc -f %file% playlist | sed -n "$( mpc status %songpos% ) {s/:.*//; p}" ) == cdda ]]; then
+		if [[ $( sed -E -n '/^file/ {s/^file="|:.*//g; p}' $dirshm/status ) == cdda ]]; then
 			touch $dirshm/cdstart
 			( sleep 20 && rm -f $dirshm/cdstart ) &
 			notify 'audiocd blink' 'Audio CD' 'Start play ...'
