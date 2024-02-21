dd06e287e292879ddb3ff0c1e35c4d8d5837ba01
rern <rernrern@gmail.com>
2024-02-21 09:47:56 +0700

    Update player-devices.sh

--- srv/http/bash/settings/player-devices.sh
+++ srv/http/bash/settings/player-devices.sh
@@ -3,24 +3,26 @@
 ### included by <<< player-conf.sh
 ! type -t args2va &> /dev/null && . /srv/http/bash/common.sh # if run directly
 
+echo 'defaults.pcm.card 0
+defaults.ctl.card 0' > /etc/asound.conf # reset in case card number missing
+
 cardfiles=$( ls -1d /proc/asound/card[0-9] )
 while read path; do
-	card=${path: -1}
+	CARD=${path: -1}
 	line=$( cat $path/*/info \
 				| head -6 \
 				| sed -E -n '/^device|^name/ {s/^.*: //; s/bcm2835/On-board/; p}' \
 				| tr '\n' ^ )
-	device=$( cut -d^ -f1 <<< $line )
-	name=$( cut -d^ -f2 <<< $line )
-	usbmixerfile=$path/usbmixer
-	if [[ -e $usbmixerfile ]]; then
-		usbname=$( sed -n -E '/^Card/ {s/^Card: | at .*//g; p}' $usbmixerfile )
-		[[ $usbname ]] && name=$usbname
+	DEVICE=$( cut -d^ -f1 <<< $line )
+	NAME=$( cut -d^ -f2 <<< $line )
+	if [[ -e $path/usbmixer ]]; then
+		usbname=$( sed -n -E '/^Card/ {s/^Card: | at .*//g; p}' $path/usbmixer )
+		[[ $usbname ]] && NAME=$usbname
 	fi
-	lastword=$( awk '{print $NF}' <<< $name )
-	[[ $lastword == *-* && $lastword =~ ^[a-z0-9-]+$ ]] && name=$( sed 's/ [^ ]*$//' <<< $name )
-	LISTDEVICE+=', "'$name'"'
-	list+="$card^$device^$name"$'\n'
+	lastword=$( awk '{print $NF}' <<< $NAME )
+	[[ $lastword == *-* && $lastword =~ ^[a-z0-9-]+$ ]] && NAME=$( sed 's/ [^ ]*$//' <<< $NAME )
+	LISTDEVICE+=', "'$NAME'"'
+	list+="$CARD^$DEVICE^$NAME"$'\n'
 done <<< $cardfiles
 
 if [[ $usbdac != add && -e $dirsystem/output-device ]]; then
@@ -31,16 +33,11 @@ if [[ $usbdac != add && -e $dirsystem/output-device ]]; then
 		DEVICE=$( cut -d^ -f2 <<< $cdn )
 		NAME=$( cut -d^ -f3 <<< $cdn )
 	else
-		rm $dirsystem/output-device # non-exist
+		rm $dirsystem/output-device # remove if not exist
 	fi
 fi
-if [[ ! $CARD ]]; then # last card
-	CARD=$card
-	DEVICE=$device
-	NAME=$name
-fi
-
-if grep -qi wm5102 <<< $NAME; then
+#card N: RPiCirrus [RPi-Cirrus], device 0: WM5102 AiFi wm5102-aif1-0 [WM5102 AiFi wm5102-aif1-0]
+if grep -qiE 'WM5102|Cirrus' <<< $NAME; then
 	MIXER='HPOUT2 Digital'
 	LISTMIXER=", 'HPOUT1 Digital', 'HPOUT2 Digital', 'SPDIF Out', 'Speaker Digital'"
 else
