fbb7513518b9ccb9f152fd4cf8127b0779ae50df
rern <rernrern@gmail.com>
2024-02-27 12:17:49 +0700

    Update camilla-bluetooth.sh

--- srv/http/bash/settings/camilla-bluetooth.sh
+++ srv/http/bash/settings/camilla-bluetooth.sh
@@ -12,47 +12,38 @@ if [[ -e $dirsystem/camilla-bt$type ]]; then
 else                                         # create yml file if not exist
 	configbt=$dircamilladsp/configs-bt/$type.yml
 	echo $configbt > $dirsystem/camilla-bt$type
-	if [[ $type == receiver ]]; then
-		sed -E '/playback:$/,/filters:$/ s/(device: ).*/\1bluealsa/' "$configfile" > "$configbt"
-	else
-		sed -E -e 's/(chunksize: ).*/\14096/
-s/(enable_rate_adjust: )/\1true/
-s/(target_level: )/\18000/
-s/(adjust_period: )/\13/
-s/(enable_resampling: )/\1true/
-' -e '/capture:$/,/playback:$/ s/(type: ).*/\1Bluez/; /device: .*/ d
-' -e '/playback:$/ i\
-dbus_path: /org/bluealsa/hci0/dev_A0_B1_C2_D3_E4_F5/a2dpsnk/source
-' "$configfile" > "$configbt"
-	fi
 fi
 
 cp $etcdefault{,.backup}
 sed -i -E 's|^(CONFIG=).*|\1"'$configbt'"|' $etcdefault
 
-fch=( $( bluealsa-aplay -L | awk '/channel.*Hz/ {print $3" "$4" "$6}' ) )
-format=${fch[0]/_} # remove underscore
-channels=${fch[1]}
-samplerate=${fch[2]}
+. <( bluealsa-aplay -L | awk '/channel.*Hz/ {print "format="$3"\nchannels="$4"\nsamplerate="$6}' )
+format=$( sed 's/_3LE/LE3/; s/FLOAT_LE/FLOAT32LE/; s/_//g' <<< $format )
 
 if [[ $type == receiver ]]; then
-	sed -i -E -e '/playback:$/,/filters:$/ {
+	sed -E -e '/playback:$/,/format:/ {
+s/(device: ).*/\1bluealsa/
 s/(channels: ).*/\1'$channels'/
 s/(format: ).*/\1'$format'/
-}
-' $configbt
+}' "$configfile" > $configbt
 else
 	dbuspath=$( gdbus introspect \
 					--recurse \
 					--system \
 					--dest org.bluealsa \
 					--object-path /org/bluealsa \
-						| awk '/node.*source {$/ {print $2}' )
-	sed -i -E -e 's/(samplerate: ).*/\1'$samplerate'/
-' -e '/capture:$/,/playback:$/ {
-s/(channels: ).*/\1'$channels'/
-s/(format: ).*/\1'$format'/
-s|(dbus_path: ).*|\1'$dbuspath'|
-}
-' $configbt
+						| awk '/node.*source {$/ {print $2}' ) # /org/bluealsa/hci0/dev_A0_B1_C2_D3_E4_F5/a2dpsnk/source
+	sed -E -e 's/(samplerate: ).*/\1'$samplerate'/
+s/(chunksize: ).*/\14096/
+s/(enable_rate_adjust: )/\1true/
+s/(target_level: )/\18000/
+s/(adjust_period: )/\13/
+s/(enable_resampling: )/\1true/
+' -e '/capture:$/,/format:/ {
+	/device: .*/ d
+	s/(type: ).*/\1Bluez/
+	s/(channels: ).*/\1'$channels'/
+	s/(format: ).*/\1'$format'/
+	s|(dbus_path: ).*|\1'$dbuspath'|
+}' "$configfile" > $configbt
 fi
