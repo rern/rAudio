e3d53db3e0b9a711b9ddb960dd78679320a457c1
rern <rernrern@gmail.com>
2024-03-09 08:32:55 +0700

    Update system-storage.sh

--- srv/http/bash/settings/system-storage.sh
+++ srv/http/bash/settings/system-storage.sh
@@ -37,14 +37,14 @@ if [[ ! -e $filesharedip ]]; then
 		done <<< $usb
 	fi
 fi
-nas=$( grep -E '/mnt/MPD/NAS|/srv/http/data' /etc/fstab | tr -s ' ' )
+nas=$( grep -E '/mnt/MPD/NAS|/srv/http/data' /etc/fstab )
 if [[ $nas ]]; then
-	nas=$( cut -d' ' -f1-2 <<< $nas | sort )
+	nas=$( awk '{print $1" "$2}' <<< $nas | sort )
 	while read line; do
 		source=${line/ *}
-		source=${source//\\040/ }
+		source=${source//\040/ }
 		mountpoint=${line/* }
-		mountpoint=${mountpoint//\\040/ }
+		mountpoint=${mountpoint//\040/ }
 		used_size=( $( timeout 0.1s df -h --output=used,size,source | grep "$source" ) )
 		list+=',{
   "icon"       : "networks"
