3f1bea9a936221d65c7f5429e7fb13daefaca7df
rern <rernrern@gmail.com>
2024-03-17 09:20:19 +0700

    Update system.sh

--- srv/http/bash/settings/system.sh
+++ srv/http/bash/settings/system.sh
@@ -567,9 +567,9 @@ timezone )
 	if [[ $TIMEZONE == auto ]]; then
 		! ipOnline 8.8.8.8 && notify timezone 'Timezone - Auto' 'No internet connection.' && exit
 		
-		tz=$( curl -s https://ipapi.co/timezone )
-		[[ ! $tz ]] && tz=$( curl -s http://ip-api.com | grep '"timezone"' | cut -d'"' -f4 )
-		[[ ! $tz ]] && tz=$( curl -s https://worldtimeapi.org/api/ip | jq -r .timezone )
+		tz=$( curl -s -m 2 https://worldtimeapi.org/api/ip | jq -r .timezone )
+		[[ ! $tz ]] && tz=$( curl -s -m 2 http://ip-api.com | grep '"timezone"' | cut -d'"' -f4 )
+		[[ ! $tz ]] && tz=$( curl -s -m 2 https://ipapi.co/timezone )
 		[[ ! $tz ]] && tz=UTC
 		timedatectl set-timezone $tz
 	else
