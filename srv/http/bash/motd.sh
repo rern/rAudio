41ebc54181c130a9d13be35c46f657665ca1e5bc
rern <rernrern@gmail.com>
2024-03-19 15:27:44 +0700

    Update motd.sh

--- srv/http/bash/motd.sh
+++ srv/http/bash/motd.sh
@@ -5,3 +5,5 @@ bg='\e[30m\e[46m'
 printf "$bg%*s$def\n" $COLUMNS
 printf "$bg%-${COLUMNS}s$def\n" "   r A u d i o"
 printf "$bg%*s$def\n\n" $COLUMNS
+export PATH=/srv/http/bash:/srv/http/bash/settings:$PATH
+. /srv/http/bash/common.sh
