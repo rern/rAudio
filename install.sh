f3fcbbb7ef60266c2b4a54bd69a224e679fa14a0
rern <rernrern@gmail.com>
2024-05-05 09:33:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,17 @@ alias=r1
 
 . /srv/http/bash/settings/addons.sh
 
+# 20240505
+file=/srv/http/data/mpdconf/conf/snapserver.conf
+if grep -q snapcast $file; then
+	echo 'audio_output {
+	name    "SnapServer"
+	type    "fifo"
+	path    "/tmp/snapfifo"
+	format  "48000:16:2"
+}' > $file
+fi
+
 # 20240315
 [[ -e /usr/bin/iwctl ]] && groupadd -f netdev
 
