1ba5d9c448823b64d534d9e6a0c2c85bd075674d
rern <rernrern@gmail.com>
2024-05-26 19:48:03 +0700

    Update status.sh

--- srv/http/bash/status.sh
+++ srv/http/bash/status.sh
@@ -134,7 +134,7 @@ $( $dirbash/status-bluetooth.sh )"
 		;;
 	snapcast )
 		serverip=$( < $dirshm/snapserverip )
-		serverstatus=$( websocat ws://$serverip:8080 <<< '{ "filesh": [ "/srv/http/bash/status.sh", "snapclient" ], "get": True }' )
+		serverstatus=$( websocat ws://$serverip:8080 <<< '{ "filesh": [ "/srv/http/bash/status.sh", "snapclient" ], "get": true }' )
 ########
 		status+="
 $( sed -E  -e '1,3d; $d
