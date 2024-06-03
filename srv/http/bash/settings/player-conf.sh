564429ae8eaebcb0755157e0a9c281d54dce6252
rern <rernrern@gmail.com>
2024-06-03 18:41:24 +0700

    Update player-conf.sh

--- srv/http/bash/settings/player-conf.sh
+++ srv/http/bash/settings/player-conf.sh
@@ -73,6 +73,7 @@ $AUDIOOUTPUTBT
 fi
 if [[ $CARD == -1 ]]; then # no audio devices
 	rm -f $dirmpdconf/{output,soxr}.conf
+	[[ ! $AUDIOOUTPUTBT ]] && ln -sf $dirmpdconf/{conf/,}httpd.conf # set as output to allow play
 	if [[ $usbdac == remove ]]; then
 		pushData display '{ "volumenone": true }'
 		pushData refresh '{ "page": "features", "nosound": true }'
