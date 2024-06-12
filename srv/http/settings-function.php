948a4e1fb8dd9da43841aca87ca28da0e0d87cb5
rern <rernrern@gmail.com>
2024-06-12 21:03:00 +0700

    u

---
+++ srv/http/settings-function.php
@@ -0,0 +1,149 @@
+<?php
+//----------------------------------------------------------------------------------
+/*
+$id_data = [ 'ID' => [               // REQUIRED
+	  'label'   => 'LABEL'
+	, 'sub'     => 'SUBLABEL'
+	, 'setting' => 'TYPE'
+	, 'status'  => 'SCRIPTCOMMAND'
+];
+$head = [
+	  'title'   => 'TITLE'           // REQUIRED
+	, 'subhead' => true/false        // with no help icon
+	, 'status'  => 'COMMAND'         // include status icon and status box
+	, 'button'  => [ 'ICON', ... ]   // icon button
+	, 'back'    => true/false        // back button
+	, 'nohelp'  => true/false
+	, 'help'    => 'HELP'
+];
+$body = [
+	 'HTML'                          // for non-switch section
+	, [
+		  'id'       => 'ID'         // REQUIRED
+		, 'input'    => 'HTML/ID'    // alternative - if not switch (ID - select)
+		, 'setting'  => TYPE         // default  = $( '#setting-'+ id ).click() before enable
+		                             // false    = no setting
+		                             // 'custom' = custom setting
+		                             // 'none'   = no setting - custom enable
+		, 'disabled' => 'MESSAGE'    // set data-diabled - prompt on click setting
+		                             // 'js'     = set by js condition
+		, 'help'     => 'HELP'
+		, 'exist'    => 'COMMAND'    // hide if COMMAND = false
+	]
+	, ...
+];
+htmlSection( $head, $body[, $id] );
+*/
+function htmlHead( $data ) {
+	if ( isset( $data[ 'exist' ] ) && ! $data[ 'exist' ] ) return;
+	
+	$title   = $data[ 'title' ];
+	$id      = $data[ 'id' ] ?? false;
+	$status  = $data[ 'status' ] ?? false;
+	$button  = $data[ 'button' ] ?? false;
+	$help    = $data[ 'help' ] ?? false;
+	$class   = $status ? 'status' : '';
+	
+	$html    = '<heading '.( $id ? ' id="'.$id.'"' : '' ).( $status ? ' data-status="'.$status.'"' : '' );
+	$html   .= $class ? ' class="'.$class.'">' : '>';
+	$html   .= '<span class="headtitle">'.$title.'</span>';
+	if ( $button ) {
+		if ( is_Array( $button ) ) {
+			foreach( $button as $icon ) $html.= i( $icon );
+		} else {
+			$html.= i( $button );
+		}
+	}
+	$html   .= isset( $data[ 'nohelp' ] ) ? '' : i( 'help help' );
+	$html   .= isset( $data[ 'back' ] ) ? i( 'back back' ) : '';
+	$html   .= '</heading>';
+	$html   .= $help ? '<span class="helpblock hide">'.$help.'</span>' : '';
+	$html   .= $status ? '<pre id="code'.$status.'" class="status hide"></pre>' : '';
+	echo str_replace( '|', '<g>|</g>', $html );
+}
+function htmlSection( $head, $body, $id = '' ) {
+	$html = '<div';
+	$html.= $id ? ' id="div'.$id.'"' : '';
+	$html.= ' class="section">';
+	echo $html;
+	if ( $head ) htmlHead( $head );
+	foreach( $body as $data ) {
+		if ( is_array( $data ) ) {
+			htmlSetting( $data );
+		} else {
+			echo $data;
+		}
+	}
+	echo '</div>';
+}
+function htmlSectionStatus( $id, $labels = '', $values = '', $help = '' ) {
+	if ( ! $labels ) $labels = '&nbsp;';
+	if ( $help ) $help = '<div class="helpblock hide">'.$help.'</div>';
+	return '
+<div id="div'.$id.'" class="row">
+<div class="col-l text label gr">'.$labels.'</div>
+<div class="col-r text value">'.$values.'</div>
+'.$help.'
+</div>';
+}
+function htmlSetting( $data ) {
+	global $id_data;
+	$id          = $data[ 'id' ];
+	$iddata      = $id_data[ $id ];
+	if ( isset( $iddata[ 'exist' ] ) && ! file_exists( '/usr/bin/'.$iddata[ 'exist' ] ) ) return;
+	
+	if ( isset( $data[ 'html' ] ) ) {
+		echo str_replace( '|', '<g>|</g>', $data[ 'html' ] );
+		return;
+	}
+	
+	global $features, $system;
+	$id          = $data[ 'id' ];
+	$iddata      = $id_data[ $id ];
+	$label       = $iddata[ 'label' ];
+	$sublabel    = $iddata[ 'sub' ] ?? false;
+	$status      = $iddata[ 'status' ] ?? false;
+	$setting     = $iddata[ 'setting' ] ?? 'common';
+	$label       = '<span class="label">'.$label.'</span>';
+	$input       = $data[ 'input' ] ?? false;
+	$settingicon = ! $setting || $setting === 'none' ? false : 'gear';
+	$help        = $data[ 'help' ] ?? false;
+	$icon        = $data[ 'icon' ] ?? false;
+	if ( $features || $system ) {
+		$icon = $id;
+	} else {
+		$icon = $data[ 'icon' ] ?? false;
+	}
+	
+	$html        = '<div id="div'.$id.'" class="row">';
+	// col-l
+	$html       .= '<div class="col-l';
+	$html       .= $sublabel ? '' : ' single';
+	$html       .= $status ? ' status" data-status="'.$id.'">' : '">';
+	$html       .= $sublabel ? '<a>'.$label.'<gr>'.$sublabel.'</gr></a>' : $label;
+	$html       .= $icon ? i( $icon ) : ''; // icon
+	$html       .= '</div>';
+	// col-r
+	$html       .= '<div class="col-r">';
+	if ( ! $input ) {
+		$disabled = isset( $data[ 'disabled' ] ) ? '<span class="hide">'.$data[ 'disabled' ].'</span>' : '';
+		$html    .= '<label>'.$disabled.'<input type="checkbox" id="'.$id.'" class="switch '.$setting.'"><div class="switchlabel"></div></label>';
+	} else {
+		if ( ltrim( $input )[ 0 ] == '<' ) {
+			$html.= $input;
+		} else { // select
+			$html.= '<select id="'.$input.'"></select>';
+		}
+	}
+	// setting
+	$html       .= $settingicon ? i( $settingicon.' setting', 'setting-'.$id ) : '';
+	// help
+	$html       .= $help ? '<span class="helpblock hide">'.$help.'</span>' : '';
+	$html       .= '</div>
+					</div>';
+	// status
+	$html       .= $status ? '<pre id="code'.$id.'" class="status hide"></pre>' : '';
+	if ( isset( $data[ 'returnhtml' ] ) ) return $html;
+	
+	echo $html;
+}
--- srv/http/settings.php
+++ srv/http/settings.php
@@ -1,5 +1,29 @@
 <?php
 include 'common.php';
+if ( ! $addons ) include 'settings-function.php';
+
+echo '
+	<div class="head">'.i( $icon.' page-icon' ).'<span class="title">'.$title.'</span>'.i( 'close close' ).i( 'help helphead' ).'</div>
+	<div class="container '.$page.' hide">
+';
+if ( $addonsprogress || $guide ) {
+	echo '
+	</div>
+</body>
+</html>
+';
+	exit;
+//----------------------------------------------------------------------------------
+}
+
+$btn     = [ 'add',     'bluetooth', 'btsender', 'code',    'gear',     'lan',    'lastfm',   'microsd', 'networks'
+			,'pause',   'play',      'power',    'refresh', 'search',   'stop',   'usbdrive', 'volume',  'wifi' ];
+$btnc    = [ 'filters', 'flowchart', 'graph',    'input',   'inverted', 'linear', 'mixers',   'output',  'set0' ];
+if ( $camilla ) $btn = array_merge( $btn, $btnc );
+foreach( $btn as $b ) {
+	$name  = 'b_'.$b;
+	$$name = i( $b.' btn' );
+}
 
 function iLabel( $label, $icon = '' ) {
 	$htmlicon = $icon ? i( $icon ) : '&emsp;';
@@ -12,205 +36,33 @@ function menu( $icon, $name, $iconsub = '' ) {
 	$submenu = $iconsub ? i( $iconsub.' sub' ) : '';
 	return '<a class="helpmenu">'.i( $icon ).' '.$name.$submenu.'</a>';
 }
-$btn  = [ 'add',     'bluetooth', 'btsender', 'code',    'gear',     'lan',    'lastfm',   'microsd', 'networks'
-		 ,'pause',   'play',      'power',    'refresh', 'search',   'stop',   'usbdrive', 'volume',  'wifi' ];
-$btnc = [ 'filters', 'flowchart', 'graph',    'input',   'inverted', 'linear', 'mixers',   'output',  'set0' ];
-if ( $camilla ) $btn = array_merge( $btn, $btnc );
-foreach( $btn as $b ) {
-	$name  = 'b_'.$b;
-	$$name = i( $b.' btn' );
-}
-
 // functions for use inside heredoc
 $Fi      = 'i';
 $FiLabel = 'iLabel';
 $FiTab   = 'iTab';
 $Fmenu   = 'menu';
 
-echo '
-	<div class="head">'.i( $icon.' page-icon' ).'<span class="title">'.$title.'</span>'.i( 'close close' ).i( 'help helphead' ).'</div>
-	<div class="container '.$page.' hide">
-';
-
-if ( ! $addons ) include 'settings/'.$page.'.php';
-
-echo '
-	</div>
-';
-if ( $addonsprogress || $guide ) {
-	echo '
-</body>
-</html>
-';
-	exit;
-//----------------------------------------------------------------------------------
-}
-// .................................................................................
+if ( ! $addons ) include 'settings/'.$page.'.php'; // addons: by addons.js
 
 // bottom bar
-$htmlbar = '<div id="bar-bottom">';
-$prefix  = '';
+$htmlbar = '';
 if ( $camilla ) {
 	$tabs   = [ 'Filters', 'Mixers', 'Processors', 'Pipeline', 'Devices', 'Config' ];
 	$prefix = 'tab';
 } else {
 	$tabs   = [ 'Features', 'Player', 'Networks', 'System', 'Addons' ];
+	$prefix = '';
 }
 foreach ( $tabs as $tab ) {
 	$id      = strtolower( $tab );
-	$htmlbar.= '<div id="'.$prefix.$id.'">'.i( $id ).'<a> '.$tab.'</a></div>';
+	$htmlbar.= '<div id="'.$prefix.$id.'">'.i( $id ).' <a>'.$tab.'</a></div>';
 }
-$htmlbar.= '</div>';
-echo $htmlbar;
+echo '
+	</div>
+	<div id="bar-bottom">
+		'.$htmlbar.'
+	</div>
+';
 
 // <script> -----------------------------------------------------
 echo $scripts;
-if ( $addons ) exit;
-//----------------------------------------------------------------------------------
-/*
-$id_data = [ 'ID' => [               // REQUIRED
-	  'label'   => 'LABEL'
-	, 'sub'     => 'SUBLABEL'
-	, 'setting' => 'TYPE'
-	, 'status'  => 'SCRIPTCOMMAND'
-];
-$head = [
-	  'title'   => 'TITLE'           // REQUIRED
-	, 'subhead' => true/false        // with no help icon
-	, 'status'  => 'COMMAND'         // include status icon and status box
-	, 'button'  => [ 'ICON', ... ]   // icon button
-	, 'back'    => true/false        // back button
-	, 'nohelp'  => true/false
-	, 'help'    => 'HELP'
-];
-$body = [
-	 'HTML'                          // for non-switch section
-	, [
-		  'id'       => 'ID'         // REQUIRED
-		, 'input'    => 'HTML/ID'    // alternative - if not switch (ID - select)
-		, 'setting'  => TYPE         // default  = $( '#setting-'+ id ).click() before enable
-		                             // false    = no setting
-		                             // 'custom' = custom setting
-		                             // 'none'   = no setting - custom enable
-		, 'disabled' => 'MESSAGE'    // set data-diabled - prompt on click setting
-		                             // 'js'     = set by js condition
-		, 'help'     => 'HELP'
-		, 'exist'    => 'COMMAND'    // hide if COMMAND = false
-	]
-	, ...
-];
-htmlSection( $head, $body[, $id] );
-*/
-function htmlHead( $data ) {
-	if ( isset( $data[ 'exist' ] ) && ! $data[ 'exist' ] ) return;
-	
-	$title   = $data[ 'title' ];
-	$id      = $data[ 'id' ] ?? false;
-	$status  = $data[ 'status' ] ?? false;
-	$button  = $data[ 'button' ] ?? false;
-	$help    = $data[ 'help' ] ?? false;
-	$class   = $status ? 'status' : '';
-	
-	$html    = '<heading '.( $id ? ' id="'.$id.'"' : '' ).( $status ? ' data-status="'.$status.'"' : '' );
-	$html   .= $class ? ' class="'.$class.'">' : '>';
-	$html   .= '<span class="headtitle">'.$title.'</span>';
-	if ( $button ) {
-		if ( is_Array( $button ) ) {
-			foreach( $button as $icon ) $html.= i( $icon );
-		} else {
-			$html.= i( $button );
-		}
-	}
-	$html   .= isset( $data[ 'nohelp' ] ) ? '' : i( 'help help' );
-	$html   .= isset( $data[ 'back' ] ) ? i( 'back back' ) : '';
-	$html   .= '</heading>';
-	$html   .= $help ? '<span class="helpblock hide">'.$help.'</span>' : '';
-	$html   .= $status ? '<pre id="code'.$status.'" class="status hide"></pre>' : '';
-	echo str_replace( '|', '<g>|</g>', $html );
-}
-function htmlSection( $head, $body, $id = '' ) {
-	$html = '<div';
-	$html.= $id ? ' id="div'.$id.'"' : '';
-	$html.= ' class="section">';
-	echo $html;
-	if ( $head ) htmlHead( $head );
-	foreach( $body as $data ) {
-		if ( is_array( $data ) ) {
-			htmlSetting( $data );
-		} else {
-			echo $data;
-		}
-	}
-	echo '</div>';
-}
-function htmlSectionStatus( $id, $labels = '', $values = '', $help = '' ) {
-	if ( ! $labels ) $labels = '&nbsp;';
-	if ( $help ) $help = '<div class="helpblock hide">'.$help.'</div>';
-	return '
-<div id="div'.$id.'" class="row">
-<div class="col-l text label gr">'.$labels.'</div>
-<div class="col-r text value">'.$values.'</div>
-'.$help.'
-</div>';
-}
-function htmlSetting( $data ) {
-	global $id_data;
-	$id          = $data[ 'id' ];
-	$iddata      = $id_data[ $id ];
-	if ( isset( $iddata[ 'exist' ] ) && ! file_exists( '/usr/bin/'.$iddata[ 'exist' ] ) ) return;
-	
-	if ( isset( $data[ 'html' ] ) ) {
-		echo str_replace( '|', '<g>|</g>', $data[ 'html' ] );
-		return;
-	}
-	
-	global $features, $system;
-	$id          = $data[ 'id' ];
-	$iddata      = $id_data[ $id ];
-	$label       = $iddata[ 'label' ];
-	$sublabel    = $iddata[ 'sub' ] ?? false;
-	$status      = $iddata[ 'status' ] ?? false;
-	$setting     = $iddata[ 'setting' ] ?? 'common';
-	$label       = '<span class="label">'.$label.'</span>';
-	$input       = $data[ 'input' ] ?? false;
-	$settingicon = ! $setting || $setting === 'none' ? false : 'gear';
-	$help        = $data[ 'help' ] ?? false;
-	$icon        = $data[ 'icon' ] ?? false;
-	if ( $features || $system ) {
-		$icon = $id;
-	} else {
-		$icon = $data[ 'icon' ] ?? false;
-	}
-	
-	$html        = '<div id="div'.$id.'" class="row">';
-	// col-l
-	$html       .= '<div class="col-l';
-	$html       .= $sublabel ? '' : ' single';
-	$html       .= $status ? ' status" data-status="'.$id.'">' : '">';
-	$html       .= $sublabel ? '<a>'.$label.'<gr>'.$sublabel.'</gr></a>' : $label;
-	$html       .= $icon ? i( $icon ) : ''; // icon
-	$html       .= '</div>';
-	// col-r
-	$html       .= '<div class="col-r">';
-	if ( ! $input ) {
-		$disabled = isset( $data[ 'disabled' ] ) ? '<span class="hide">'.$data[ 'disabled' ].'</span>' : '';
-		$html    .= '<label>'.$disabled.'<input type="checkbox" id="'.$id.'" class="switch '.$setting.'"><div class="switchlabel"></div></label>';
-	} else {
-		if ( ltrim( $input )[ 0 ] == '<' ) {
-			$html.= $input;
-		} else { // select
-			$html.= '<select id="'.$input.'"></select>';
-		}
-	}
-	// setting
-	$html       .= $settingicon ? i( $settingicon.' setting', 'setting-'.$id ) : '';
-	// help
-	$html       .= $help ? '<span class="helpblock hide">'.$help.'</span>' : '';
-	$html       .= '</div>
-					</div>';
-	// status
-	$html       .= $status ? '<pre id="code'.$id.'" class="status hide"></pre>' : '';
-	if ( isset( $data[ 'returnhtml' ] ) ) return $html;
-	
-	echo $html;
-}
