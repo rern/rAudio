22b7e5fafb3f7f86d121a0e9d63a1711c4c1f93f
rern <rernrern@gmail.com>
2024-06-16 10:39:03 +0700

    Update bluetoothbutton.sh

--- srv/http/bash/bluetoothbutton.sh
+++ srv/http/bash/bluetoothbutton.sh
@@ -14,13 +14,19 @@ event=$( sed -n "/$mac/,/^H:/ {/^H:/ p}" /proc/bus/input/devices | awk '{print $
 
 # line='Event: time nnnnnnnnnn.nnnnnn, type 1 (EV_KEY), code NNN (KEY_XXXXXX), value N'
 evtest /dev/input/$event | while read line; do
-	! grep -q 1$ <<< $line && continue # 1st of multiple 'value N'
+	! grep -q 1$ <<< $line && continue                     # 1st of multiple 'value N'
 	
 	! grep -Eq 'KEY_.*CD|KEY_.*SONG' <<< $line && continue # PLAYCD PAUSECD STOPCD NEXTSONG PREVIOUSSONG
 	
 	key=$( sed -E 's/.*KEY_|\).*//g; s/CD|SONG//; s/.*/\L&/' <<< $line )
 	case $key in
-		next | prev )
+		play | pause )
+			$dirbash/cmd.sh mpcplayback
+			;;
+		stop )
+			$dirbash/cmd.sh mpcplayback$'\n'stop$'\nCMD ACTION'
+			;;
+		next | previous )
 			. <( mpc status 'current=%songpos%; length=%length% random=%random%' )
 			if [[ $key == next ]]; then
 				(( $current == $length )) && pos=1 || pos=$(( current + 1 ))
@@ -29,7 +35,5 @@ evtest /dev/input/$event | while read line; do
 			fi
 			$dirbash/cmd.sh mpcskip$'\n'$pos$'\nCMD POS'
 			;;
-		play | pause ) $dirbash/cmd.sh mpcplayback;;
-		stop )         $dirbash/cmd.sh mpcplayback$'\n'stop$'\nCMD ACTION';;
 	esac
 done
