f6b2df00e6ac800fb981fb9d26efc7819b65b097
rern <rernrern@gmail.com>
2024-06-17 09:09:06 +0700

    u

--- srv/http/assets/js/settings.js
+++ srv/http/assets/js/settings.js
@@ -355,6 +355,7 @@ $( '.container' ).on( 'click', '.status .headtitle, .col-l.status', function() {
 	var id    = $this.hasClass( 'col-l' ) ? $this.data( 'status' ) : $this.parent().data( 'status' );
 	var $code = $( '#code'+ id );
 	$code.hasClass( 'hide' ) ? currentStatus( id ) : $code.addClass( 'hide' );
+	$this.toggleClass( 'active' );
 } );
 $( '.playback' ).on( 'click', function() { // for player and camilla
 	S.state = S.state === 'play' ? 'pause' : 'play';
