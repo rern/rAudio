a0be0be6974bf4fbb30b12857693a325c0c2d65c
rern <rernrern@gmail.com>
2024-07-12 21:39:41 +0700

    Update system-storage.sh

--- srv/http/bash/settings/system-storage.sh
+++ srv/http/bash/settings/system-storage.sh
@@ -2,15 +2,22 @@
 
 . /srv/http/bash/common.sh
 
+size() {
+	path=$1
+	[[ $path == / ]] && target_source=target || target_source=source
+	# timeout for network shares
+	timeout 1 df -H --output=used,size,$target_source \
+		| grep "$path$" \
+		| awk '{print $1"B/"$2"B"}'
+}
+
 if mount | grep -q -m1 'mmcblk0p2 on /'; then
-	used_size=( $( df -lh --output=used,size,target | grep '/$' ) )
-	[[ $used_size ]] && size=${used_size[0]}B/${used_size[1]}B || size=
 	list+=',{
 "icon"       : "microsd"
 , "mountpoint" : "/<g>mnt/MPD/SD</g>"
 , "mounted"    : true
 , "source"     : "/dev/mmcblk0p2"
-, "size"       : "'$size'"
+, "size"       : "'$( size / )'"
 }'
 fi
 usb=$( mount | grep ^/dev/sd | cut -d' ' -f1 )
@@ -18,14 +25,12 @@ if [[ $usb ]]; then
 	while read source; do
 		mountpoint=$( df -l --output=target,source | sed -n "\|$source| {s| *$source||; p}" )
 		if [[ $mountpoint ]]; then
-			used_size=( $( df -lh --output=used,size,source | grep "$source" ) )
-			[[ $used_size ]] && size=${used_size[0]}B/${used_size[1]}B || size=
 			list+=',{
 "icon"       : "usbdrive"
 , "mountpoint" : "'$( stringEscape $mountpoint )'"
 , "mounted"    : true
 , "source"     : "'$source'"
-, "size"       : "'$size'"
+, "size"       : "'$( size "$source" )'"
 }'
 		else
 			label=$( e2label $source )
@@ -49,20 +54,13 @@ if [[ $nas ]]; then
 	while read line; do
 		source=${line/^*}
 		mountpoint=${line/*^}
-		if mountpoint -q "$mountpoint"; then
-			mounted=true
-			used_size=( $( timeout 1 df -h --output=used,size,source | grep "$source" ) )
-			size=${used_size[0]}B/${used_size[1]}B
-		else
-			mounted=false
-			size=
-		fi
+		mountpoint -q "$mountpoint" && mounted=true || mounted=false
 		list+=',{
   "icon"       : "networks"
 , "mountpoint" : "'$( stringEscape $mountpoint )'"
 , "mounted"    : '$mounted'
 , "source"     : "'$source'"
-, "size"       : "'$size'"
+, "size"       : "'$( [[ $mounted == true ]] && size "$source" )'"
 }'
 	done <<< $nas
 fi
