89dab0b741cdffc536f5c69866b719d86f49f5c0
rern <rernrern@gmail.com>
2024-07-12 20:38:32 +0700

    Update system-storage.sh

--- srv/http/bash/settings/system-storage.sh
+++ srv/http/bash/settings/system-storage.sh
@@ -2,48 +2,47 @@
 
 . /srv/http/bash/common.sh
 
-if [[ ! -e $filesharedip ]] || systemctl -q is-active nfs-server; then
-	if mount | grep -q -m1 'mmcblk0p2 on /'; then
-		used_size=( $( df -lh --output=used,size,target | grep '/$' ) )
-		[[ $used_size ]] && size=${used_size[0]}B/${used_size[1]}B || size=
-		list+=',{
-  "icon"       : "microsd"
+if mount | grep -q -m1 'mmcblk0p2 on /'; then
+	used_size=( $( df -lh --output=used,size,target | grep '/$' ) )
+	[[ $used_size ]] && size=${used_size[0]}B/${used_size[1]}B || size=
+	list+=',{
+"icon"       : "microsd"
 , "mountpoint" : "/<g>mnt/MPD/SD</g>"
 , "mounted"    : true
 , "source"     : "/dev/mmcblk0p2"
 , "size"       : "'$size'"
 }'
-	fi
-	usb=$( mount | grep ^/dev/sd | cut -d' ' -f1 )
-	if [[ $usb ]]; then
-		while read source; do
-			mountpoint=$( df -l --output=target,source | sed -n "\|$source| {s| *$source||; p}" )
-			if [[ $mountpoint ]]; then
-				used_size=( $( df -lh --output=used,size,source | grep "$source" ) )
-				[[ $used_size ]] && size=${used_size[0]}B/${used_size[1]}B || size=
-				list+=',{
-  "icon"       : "usbdrive"
+fi
+usb=$( mount | grep ^/dev/sd | cut -d' ' -f1 )
+if [[ $usb ]]; then
+	while read source; do
+		mountpoint=$( df -l --output=target,source | sed -n "\|$source| {s| *$source||; p}" )
+		if [[ $mountpoint ]]; then
+			used_size=( $( df -lh --output=used,size,source | grep "$source" ) )
+			[[ $used_size ]] && size=${used_size[0]}B/${used_size[1]}B || size=
+			list+=',{
+"icon"       : "usbdrive"
 , "mountpoint" : "'$( stringEscape $mountpoint )'"
 , "mounted"    : true
 , "source"     : "'$source'"
 , "size"       : "'$size'"
 }'
-			else
-				label=$( e2label $source )
-				[[ ! $label ]] && label=?
-				list+=',{
-  "icon"      : "usbdrive"
+		else
+			label=$( e2label $source )
+			[[ ! $label ]] && label=?
+			list+=',{
+"icon"      : "usbdrive"
 ,"mountpoint" : "'$dirusb/$label'"
 ,"mounted"    : false
 ,"source"     : "'$source'"
 }'
-			fi
-			[[ ! $hddapm ]] && hddapm=$( hdparm -B $source \
-											| grep -m1 APM_level \
-											| tr -d -c 0-9 )
-		done <<< $usb
-	fi
+		fi
+		[[ ! $hddapm ]] && hddapm=$( hdparm -B $source \
+										| grep -m1 APM_level \
+										| tr -d -c 0-9 )
+	done <<< $usb
 fi
+
 nas=$( grep -E '/mnt/MPD/NAS|/srv/http/data' /etc/fstab )
 if [[ $nas ]]; then
 	nas=$( awk '{print $1"^"$2}' <<< $nas | sed 's/\\040/ /g' | sort )
