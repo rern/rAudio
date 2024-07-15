e71f3a1c8a3eb968b22b2bb3f6e25a207792c041
rern <rernrern@gmail.com>
2024-07-15 14:07:04 +0700

    u

--- srv/http/settings.php
+++ srv/http/settings.php
@@ -30,6 +30,65 @@ foreach ( $tabs as $tab ) {
 	if ( ! $camilla ) ${'t_'.$id} = '<a class="helpmenu tab">'.i( $id ).' '.$tab.'</a>';
 }
 if ( ! $addons ) {
+	$btn     = [ 'add',     'bluetooth', 'btsender', 'code',    'gear',     'lan',    'lastfm',   'microsd', 'networks'
+				,'pause',   'play',      'power',    'refresh', 'search',   'stop',   'usbdrive', 'volume',  'wifi' ];
+	$btnc    = [ 'filters', 'flowchart', 'graph',    'input',   'inverted', 'linear', 'mixers',   'output',  'set0' ];
+	if ( $camilla ) $btn = array_merge( $btn, $btnc );
+	foreach( $btn as $b ) ${'b_'.$b} = i( $b.' btn' ); // btn
+	if ( $features ) {
+		$labels = [
+			  [ 'Equalizer',     'equalizer' ]
+			, [ 'DSP',           'camilladsp' ]
+			, [ 'Server rAudio', 'rserver' ]
+			, [ 'Shared Data',   'networks' ]
+			, [ 'Wi-Fi',         'wifi' ]
+		];
+		$menus = [
+			  [ 'features', 'Features', 'camilladsp' ]
+			, [ 'features', 'Features', 'equalizer' ]
+			, [ 'playlist', 'Playlist', 'multiraudio' ]
+			, [ 'player',   'Player',   'lock' ]
+			, [ 'power',    'Power',    'screenoff' ]
+		];
+	} else if ( $player ) {
+		$labels = [
+			  [ 'DAB Radio',   'dabradio' ]
+			, [ 'Device' ]
+			, [ 'Shared Data', 'networks' ]
+			, [ 'SoX Resampler' ]
+			, [ 'Volume Control' ]
+		];
+		$menus = '';
+	} else if ( $networks ) {
+		$labels = [
+			  [ 'Access Point', 'ap' ]
+			, [ 'Bluetooth', 'bluetooth' ]
+		];
+		$menus = '';
+	} else if ( $system ) {
+		$labels = [
+			  [ 'Device' ]
+			, [ 'Output' ]
+			, [ 'Server rAudio', 'rserver' ]
+			, [ 'Shared Data',   'networks' ]
+			, [ 'Storage' ]
+		];
+		$menus = [
+			  [ 'library', 'Library', 'refresh-library' ]
+			, [ 'raudio',  'System',  'relays' ]
+		];
+	}
+	foreach( $labels as $l ) { // switch label
+		$icon = isset( $l[ 1 ] ) ? i( $l[ 1 ] ) : ' &emsp;';
+		$l    = $l[ 0 ];
+		$name = strtolower( preg_replace( '/ |-/', '', $l ) );
+		${'l_'.$name} = '<a class="helpmenu label">'.$l.$icon.'</a>';
+	}
+	foreach( $menus as $m ) { // menu
+		$name = str_replace( '-', '', $m[ 2 ] );
+		${'m_'.$name} = '<a class="helpmenu">'.i( $m[ 0 ] ).' '.$m[ 1 ].i( $m[ 2 ].' sub' ).'</a>';
+	}
+	
 	include 'settings/function.php';
 	include 'settings/'.$page.'.php'; // addons: by addons.js
 }
