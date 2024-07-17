8de61361908dca421bf094d65a4a73a2c6ff6ffd
rern <rernrern@gmail.com>
2024-07-17 17:24:52 +0700

    Update guide.php

--- srv/http/settings/guide.php
+++ srv/http/settings/guide.php
@@ -1,63 +1,26 @@
+</div>
 <style>
-#guide {
-	width: 100%;
-	height: 100%;
-	left: 0;
-	right: 0;
-	margin: 0 auto;
-	user-select: none;
-}
-.helpblock { margin: 0 }
-#guide .bottom-bar {
-	height: 33px;
-	background: var( --cgd );
-}
-#guide p {
-	margin: 0;
-	line-height: 40px;
-}
-#guide a {
-	display: inline-block;
-	width: 20%;
-	line-height: 33px;
-	padding-right: 5px;
-	text-align: center;
-	cursor: pointer;
-}
-#guide i {
-	width: 33px;
-	vertical-align: -2px;
-	font-size: 20px;
-	text-align: center;
-}
-#guide a.active { background-color: var( --cm ) !important }
-#guide img { width: 100% }
-#prevnext { padding-right: 0 !important }
-@media ( max-width: 450px ) {
-	#guide a span { display: none }
-}
-@media (max-width: 700px) {
-    .container { padding: unset }
+.head i:not( .page-icon, .close ) { display: none }
+img {
+	display: block;
+	margin: 40px auto 0;
+	width: auto;
+	height: auto;
+	max-width: 100%;
+	max-height: 100%;
+	border: 1px solid hsl(0,0%,15%);
 }
 </style>
 
-<p class="helpblock" style="display: none">
-Bottom bar :
- • Arrow icons       - Previous / next page
- • Icons with label - Skip to each section
-</p>
-<div id="guide">
-	<p><span class="count" style="float: right"></span></p>
-	
-	<img class="image" src="/assets/img/guide/1.jpg<?=$hash?>">
-	
+<img class="image" src="/assets/img/guide/1.jpg<?=$hash?>">
+<div id="bar-bottom">
 <?php
-$html     = '<div class="bottom-bar">';
+$html     = '';
 foreach( [ 'library', 'playback', 'playlist', 'settings' ] as $id ) {
-	$html.= '<a id="'.$id.'" class="btn">'.i( $id ).'<span>'.ucfirst( $id ).'</span></a>';
+	$html.= '<div id="'.$id.'" class="btn">'.i( $id ).' <a>'.ucfirst( $id ).'</a></div>';
 }
-$html    .= '<a id="prevnext">'.i( 'back prev' ).i( 'arrow-right next' ).'</a></div>';
-echo $html;
+$html    .= '<div id="prevnext">'.i( 'back prev' ).' '.i( 'arrow-right next' ).'</div>';
+echo $html.'</div>';
 ?>
 </div>
 <script>
@@ -67,25 +30,14 @@ nsettings = 47;
 ntotal    = 58;
 n         = 1;
 E         = {};
-[ 'close', 'container', 'count', 'helpblock', 'helphead', 'image', 'next', 'prev' ].forEach( ( el ) => {
+[ 'close', 'image', 'next', 'prev' ].forEach( ( el ) => {
 	E[ el ] = document.getElementsByClassName( el )[ 0 ];
 } );
 E.btns    = Array.from( document.getElementsByClassName( 'btn' ) );
 
 E.btns[ 1 ].classList.add( 'active' );
-E.count.textContent = n +' / '+ ntotal;
-E.container.classList.remove( 'hide' );
 E.close.addEventListener( 'click', function() {
 	location.href = '/';
-} );
-E.helphead.addEventListener( 'click', function() {
-	if ( E.helpblock.style.display === 'none' ) {
-		this.classList.add( 'bl' );
-		E.helpblock.style.display = '';
-	} else {
-		this.classList.remove( 'bl' );
-		E.helpblock.style.display = 'none';
-	}
 } );
 E.btns.forEach( ( el ) => {
 	el.addEventListener( 'click', function() {
@@ -111,15 +63,13 @@ E.prev.addEventListener( 'click', function() {
 document.body.addEventListener( 'keydown', ( e ) => {
 	switch ( e.key ) {
 		case 'ArrowLeft':
+		case 'ArrowUp':
 			E.prev.click();
 			break
 		case 'ArrowRight':
+		case 'ArrowDown':
 			E.next.click();
 			break
-		case 'F1':
-			e.preventDefault();
-			E.helphead.click();
-			return
 		case 'x':
 			if ( e.ctrlKey ) location.href = '/';
 			break
@@ -137,11 +87,9 @@ if ( navigator.maxTouchPoints ) { // swipe
 			xdiff > 0 ? E.next.click() : E.prev.click();
 		}
 	} );
-	E.helpblock.prepend( '\nSwipe          : Previous / next page' );
 }
 
 function renderPage( n ) {
-	E.count.textContent = n +' / '+ ntotal;
 	E.image.src = '/assets/img/guide/'+ n +'.jpg<?=$hash?>';
 	E.btns.forEach( ( el ) => {
 		el.classList.remove( 'active' );
