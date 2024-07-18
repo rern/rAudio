25d29bacf58dffbaf5ada73eeac9b1fcefae51ba
rern <rernrern@gmail.com>
2024-07-18 15:56:31 +0700

    Update guide.js

--- srv/http/assets/js/guide.js
+++ srv/http/assets/js/guide.js
@@ -7,33 +7,35 @@ var page  = {
 	, total    : 58
 }
 var E     = {};
-[ 'close', 'bar-bottom', 'guideimg' ].forEach( el => E[ el ] = document.getElementById( el ) );
+[ 'bar-bottom', 'close', 'guideimg' ].forEach( el => E[ el ] = document.getElementById( el ) );
 var hash  = E.guideimg.src.replace( /.*jpg/, '' )
 var tabs  = E[ 'bar-bottom' ].children;
 var tabsL = tabs.length;
 for( i = 0; i < tabsL; i++ ) {
 	E[ tabs[ i ].id ] = tabs[ i ];
 	tabs[ i ].addEventListener( 'click', function() {
-		var tabactive = document.getElementsByClassName( 'active' )[ 0 ];
+		var tabactive = E[ 'bar-bottom' ].querySelector( '.active' );
 		if ( this === tabactive ) return
 		
 		var id = this.id;
 		var active;
 		if ( id === 'guidenext' ) {
-			n = n < page.total ? n + 1 : 1;
+			n++;
+			if ( n > page.total ) n = 1;
 		} else if ( id === 'guideprev' ) {
-			n = n > 1 ? n - 1 : page.total;
+			n--;
+			if ( n < 1 ) n = page.total;
 		} else {
 			n = page[ id ];
 		}
-		if ( n > 0 && n < page.library ) {
-			active = 'playback';
-		} else if ( n >= page.library && n < page.playlist ) {
-			active = 'library';
-		} else if ( n >= page.playlist && n < page.settings ) {
-			active = 'playlist';
-		} else if ( n >= page.settings && n <= page.total ) {
+		if ( n >= page.settings ) {
 			active = 'settings';
+		} else if ( n >= page.playlist ) {
+			active = 'playlist';
+		} else if ( n >= page.library ) {
+			active = 'library';
+		} else {
+			active = 'playback';
 		}
 		tabactive.className = '';
 		E[ active ].className = 'active'
