65ef1bc988dcc9fea9fc61961f511de9c2e12979
rern <rernrern@gmail.com>
2024-11-11 10:00:45 +0700

    u

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ alias=r1
 
 . /srv/http/bash/settings/addons.sh
 
-# 20241110
+# 20241111
+file=/etc/mediamtx/mediamtx.yml
+[[ -e $file ]] && sed -i 's/RTSP_PATH/MTX_PATH/' $file
+
 if [[ ! -e /boot/kernel.img ]]; then
 	revision=$( grep ^Revision /proc/cpuinfo )
 	[[ ${revision: -3:2} < 11 ]] && echo 'options brcmfmac feature_disable=0x82000' > /etc/modprobe.d/brcmfmac.conf
