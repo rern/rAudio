8f4bafed1b13fefa8e3c4aba4e6c3a723b17875d
rern <rernrern@gmail.com>
2024-11-11 18:49:01 +0700

    u

--- srv/http/bash/relays-timer.sh
+++ srv/http/bash/relays-timer.sh
@@ -5,21 +5,16 @@
 killProcess relaystimer
 echo $$ > $dirshm/pidrelaystimer
 
-timerfile=$dirshm/relayson
-timer=$( getVar timer $dirsystem/relays.conf | tee > $timerfile )
+timer=$( getVar timer $dirsystem/relays.conf )
 i=$timer
 while sleep 60; do
 	if grep -q -m1 '^state.*play' $dirshm/status || grep -q -m1 RUNNING /proc/asound/card*/pcm*p/sub*/status; then
-		(( $i != $timer )) && echo $timer > $timerfile
+		i=$timer
 	else
-		[[ $off ]] && $dirbash/relays.sh off && exit
-# --------------------------------------------------------------------
-		i=$(( $( getContent $timerfile 1 ) - 1 ))
-		if (( $i > 1 )); then
-			echo $i > $timerfile
-		else
-			off=1
-			pushData relays '{ "timer": '$timer' }'
-		fi
+		(( i-- ))
+		case $i in
+			1 ) pushData relays '{ "timer": '$timer' }';;
+			0 ) $dirbash/relays.sh off && break;;
+		esac
 	fi
 done
