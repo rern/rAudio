b56ce2ef750baeb7128149673b33a8c2057724be
rern <rernrern@gmail.com>
2025-01-04 17:01:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ alias=r1
 
 . /srv/http/bash/settings/addons.sh
 
-# 20250106
+# 20250110
 if ! pacman -Q mpd_oled &> /dev/nul; then
 	pacman -R --noconfirm audio_spectrum_oled &> /dev/null
+	rm -f /etc/systemd/system/mpd_oled.service
 	pacman -Sy --noconfirm mpd_oled
 fi
 
@@ -34,55 +35,10 @@ if [[ -e /boot/kernel7.img ]] && ! grep -q mesa $file; then
 	sed -i '/^IgnorePkg/ s/$/ mesa/' $file
 fi
 
-sed -i '/^brightness/ d' $dirsystem/localbrowser.conf
+[[ ! -e /boot/kernel.img ]] && sed -i '/^brightness/ d' $dirsystem/localbrowser.conf
 
 # 20241130
 systemctl -q is-active mediamtx && touch $dirsystem/dabradio
-
-# 20241110
-if [[ ! -e /boot/kernel.img ]]; then
-	revision=$( grep ^Revision /proc/cpuinfo )
-	if [[ ${revision: -3:2} < 11 ]]; then
-		file=/etc/modprobe.d/brcmfmac.conf
-		[[ ! -e $file ]] && echo 'options brcmfmac feature_disable=0x82000' > $file
-	fi
-fi
-
-# 20241108
-[[ $( pacman -Q cava ) < 'cava 0.10.2-2' ]] && pacman -Sy --noconfirm cava
-
-file=$dirsystem/lcdchar.conf
-if [[ -e $file ]] && grep -q -m1 ^0= $file; then
-	rm $dirsystem/lcdchar*
-fi
-
-# 20241111
-if [[ ! -e /boot/kernel.img ]]; then
-	revision=$( grep ^Revision /proc/cpuinfo )
-	[[ ${revision: -3:2} < 11 ]] && echo 'options brcmfmac feature_disable=0x82000' > /etc/modprobe.d/brcmfmac.conf
-fi
-
-file=/etc/systemd/system/dab.service
-if [[ -e $file ]] && grep -q Requires $file; then
-	sed -i '/^Requires\|^After/ d' $file
-	rm -rf /etc/systemd/system/mediamtx.service.d
-	systemctl daemon-reload
-	systemctl try-restart mediamtx
-fi
-
-# 20241108
-[[ $( pacman -Q cava ) < 'cava 0.10.2-2' ]] && pacman -Sy --noconfirm cava
-
-rm -f $dirsystem/lcdmodel
-
-file=$dirsystem/lcdchar.conf
-if [[ -e $file && $( sed -n -E '/^charmap/,/^p0/ p' $file | wc -l ) -gt 2 ]]; then
-	. $file
-	for k in inf cols charmap p0 pin_rs p1 pin_rw p2 pin_e p3 backlight; do
-		conf+="$k=${!k}\n"
-	done
-	echo -e $conf > $file
-fi
 
 #-------------------------------------------------------------------------------
 installstart "$1"
