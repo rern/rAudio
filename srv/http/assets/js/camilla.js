fcb7b1f88155ab56f1b9cdcf4c8cc3d577fdeef5
rern <rernrern@gmail.com>
2025-01-19 13:57:36 +0700

    Update camilla.js

--- srv/http/assets/js/camilla.js
+++ srv/http/assets/js/camilla.js
@@ -429,7 +429,7 @@ var ycommon   = {
 	, anchor     : 'free'
 	, autoshift  : true
 }
-var ticktext  = [ 'Hz 20 &nbsp; ', '', '100', '', '1k', '', '10k', '20k' ];
+var ticktext  = [ 'Hz 20 &nbsp; ', 50, 100, 500, '1k', '5k', '10k', '20k' ];
 var axes      = {
 	  freq       : {
 		  filters  : {
