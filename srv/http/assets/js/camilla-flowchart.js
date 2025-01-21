06fe25260e933689d26ed3dc3c40887caf0867e3
rern <rernrern@gmail.com>
2025-01-21 15:03:27 +0700

    Update camilla-flowchart.js

--- srv/http/assets/js/camilla-flowchart.js
+++ srv/http/assets/js/camilla-flowchart.js
@@ -85,11 +85,9 @@ function deviceText( device ) {
 function createPipelinePlot() {
 	var ch               = DEV.capture.channels > DEV.playback.channels ? DEV.capture.channels : DEV.playback.channels;
 	var vb_h             = FL.height / 4 * ch;            // boxH - @ ch/box = 1/4 h
-	var vb_x             = 30;
-	var vb_y             = ( FL.height - vb_h - 20 ) / 2; // top  - move up: ( h - boxH - textH ) / 2
-	var vb_w             = FL.width - 90;
+	var vb               = { x: 30, y: ( FL.height - vb_h - 20 ) / 2, w: FL.width - 90, h: vb_h } // top  - move up: ( h - boxH - textH ) / 2
 	$( '#divpipeline .entries.main' ).before(
-		'<svg class="flowchart" xmlns="http://www.w3.org/2000/svg" viewBox="'+ vb_x +' '+ vb_y +' '+ vb_w +' '+ vb_h +'"></svg>'
+		'<svg class="flowchart" xmlns="http://www.w3.org/2000/svg" viewBox="'+ vb.x +' '+ vb.y +' '+ vb.w +' '+ vb.h +'"></svg>'
 	);
 	var d3svg            = d3.select( $( '#pipeline .flowchart' )[ 0 ] );
 	
