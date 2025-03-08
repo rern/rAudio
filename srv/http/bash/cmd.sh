805997a095f52529e9105f2cda8520188e70dde1
rern <rernrern@gmail.com>
2025-03-08 18:12:00 +0700

    Update cmd.sh

--- srv/http/bash/cmd.sh
+++ srv/http/bash/cmd.sh
@@ -100,7 +100,7 @@ playerStop() {
 			$dirbash/status-push.sh
 			;;
 	esac
-	[[ -e $dirshm/pidrelaystimer ]] && $dirbash/relays-timer.sh &> /dev/null &
+	[[ $( getVar timeron $dirsystem/relays.conf ) == true ]] && $dirbash/relays-timer.sh &> /dev/null &
 }
 plClear() {
 	mpc -q clear
