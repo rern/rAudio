91f014eea9bb651ef8cc19ec47cff9e5f3876194
rern <rernrern@gmail.com>
2025-03-09 12:57:09 +0700

    Update startup.sh

--- srv/http/bash/startup.sh
+++ srv/http/bash/startup.sh
@@ -23,6 +23,15 @@ if [[ -e /boot/expand ]]; then # run once
 	if [[ ! -e /boot/kernel.img ]] && (( ${revision: -4:1} < 3 )); then # not legacy kernel && not RPi 4/5: SAE(WPA3), FWSUP
 		echo 'options brcmfmac feature_disable=0x82000' > /etc/modprobe.d/brcmfmac.conf
 	fi
+	if [[ ${revision: -3:2} == 12 ]]; then # zero 2
+		systemctl enable getty@tty1
+		systemctl disable --now bootsplash localbrowser
+		pacman -R --noconfirm firefox matchbox-window-manager plymouth-lite-rbp-git upower \
+			xf86-video-fbturbo xf86-input-evdev xf86-video-fbdev xf86-video-vesa xinput_calibrator xorg-server xorg-xinit
+		rm -f /etc/systemd/system/{bootsplash,localbrowser}
+		rm -rf /etc/X11
+		sed -i 's/tty3 .*/tty1/' /boot/cmdline.txt
+	fi
 fi
 
 backupfile=$( ls /boot/*.gz 2> /dev/null | head -1 )
