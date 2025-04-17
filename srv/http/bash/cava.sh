f2854797551106e24c90bd5cd66db9886233048c
rern <rernrern@gmail.com>
2025-04-17 09:05:24 +0700

    u

--- srv/http/bash/cava.sh
+++ srv/http/bash/cava.sh
@@ -2,10 +2,33 @@
 
 . /srv/http/bash/common.sh
 
-if [[ $1 == stop ]]; then
-	killall vu.sh
-	pin_0=$( sed 's/^.*=//; s/$/=0/' $dirsystem/vuled.conf )
-	gpioset -t0 -c0 $pin_0 # all off
-else
-	[[ -e $dirsystem/vuled || -e $dirsystem/vumeter ]] && cava -p /etc/cava.conf | $dirbash/vu.sh
+pin_0=$( sed 's/.*=//; s/$/=0/' $dirsystem/vuled.conf )
+[[ $1 == stop ]] && gpioset -t0 -c0 $pin_0 && exit
+# --------------------------------------------------------------------
+[[ -e $dirsystem/vumeter ]] && vumeter=1 && j=0
+if [[ -e $dirsystem/vuled ]]; then
+	vuled=1
+	pL=$( wc -l <<< $pin_0 )
+	on=( "$pin_0" )
+	for (( i=1; i < $pL + 1; i++ )); do
+		on+=( "$( sed '1,'$i' s/0$/1/' <<< $pin_0 )" )
+	done
 fi
+cava -p /etc/cava.conf | while read vu; do
+	v=${vu:0:-1}
+	if [[ $vuled ]]; then
+		if (( v > 0 )); then
+			ir=$(( pL - 1 )) # i - roundup
+			vr=$(( v + ir )) # v - roundup
+			v=$(( vr / pL ))
+		fi
+		gpioset -t0 -c0 ${on[v]}
+	fi
+	if [[ $vumeter ]]; then
+		(( j++ ))
+		if (( $j == 10 )); then # framerate throttle - push @10 signal
+			pushData vumeter '{"val":'$v'}'
+			j=0
+		fi
+	fi
+done
