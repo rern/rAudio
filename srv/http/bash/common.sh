28f659381f6a224e4313990894eb1f171ce584e7
rern <rernrern@gmail.com>
2025-04-17 18:09:39 +0700

    Update common.sh

--- srv/http/bash/common.sh
+++ srv/http/bash/common.sh
@@ -270,6 +270,7 @@ getVar() { # var=value
 	esac
 }
 gpioState() {
+	local p state tf
 	for p in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27; do
 		[[ $( gpioget -a -c0 --numeric $p ) == 0 ]] && tf=false || tf=true # -a : prevent reset
 		state+=', "'$p'": '$tf
