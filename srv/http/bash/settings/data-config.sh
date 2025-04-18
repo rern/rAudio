76fb54346c248f76be43df82d09b81be2aee7f0b
rern <rernrern@gmail.com>
2025-04-18 13:31:56 +0700

    Update data-config.sh

--- srv/http/bash/settings/data-config.sh
+++ srv/http/bash/settings/data-config.sh
@@ -3,7 +3,7 @@
 . /srv/http/bash/common.sh
 
 gpioState() {
-	gpioget -c0 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 \
+	gpioget -a -c0 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 \
 		| sed -e 's/=active/: true,/g; s/=inactive/: false,/g;' -e 's/^/{ /; s/,$/ }/'
 }
 
