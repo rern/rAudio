b972ade292372f708523fdcc3c2facee06b0d9a7
rern <rernrern@gmail.com>
2025-04-26 14:11:08 +0700

    Update status-radiotitle.py

--- srv/http/bash/status-radiotitle.py
+++ srv/http/bash/status-radiotitle.py
@@ -6,15 +6,13 @@ import urllib.request
 request  = urllib.request.Request( sys.argv[ 1 ], headers={ 'Icy-MetaData': 1 } )
 response = urllib.request.urlopen( request )
 metaint  = response.headers.get( 'icy-metaint' )
-if metaint is None:
-    print( -1 )
-    sys.exit()
+if metaint is None: sys.exit()
 # --------------------------------------------------------------------
-buffer        = response.read( int( metaint ) + 255 )
-string        = ''
+buffer   = response.read( int( metaint ) + 255 )
+string   = ''
 for byte in buffer: string += chr( int( byte ) )
-title_pos     = string.find( 'StreamTitle=' )
-title_content = string[ title_pos + 13: ]
-semicolon_pos = title_content.find( ';' )
-title         = title_content[ :semicolon_pos - 1 ]
+start    = string.find( 'StreamTitle=' )
+title    = string[ start: ]
+end      = title.find( "';" )
+title    = title[ 13:end ]
 print( title )
