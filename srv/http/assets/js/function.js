321e7cea63de2f495144b3f564636b93d55204f8
rern <rernrern@gmail.com>
2025-05-28 17:39:23 +0700

    Update function.js

--- srv/http/assets/js/function.js
+++ srv/http/assets/js/function.js
@@ -2235,7 +2235,7 @@ var PROGRESS  = {
 	  animate : () => { // knob + bar
 		if ( ! D.time && ! D.cover ) return
 		
-		$( '#time svg, #time-bar' ).css( 'transition-duration', S.Time - S.elapsed +'s' ); // ms - from current to full
+		$( '#time path, #time-bar' ).css( 'transition-duration', S.Time - S.elapsed +'s' ); // ms - from current to full
 		PROGRESS.arc( 654 );                                                               // full circle: stroke-dasharray = 654
 		$( '#time-bar' ).css( 'width', '100%' );
 	}
