6a645041e689bcf1a5f1a675684fffcf01be6d01
rern <rernrern@gmail.com>
2025-07-19 19:32:13 +0700

    Update status-push.sh

--- srv/http/bash/status-push.sh
+++ srv/http/bash/status-push.sh
@@ -31,7 +31,7 @@ if [[ $1 == statusradio ]]; then # from status-radio.sh radioStatusFile
 	onWhilePlay
 else
 	status=$( $dirbash/status.sh )
-	grep -q '"state".*""' <<< $status && status=$( $dirbash/status.sh ) # fix: no state on start playing dsd (<rpi4)
+	grep -q '"state".*""' <<< $status && status=$( $dirbash/status.sh ) # fix: no state on start playing dsd from network (<rpi4)
 	status=$( jq <<< $status )
 	for k in Artist Album Composer Conductor elapsed file player station state Time timestamp Title volume webradio; do
 		filter+='|^  "'$k'"'
