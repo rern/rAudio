e343d4ea69eb00af8035c8d87f8e2eadeef86572
rern <rernrern@gmail.com>
2025-08-01 17:56:01 +0700

    Update stoptimer.sh

--- srv/http/bash/stoptimer.sh
+++ srv/http/bash/stoptimer.sh
@@ -4,15 +4,6 @@
 
 . $dirsystem/stoptimer.conf
 
-volumeToggle() {
-	$dirbash/cmd.sh "volume
-$1
-$2
-$mixer
-$card
-CMD CURRENT TARGET CONTROL CARD"
-}
-
 killProcess stoptimer
 echo $$ > $dirshm/pidstoptimer
 
@@ -22,13 +13,21 @@ pushData mpdplayer '{ "stoptimer": true }'
 sleep $(( min * 60 ))
 
 rm $dirshm/pidstoptimer
-. <( grep -E '^card|^mixer' $dirshm/output )
 volume=$( volumeGet )
+. <( grep -E '^card|^mixer' $dirshm/output )
 
-volumeToggle $volume 0
+$dirbash/cmd.sh "volume
+$volume
+0
+$mixer
+$card
+CMD CURRENT TARGET CONTROL CARD"
+
 $dirbash/cmd.sh playerstop
+
 sleep 1
-volumeToggle 0 $volume
+fn_volume=$( < $dirshm/volumefunction )
+$fn_volume $volume% "$mixer" $card
 
 if [[ $poweroff ]]; then
 	$dirbash/power.sh
