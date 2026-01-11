ceed9dab7d29e1bea0e0a2345560191ffe7abe4b
rern <rernrern@gmail.com>
2026-01-11 19:38:19 +0700

    Update system.sh

--- srv/http/bash/settings/system.sh
+++ srv/http/bash/settings/system.sh
@@ -463,7 +463,8 @@ wlan )
 			iw reg set $REGDOM
 		fi
 	else
-		rmmod brcmfmac_wcc brcmfmac &> /dev/null
+		lsmod | grep -q brcmfmac_cyw && mod=cyw || mod=wcc
+		rmmod brcmfmac_$mod brcmfmac &> /dev/null
 	fi
 	pushRefresh
 	[[ $( cat /sys/class/net/wlan0/operstate ) == up ]] && active=true || active=false
