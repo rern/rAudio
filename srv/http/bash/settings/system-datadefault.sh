2c35cc394f52ae90988aa57b3ca625c9e1ce3deb
rern <rernrern@gmail.com>
2026-04-06 17:01:59 +0700

    u

--- srv/http/bash/settings/system-datadefault.sh
+++ srv/http/bash/settings/system-datadefault.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # data directories
-dirdata=/srv/http/data
-mkdir -p $dirdata/{addons,audiocd,bookmarks,camilladsp,lyrics,mpd,mpdconf,playlists,system,webradio,webradio/img} \
+mkdir -p /srv/http/data/{addons,audiocd,bookmarks,camilladsp,lyrics,mpd,mpdconf,playlists,system,webradio,webradio/img} \
 		 /mnt/MPD/{NAS,SD,USB}
+
+. /srv/http/bash/common.sh
+
 [[ -e /bin/camilladsp ]] && mkdir -p $dircamilladsp/{coeffs,configs,configs-bt,raw} || rmdir $dircamilladsp
 ln -sf /dev/shm $dirdata
 ln -sf /mnt /srv/http/
 chown -h http:http $dirshm /srv/http/mnt
-
-. /srv/http/bash/common.sh
-
 # display
 true='album albumartist artist bars buttons composer conductor count cover date fixedcover genre
 	  label latest nas playbackswitch playlists plclear plsimilar sd time usb volume webradio'
