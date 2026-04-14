c8ce4637daafec02bb46ce2212ea539631460ab6
rern <rernrern@gmail.com>
2026-04-15 03:18:04 +0700

    Update login.js

--- srv/http/assets/js/login.js
+++ srv/http/assets/js/login.js
@@ -1,8 +1,10 @@
 $( function() { // document ready start >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 var E = {
-	  input  : $( 'input' )
-	, passwd : typeof ip !== 'undefined'
+	  eye    : $( '.i-eye' )
+	, input  : $( 'input' )
+	, logo   : $( 'svg' ).eq( 0 )
+	, passwd : $( '#qr' ).length
 };
 [ 'infoOverlay', 'ok', 'pwd', 'pwd2', 'qr', 'set' ].forEach( id => {
 	E[ id ] = $( '#'+ id );
@@ -17,7 +19,7 @@ if ( E.passwd ) {
 } else {
 	$( '#qr, #message, label, #pwd2' ).remove();
 	E.input.attr( 'type', 'password' );
-	$( '.i-eye' ).removeClass( 'bl' );
+	E.eye.removeClass( 'bl' );
 }
 E.pwd.focus();
 E.input.on( 'keyup cut paste', e => {
@@ -36,7 +38,7 @@ E.input.on( 'keyup cut paste', e => {
 		}
 	}, 0 );
 } );
-$( '.i-eye' ).on( 'click', function() {
+E.eye.on( 'click', function() {
 	E.input.attr( 'type', E.input.attr( 'type' ) === 'text' ? 'password' : 'text' );
 	$( this ).toggleClass( 'bl' );
 } );
@@ -58,12 +60,8 @@ E.set.on( 'click', function() {
 			E.infoOverlay.removeClass( 'hide' );
 		} else if ( E.passwd ) {
 			var localhost = location.hostname === 'localhost';
-			if ( localhost ) {
-				E.logo = $( 'svg' ).eq( 0 );
-			} else {
-				$( '#login' ).addClass( 'blink' );
-			}
-			$( '#message, label, input, i, #set' ).remove();
+			if ( ! localhost ) $( '#login' ).addClass( 'blink' );
+			$( '#login' ).children().slice( 3 ).remove();
 			setInterval( () => {
 				fetch( '/data/shm/startup' ).then( response => {
 					if ( response.ok ) location.reload();
