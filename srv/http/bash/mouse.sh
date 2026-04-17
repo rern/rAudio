578fbe84e52482696073c3582e5a01aac537d173
rern <rernrern@gmail.com>
2026-04-17 14:37:43 +0700

    Update mouse.sh

--- srv/http/bash/mouse.sh
+++ srv/http/bash/mouse.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+file_last=/dev/shm/mouse
+now=$( date +%s%3N )
+[[ -e $file_last ]] && last=$( < $file_last ) || last=0
+(( $(( now - last )) < 1000 )) && exit # 1s throttle udev.rules events
+# --------------------------------------------------------------------
+echo $now > $file_last
 [[ $1 ]] && remove=1 || add=1
 file=/srv/http/data/system/localbrowser.conf
 grep -q ^cursor=true $file && cursor=1
