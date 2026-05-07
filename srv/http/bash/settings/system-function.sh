3943f34115bc35e1fe900f699fdb3e669b18896f
rern <rernrern@gmail.com>
2026-05-07 14:16:02 +0700

    Update system-function.sh

--- srv/http/bash/settings/system-function.sh
+++ srv/http/bash/settings/system-function.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 configReboot() {
-	[[ $ON ]] && pushData reboot '{ "id": "'$CMD'" }'
+	[[ ! $ON ]] && return
+	
+	pushData reboot '{ "id": "'$CMD'" }'
 	name=$( sed -n "/'id'.*'$CMD'/ {n; s/.* => *'//; s/'//; p}" /srv/http/settings/system.php )
 	appendSortUnique $dirshm/reboot ', "'$CMD'": "'$name'"'
 }
