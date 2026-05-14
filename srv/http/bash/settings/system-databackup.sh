9fdd6aa638f049fd59a380cd7983d5b4613f8b48
rern <rernrern@gmail.com>
2026-05-14 07:39:10 +0700

    u

--- srv/http/bash/settings/system-databackup.sh
+++ srv/http/bash/settings/system-databackup.sh
@@ -2,66 +2,65 @@
 
 . /srv/http/bash/common.sh
 
-backupfile=$dirshm/backup.gz
-dirconfig=$dirdata/config
-rm -f $backupfile
+file_backup=$dirshm/backup.gz
+dir_config=$dirdata/config
+name_host=$( hostname | tee $dirsystem/hostname )
 alsactl store
-files=(
-	/boot/cmdline.txt
-	/boot/config.txt
-	/boot/shutdown.sh
-	/boot/startup.sh
-	/etc/exports
-	/etc/fstab
-	/etc/shairport-sync.conf
-	/etc/shairport-sync.conf.default
-	/etc/spotifyd.conf
-	/etc/spotifyd.conf.default
-	/etc/upmpdcli.conf
-	/etc/conf.d/wireless-regdom
-	/etc/default/snapclient
-	/etc/modprobe.d/cirrus.conf
-	/etc/modules-load.d/loopback.conf
-	/etc/pacman.d/mirrorlist
-	/etc/samba/smb.conf
-	/etc/systemd/network/en.network
-	/etc/systemd/network/eth.network
-	/etc/systemd/timesyncd.conf
-	/etc/X11/xorg.conf.d/99-calibration.conf
-	/etc/X11/xorg.conf.d/99-raspi-rotate.conf
-	/var/lib/alsa/asound.state
-	/var/lib/iwd/ap/$( hostname ).ap
-	/var/lib/snapserver/server.json
-)
-for file in ${files[@]}; do
+files="\
+/boot/cmdline.txt
+/boot/config.txt
+/boot/shutdown.sh
+/boot/startup.sh
+/etc/exports
+/etc/fstab
+/etc/shairport-sync.conf
+/etc/shairport-sync.conf.default
+/etc/snapserver.conf
+/etc/spotifyd.conf
+/etc/spotifyd.conf.default
+/etc/upmpdcli.conf
+/etc/conf.d/wireless-regdom
+/etc/default/snapclient
+/etc/modprobe.d/cirrus.conf
+/etc/modules-load.d/loopback.conf
+/etc/pacman.d/mirrorlist
+/etc/samba/smb.conf
+/etc/systemd/network/en.network
+/etc/systemd/network/eth.network
+/etc/systemd/timesyncd.conf
+/etc/X11/xorg.conf.d/99-calibration.conf
+/etc/X11/xorg.conf.d/99-raspi-rotate.conf
+/var/lib/alsa/asound.state
+/var/lib/iwd/ap/$name_host.ap
+/var/lib/snapserver/server.json"
+while read file; do
 	if [[ -e $file ]]; then
-		mkdir -p $dirconfig/$( dirname $file )
-		cp {,$dirconfig}$file
+		mkdir -p $dir_config/$( dirname $file )
+		cp {,$dir_config}$file
 	fi
-done
+done  <<< $files
 crossfade=$( mpc crossfade | cut -d' ' -f2 )
 [[ $crossfade ]] && echo $crossfade > $dirsystem/crossfade
-hostname > $dirsystem/hostname
 timedatectl | awk '/zone:/ {print $3}' > $dirsystem/timezone
-profiles=$( ls -p /etc/netctl | grep -v / )
-if [[ $profiles ]]; then
-	cp -r /etc/netctl $dirconfig/etc
+file_profile=$( ls -p /etc/netctl | grep -v / )
+if [[ $file_profile ]]; then
+	cp -r /etc/netctl $dir_config/etc
 	while read profile; do
 		if [[ $( netctl is-enabled "$profile" ) == enabled ]]; then
 			echo $profile > $dirsystem/netctlprofile
 			break
 		fi
-	done <<< $profiles
+	done <<< $file_profile
 fi
-mkdir -p $dirconfig/var/lib
-cp -r /var/lib/bluetooth $dirconfig/var/lib &> /dev/null
-xinitrcfiles=$( ls /etc/X11/xinit/xinitrc.d | grep -v 50-systemd-user.sh )
-if [[ $xinitrcfiles ]]; then
-	mkdir -p $dirconfig/etc/X11/xinit
-	cp -r /etc/X11/xinit/xinitrc.d $dirconfig/etc/X11/xinit
+mkdir -p $dir_config/var/lib
+cp -r /var/lib/bluetooth $dir_config/var/lib &> /dev/null
+file_xinitrc=$( ls /etc/X11/xinit/xinitrc.d | grep -v 50-systemd-user.sh )
+if [[ $file_xinitrc ]]; then
+	mkdir -p $dir_config/etc/X11/xinit
+	cp -r /etc/X11/xinit/xinitrc.d $dir_config/etc/X11/xinit
 fi
 if [[ -e $dirshareddata ]]; then
-	nfsServerActive && cp -rL $dirshareddata $dirconfig
+	nfsServerActive && cp -rL $dirshareddata $dir_config
 fi
 services='bluetooth camilladsp iwd localbrowser mediamtx nfs-server powerbutton shairport-sync smb snapclient spotifyd upmpdcli'
 for service in $services; do
@@ -74,10 +73,10 @@ bsdtar \
 	--exclude './addons' \
 	--exclude './embedded' \
 	--exclude './shm' \
-	-czf $backupfile \
+	-czf $file_backup \
 	-C /srv/http \
 	data \
 	2> /dev/null && echo 1
 
-rm -rf $dirconfig
+rm -rf $dir_config
 rm -f $dirsystem/{crossfade,disable,enable,hostname,timezone}
--- srv/http/bash/settings/system-datarestore.sh
+++ srv/http/bash/settings/system-datarestore.sh
@@ -4,41 +4,40 @@
 
 [[ $1 == true ]] && libraryonly=1
 
-backupfile=$dirshm/backup.gz
-! bsdtar tf "$backupfile" 2> /dev/null | grep -q -m1 ^data/system/display.json$ && exit -1
+file_backup=$dirshm/backup.gz
+! bsdtar tf $file_backup 2> /dev/null | grep -q -m1 ^data/system/display.json$ && exit -1
 # --------------------------------------------------------------------
-dirconfig=$dirdata/config
+dir_config=$dirdata/config
 
 [[ $( mpcState ) == play ]] && $dirbash/cmd.sh playerstop
 [[ -e $dirmpd/listing ]] && killall cmd-list.sh
 mpc | grep -q ^Updating && systemctl restart mpd
 rm -rf $dirdata/{mpd,playlists,webradio}
 if [[ $libraryonly ]]; then
-	bsdtar xpf $backupfile -C /srv/http data/{mpd,playlists,webradio}
+	bsdtar xpf $file_backup -C /srv/http data/{mpd,playlists,webradio}
 	systemctl restart mpd
 	exit
 # --------------------------------------------------------------------
 fi
 find $dirmpdconf -maxdepth 1 -type l -exec rm {} \; # mpd.conf symlink
-bsdtar xpf $backupfile -C /srv/http
+bsdtar xpf $file_backup -C /srv/http
 [[ -e $dirsystem/color ]] && $dirbash/cmd.sh color
 partuuid=$( grep -m1 ^PARTUUID /etc/fstab | cut -d- -f1 )
 for file in boot/cmdline.txt etc/fstab; do
-	sed -i "s/PARTUUID=.*-/$partuuid-/" $dirconfig/$file
+	sed -i "s/PARTUUID=.*-/$partuuid-/" $dir_config/$file
 done
-cp -rf $dirconfig/* /
+cp -rf $dir_config/* /
 [[ -e $dirsystem/enable ]] && systemctl -q enable $( < $dirsystem/enable ) &> /dev/null
 [[ -e $dirsystem/disable ]] && systemctl -q disable $( < $dirsystem/disable ) &> /dev/null
 grep -q nfs-server $dirsystem/enable && $dirsettings/features.sh nfsserver
-name=$( < $dirsystem/hostname )
-hostnamectl set-hostname $name
+hostnamectl set-hostname $( < $dirsystem/hostname )
+timedatectl set-timezone $( < $dirsystem/timezone )
 if [[ -e $dirsystem/netctlprofile ]]; then
 	profile=$( < $dirsystem/netctlprofile )
 	[[ $( netctl is-enabled "$profile" ) != enabled ]] && netctl enable "$profile"
 fi
-timedatectl set-timezone $( < $dirsystem/timezone )
 [[ -e $dirsystem/crossfade ]] && mpc -q crossfade $( < $dirsystem/crossfade )
-rm -rf $dirconfig $dirsystem/{crossfade,enable,disable,hostname,netctlprofile,timezone}
+rm -rf $dir_config $dirsystem/{crossfade,enable,disable,hostname,netctlprofile,timezone}
 dirs=$( ls -d $dirnas/*/ 2> /dev/null )
 if [[ $dirs ]]; then
 	while read dir; do
