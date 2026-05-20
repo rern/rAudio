96091ad88c5cb7ac77254c9e4fa8a21d7b59d9de
rern <rernrern@gmail.com>
2026-05-20 15:51:09 +0700

    Update common.sh

--- srv/http/bash/common.sh
+++ srv/http/bash/common.sh
@@ -348,9 +348,9 @@ inOutputConf() {
 }
 ipAddress() {
 	if [[ $1 ]]; then
-		ip route show dev $( netDevice $1 ) | awk '/^default/ {print $7}'
+		ip route show dev $( netDevice $1 ) | awk '/^default/ {print $7}' | head -1
 	else
-		ip route get 1.1.1.1 | awk '/src/ {print $7}'
+		ip route get 1.1.1.1 | awk '/src/ {print $7}' | head -1
 	fi
 }
 ipOnline() {
