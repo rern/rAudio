eac7d37d7310ddcd3eebedc1e21e50bc7304df77
rern <rernrern@gmail.com>
2026-06-11 09:40:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ alias=r1
 
 . /srv/http/bash/settings/addons.sh
 
+# 20260616
+file=$dirsystem/localbrowser.conf
+grep -q -m1 ^rotate $file && sed -i 's/.*/\U&/' $file
+
 # 20260529
 if [[ $( pacman -Q mpd_oled ) < 'mpd_oled 0.03-2' ]]; then
 	pacman -Sy --noconfirm mpd_oled
@@ -65,66 +69,12 @@ fi
 
 rm -f /root/.bashrc
 
-# 20260409
-if [[ -e /bin/firefox ]]; then
-	file=/lib/firefox/distribution/policies.json
-	[[ ! -e $file ]] && cat << EOF > $file
-{
-	"policies": {
-		"DisableAppUpdate": true,
-		"DontCheckDefaultBrowser": true,
-		"OverrideFirstRunPage": "",
-		"SkipOnboarding": true,
-		"Preferences": {
-			"browser.startup.homepage_override.mstone": "ignore",
-			"browser.sessionstore.resume_from_crash": false,
-			"layout.css.prefers-color-scheme.content-override": 0,
-			"layout.css.devPixelsPerPx": "1.00"
-		}
-	}
-}
-EOF
-	find /root/.config/mozilla -name user.js -delete &> /dev/null
-	file=/etc/systemd/system/localbrowser.service
-	if ! grep -q ^User $file; then
-		sed -i '/^Type/ a\User=root' $file
-		systemctl daemon-reload
-	fi
-fi
-
-dir=/etc/systemd/system/nfs-server.service.d
-if [[ -e /bin/nfsdctl && ! -e $dir ]]; then
-	mkdir -p $dir
-	cat << EOF > $dir/override.conf
-[Service]
-ExecStart=
-ExecStopPost=
-
-ExecStart=/bin/rpc.nfsd
-ExecStop=/bin/rpc.nfsd 0
-EOF
-	systemctl daemon-reload
-	systemctl try-restart nfs-server
-fi
-
-# 20260401
-file=/etc/conf.d/wireless-regdom
-if ! grep -q '^#W' $file; then
-	current=$( < $file )
-	curl -sL https://raw.githubusercontent.com/rern/rAudio/main/wireless-regdom -o $file
-	echo $current >> $file
-fi
-
-file=/etc/ssh/sshd_config
-if grep -q '^PermitEmptyPasswords *yes' $file; then
-	sed -i -E 's/.*(PermitEmptyPasswords ).*/\1no/' $file
-	systemctl restart sshd
-fi
-
 #-------------------------------------------------------------------------------
 installstart "$1"
 
 rm -rf /srv/http/assets/{css,js}
+# 20260616
+rm $dirbash/*
 
 getinstallzip
 
@@ -136,7 +86,11 @@ if [[ ! -e /bin/camilladsp ]]; then
 	rm -rf $dircamilladsp
 	find /srv/http -type f -name camilla* -delete
 fi
-[[ -e /bin/firefox ]] && splashRotate
+if [[ -e /bin/firefox ]]; then
+	splashRotate
+else
+	rm -f $dirbash/startx.sh $dirsettings/features-localbrowser.sh
+fi
 [[ -e $dirsystem/color ]] && $dirbash/cmd.sh color
 rm -f $dirshm/system
 
