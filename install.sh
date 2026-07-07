6f310154214551d20750ab61cc71765e85d27d19
rern <rernrern@gmail.com>
2026-07-07 13:57:53 +0700

    u

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ alias=r1
 
 . /srv/http/bash/settings/addons.sh
 
-# 20260616
+# 20260717
+! pacman -Q gcc &> /dev/null && pacman -Sy --noconfirm gcc
+
 file=$dirmpdconf/conf/bluetooth.conf
 if [[ ! -e $file ]]; then
 	cat << EOF > $file
@@ -40,56 +42,6 @@ if [[ $( pacman -Q mpd_oled ) < 'mpd_oled 0.03-2' ]]; then
 		sed -i 's/fifo"/fifo -X"/' $file
 	fi
 fi
-
-# 20260509
-file=$dirshareddata/source
-if [[ -e $file && $( awk '{print $6}' $file ) ]]; then
-	mp=$( awk '{print $2}' $file | sed 's/\\040/ /g' )
-	echo "$mp" > $file
-fi
-
-if [[ -e /boot/kernel.img ]] && grep -q '^\[core' /etc/pacman.conf; then
-	sed -i '/^\[core]/,$ d' /etc/pacman.conf
-fi
-
-file=/etc/udevil/udevil.conf
-if ! grep -q /mnt/MPD/NAS/USB $file; then
-	sed -i -e 's/, utf8//
-' -e '/allowed_media_dirs/ s|$|,/mnt/MPD/NAS/USB|
-' $file
-	systemctl restart devmon@http
-	systemctl -q is-active nfs-server && $dirsettings/features.sh nfsserver
-fi
-file=/mnt/MPD/.mpdignore
-if [[ -e $file ]]; then
-	rm $file
-	mv /mnt/MPD/{NVME,SATA,SD,USB} /mnt &> /dev/null
-fi
-
-if [[ -e /bin/camilladsp && $( pacman -Q camilladsp ) < 'camilladsp 4.1.3-1' ]]; then
-	systemctl -q is-active camilladsp && active=1
-	[[ $active ]] && systemctl stop camilladsp
-	rm -f $dirshm/hwparams
-	pacman -Sy --noconfirm camilladsp
-	while read f; do
-		sed -i -E -e 's/FLOAT/F/; s/S24LE3/S24_3_LE/' -e 's/([246])LE/\1_LE/' $f
-	done < <( grep -rl '[246]\+LE' $dircamilladsp )
-	[[ $active ]] && systemctl start camilladsp
-fi
-
-if [[ -e /bin/firefox ]]; then
-	file=/etc/udev/rules.d/mouse.rules
-	if [[ ! -e $file ]]; then
-		echo 'ACTION=="add|remove", SUBSYSTEM=="input", ENV{ID_INPUT_MOUSE}=="1", RUN+="/srv/http/bash/settings/features.sh mouse"' > $file
-		udevadm control --reload-rules
-		udevadm trigger
-	fi
-
-	file=$dirsystem/localbrowser.conf
-	grep -q ^cursor $file && sed -i '/^cursor/ d' $file
-fi
-
-rm -f /root/.bashrc
 
 #-------------------------------------------------------------------------------
 installstart "$1"
@@ -129,6 +81,6 @@ rm -f $dirshm/system
 
 installfinish
 
-# 20260616
+# 20260717
 file=$dirmpdconf/bluetooth.conf
 [[ -e $file && ! -L $file ]] && $dirsettings/player-conf.sh
