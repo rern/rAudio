744bc5902379ccf18f835b87a7eec041d00f0b89
rern <rernrern@gmail.com>
2026-08-07 11:06:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ alias=r1
 . /srv/http/bash/settings/addons.sh
 
 # 20260808
+[[ -e /boot/kernel.img ]] && sed -i 's|/+R||' /etc/pacman.conf
 [[ $( pacman -Q audiocd-meta 2> /dev/null ) < 'audiocd-meta 1.0.1-1' ]] && packages+=' audiocd-meta'
 
 # 20260801
