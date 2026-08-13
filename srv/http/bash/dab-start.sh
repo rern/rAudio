75c34a02cf6efea49a8161505a3ad663e4aa87bd
rern <rernrern@gmail.com>
2026-08-13 08:08:56 +0700

    u

--- srv/http/bash/dab-start.sh
+++ srv/http/bash/dab-start.sh
@@ -25,7 +25,7 @@ mkfifo $MYPIPE
 pidof -q dab-rtlsdr-3 && sleep 4 # if another radio is playing, give time to stop
 
 channel_id=${2,,}_${1,,}
-file=$( find $dirdabradio -name *"|$channel_id" ) # .../rtsp:||$host:8554|$channel_id
+file=$( find $dirdabradio -name *"|$channel_id" ) # .../rtsp://$host:8554/$channel_id
 [[ $file ]] && head -1 "$file" > $dirshm/radio
 
 dab-rtlsdr-3 \
@@ -50,7 +50,7 @@ ffmpeg \
 	&> /dev/null &
 FFMPID=$!
 for pid in $( pgrep $FFMPID ); do
-	ionice -c 0 -n 0 -p $pid &> /dev/null 
+	ionice -c 0 -n 0 -p $pid &> /dev/null
 	renice -n -19 -p $pid &> /dev/null
 done
 
