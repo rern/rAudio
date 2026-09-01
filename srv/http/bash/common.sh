6342fb9046012c1a51be58ee006c20e361687f21
rern <rernrern@gmail.com>
2026-09-01 19:04:28 +0700

    Update common.sh

--- srv/http/bash/common.sh
+++ srv/http/bash/common.sh
@@ -631,7 +631,7 @@ volume() {
 			$fn_volume $val% "$CONTROL"
 			sleep 0.2
 		done
-		[[ $fn_volume == volumeAmixer ]] && pushVolume # some dac cannot set exactly on some 1% increments
+		[[ $TYPE != mute && $fn_volume == volumeAmixer ]] && pushVolume # some dac cannot set exactly on some 1% increments
 	fi
 	[[ $fn_volume == volumeAmixer && -e $dirshm/usbdac ]] && alsactl store & # fix: not saved on off / disconnect
 }
