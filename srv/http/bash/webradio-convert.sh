16c177fcfba311c2e3cd318041ca39eeabd5fe21
rern <rernrern@gmail.com>
2026-09-06 17:03:17 +0700

    Update webradio-convert.sh

--- srv/http/bash/webradio-convert.sh
+++ srv/http/bash/webradio-convert.sh
@@ -2,21 +2,6 @@
 
 . /srv/http/bash/common.sh
 
-coverart() {
-	local dir file_cover
-	dir=$1
-	file_cover=$( compgen -G "$dir/cover".* )
-	[[ ! $file_cover ]] && return
-	
-	compgen -G "$dir/coverart".* > /dev/null && return
-	
-	if [[ ${file_cover: -3} == gif ]]; then
-		gifsicle -O3 --resize-fit 200x200 "$file_cover" > "$dir/coverart.gif"
-	else
-		magick "$file_cover" -thumbnail 200x200\> -unsharp 0x.5 "$dir/coverart.jpg"
-	fi
-}
-
 # webradio
 for radio in webradio dabradio; do
 	dir_radio=$dirdata/$radio
@@ -27,27 +12,37 @@ for radio in webradio dabradio; do
 			[[ -e "$file/data" ]] && list+="\
 $( head -1 "$file/data" )^^$file
 "
-			coverart "$file"
-			continue
+		else
+			uri_name=$( basename "$file" )
+			[[ $file == $dir_radio/img/* || $uri_name != http* ]] && continue
+
+			dir=$( dirname "$file" )
+			station=$( head -1 "$file" )
+			dir_station="$dir/$station"
+			mkdir -p "$dir_station"
+			uri=${uri_name//|/\/}
+			sed "1 s|.*|$uri|" "$file" > "$dir_station/data"
+			rm "$file"
+			list+="$uri^^$dir_station"$'\n'
+			while read file_prev; do
+				[[ ${file_prev: -10:6} == -thumb ]] && name=thumb || name=cover
+				file_new="$dir_station/$name.${file_prev: -3}"
+				mv $file_prev "$file_new"
+				coverart "$dir_station"
+			done < <( ls $dir_radio/img/$uri_name* 2> /dev/null )
 		fi
-
-		uri_name=$( basename "$file" )
-		[[ $file == $dir_radio/img/* || $uri_name != http* ]] && continue
-
-		dir=$( dirname "$file" )
-		station=$( head -1 "$file" )
-		dir_station="$dir/$station"
-		mkdir -p "$dir_station"
-		uri=${uri_name//|/\/}
-		sed "1 s|.*|$uri|" "$file" > "$dir_station/data"
-		rm "$file"
-		list+="$uri^^$dir_station"$'\n'
-		while read file_prev; do
-			[[ ${file_prev: -10:6} == -thumb ]] && name=thumb || name=cover
-			file_new="$dir_station/$name.${file_prev: -3}"
-			mv $file_prev "$file_new"
-			coverart "$dir_station"
-		done < <( ls $dir_radio/img/$uri_name* 2> /dev/null )
+		
+		dir=$file
+		file_cover=$( compgen -G "$dir/cover".* )
+		[[ ! $file_cover ]] && continue
+		
+		compgen -G "$dir/coverart".* > /dev/null && continue
+		
+		if [[ ${file_cover: -3} == gif ]]; then
+			gifsicle -O3 --resize-fit 200x200 "$file_cover" > "$dir/coverart.gif"
+		else
+			magick "$file_cover" -thumbnail 200x200\> -unsharp 0x.5 "$dir/coverart.jpg"
+		fi
 	done < <( find $dir_radio )
 	n=$( find $dir_radio -type f -name data | wc -l )
 	sed -i -E 's/("'$radio'": )[0-9]+(,*)$/\1'$n'\2/' $dirmpd/counts
