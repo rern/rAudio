080783f5fbb0c028ac47a3ae04d4bf37d9f89c90
rern <rernrern@gmail.com>
2026-09-06 10:01:27 +0700

    Update function.php

--- srv/http/function.php
+++ srv/http/function.php
@@ -65,7 +65,8 @@ function indexBar( $indexes ) {
 <div class="index index1">'.$indexbar1.'</div>';
 }
 function radioDir( $url ) {
-	$dir   = exec( 'grep -m1 "^'.$url.'" /srv/http/data/mpd/radio | cut -d^ -f3' );
+	$url = str_replace( '*', '.', $url );
+	$dir = exec( 'grep -m1 "^'.$url.'" /srv/http/data/mpd/radio | cut -d^ -f3' );
 	return substr( $dir, 9 ); // /srv/http/data/webradio/... > /data/webradio/...
 }
 function second2HMS( $second ) {
