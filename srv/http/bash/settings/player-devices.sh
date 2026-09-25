846f41a9024358bd3ebfd45240c9f69b5a0c242a
rern <rernrern@gmail.com>
2026-09-25 21:59:47 +0700

    Update player-devices.sh

--- srv/http/bash/settings/player-devices.sh
+++ srv/http/bash/settings/player-devices.sh
@@ -26,8 +26,8 @@
 outputdevice=$( getContent $dirsystem/output-device )
 proccardn=$( ls -d /proc/asound/card[0-9] ) # not depend on /etc/asound.conf which might be broken from bad script
 card=${proccardn: -1}
-usbdac=$( ls -d /proc/asound/card[0-9]/usbmixer 2> /dev/null | wc -l )
-lastcard=$(( card - usbdac )) # last card - not usb
+card_usb=$( ls -d /proc/asound/card[0-9]/usbmixer 2> /dev/null | wc -l )
+lastcard=$(( card - card_usb )) # last card - not usb
 while read path; do
 	info=$( sed 's/bcm2835/On-board/' $path/*/info )
 	name=$( grep -m1 ^name <<< $info | cut -d' ' -f2- )
